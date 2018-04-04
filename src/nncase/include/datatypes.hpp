f763794fd9e6d9e94e9c46db363421c1efd7e868
Guo Hui <sunnycase@live.cn>
2018-04-04 20:43:17 +0800

    initial commit

---
+++ src/nncase/include/datatypes.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace nncase
+{
+	typedef std::vector<float> vec_t;
+
+	struct qvec_t
+	{
+		std::vector<uint8_t> vec;
+		float min, max;
+	};
+
+	struct forward_ctx
+	{
+		vec_t inout;
+		size_t width;
+
+		virtual const vec_t& get_weights(const std::string& name) = 0;
+	};
+}
