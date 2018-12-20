e7c01d6f65e28312545ca42524da9a47e167998b
sunnycase <sunnycase@live.cn>
2018-12-20 13:40:51 +0800

    Add toolbox

---
+++ src/NnCase.Designer.Shell/Modules/Toolbox/Views/ToolboxView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NnCase.Designer.Modules.Toolbox.ViewModels;
+using ReactiveUI;
+
+namespace NnCase.Designer.Modules.Toolbox.Views
+{
+    /// <summary>
+    /// ToolboxView.xaml 的交互逻辑
+    /// </summary>
+    public partial class ToolboxView : ReactiveUserControl<ToolboxViewModel>
+    {
+        public ToolboxView()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                this.OneWayBind(ViewModel, vm => vm.Items, v => v._toolbox.ItemsSource);
+            });
+        }
+    }
+}
