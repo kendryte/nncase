effcb5c60512a0ff9b0593b43126fe3d5a7ba3f7
sunnycase <sunnycase@live.cn>
2018-12-29 11:33:47 +0800

    Add cli

--- src/NnCase.Cli/Program.cs
+++ src/NnCase.Cli/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
 using NnCase.Converter.Converters;
 using NnCase.Converter.Data;
 using NnCase.Converter.Model;
@@ -10,102 +12,106 @@ using NnCase.Converter.Transforms.K210;
 
 namespace NnCase.Cli
 {
+    public class Options
+    {
+        [Option('i', "input-format", Required = true, HelpText = "Set the input format.")]
+        public string InputFormat { get; set; }
+
+        [Option('o', "output-format", Required = true, HelpText = "Set the input format.")]
+        public string OutputFormat { get; set; }
+
+        [Option("dataset", Required = false, HelpText = "Dataset path")]
+        public string Dataset { get; set; }
+
+        [Value(0, MetaName = "input", HelpText = "Input path")]
+        public string Input { get; set; }
+
+        [Value(1, MetaName = "output", HelpText = "Output path")]
+        public string Output { get; set; }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            DoJobAsync().Wait();
-        }
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                if (e.ExceptionObject is Exception ex)
+                    Console.WriteLine("Fatal: " + ex.Message);
+                else
+                    Console.WriteLine("Fatal: Unexpected error occurred.");
+                Environment.Exit(-1);
+            };
 
-        static async Task DoJobAsync()
-        {
-#if false
-            var file = File.ReadAllBytes(@"D:\Work\Repository\models\mobilev1_alpha_1219.tflite");
-            var model = tflite.Model.GetRootAsModel(new FlatBuffers.ByteBuffer(file));
-            var tfc = new TfLiteToGraphConverter(model, model.Subgraphs(0).Value);
-            tfc.Convert();
-            var graph = tfc.Graph;
-            Transform.Process(graph, new Transform[] {
-                new K210SeprableConv2dTransform(),
-                new K210SpaceToBatchNdAndValidConv2dTransform(),
-                new K210SameConv2dTransform(),
-                new K210Stride2Conv2dTransform(),
-                new K210GlobalAveragePoolTransform(),
-                new K210FullyConnectedTransform(),
-                new K210Conv2dWithMaxPoolTransform()
-            });
-            var ctx = new GraphPlanContext();
-            graph.Plan(ctx);
-            var k210c = new GraphToK210Converter(graph);
-            await k210c.ConvertAsync(new ImageDataset(
-                @"D:\Work\Repository\models\img",
-                new[] { 3, 128, 128 },
-                1,
-                PreprocessMethods.None,
-                PostprocessMethods.NormalizeMinus1To1),
-                ctx,
-                @"D:\Work\Repository\kendryte-standalone-sdk\src\kpu6\",
-                "mobilev1_alpha");
+            Options options = null;
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(o => options = o);
+            if (options == null) return;
 
-            using (var f = File.Open(@"D:\Work\Repository\models\mobilev1_test2.pb", FileMode.Create, FileAccess.Write))
-                await ctx.SaveAsync(f);
-#elif true
-            var file = File.ReadAllBytes(@"D:\Work\Repository\models\80class.tflite");
-            var model = tflite.Model.GetRootAsModel(new FlatBuffers.ByteBuffer(file));
-            var tfc = new TfLiteToGraphConverter(model, model.Subgraphs(0).Value);
-            tfc.Convert();
-            var graph = tfc.Graph;
-            Transform.Process(graph, new Transform[] {
-                new K210SeprableConv2dTransform(),
-                new K210SpaceToBatchNdAndValidConv2dTransform(),
-                new K210SameConv2dTransform(),
-                new K210Stride2Conv2dTransform(),
-                new K210GlobalAveragePoolTransform(),
-                new K210FullyConnectedTransform(),
-                new K210Conv2dWithMaxPoolTransform()
-            });
-            var ctx = new GraphPlanContext();
-            graph.Plan(ctx);
-            var k210c = new GraphToK210Converter(graph);
-            await k210c.ConvertAsync(new ImageDataset(
-                @"D:\Work\Repository\models\temp_for_320x256_80classes",
-                new[] { 3, 256, 320 },
-                1,
-                PreprocessMethods.Darknet,
-                PostprocessMethods.Normalize0To1),
-                ctx,
-                @"D:\Work\Repository\kendryte-standalone-sdk\src\kpu8\",
-                "80class");
+            Graph graph;
+            switch (options.InputFormat.ToLowerInvariant())
+            {
+                case "tflite":
+                    {
+                        var file = File.ReadAllBytes(options.Input);
+                        var model = tflite.Model.GetRootAsModel(new FlatBuffers.ByteBuffer(file));
+                        var tfc = new TfLiteToGraphConverter(model, model.Subgraphs(0).Value);
+                        tfc.Convert();
+                        graph = tfc.Graph;
+                        break;
+                    }
+                case "paddle":
+                    {
+                        var tfc = new PaddleToGraphConverter(options.Input);
+                        tfc.Convert(0);
+                        graph = tfc.Graph;
+                        break;
+                    }
+                default:
+                    throw new ArgumentException("input-format");
+            }
 
-            using (var f = File.Open(@"D:\Work\Repository\models\80class2.pb", FileMode.Create, FileAccess.Write))
-                await ctx.SaveAsync(f);
-#elif false
-            var file = File.ReadAllBytes(@"D:\Work\Repository\models\mbnetv1_test.tflite");
-            var model = tflite.Model.GetRootAsModel(new FlatBuffers.ByteBuffer(file));
-            var tfc = new TfLiteToGraphConverter(model, model.Subgraphs(0).Value);
-            tfc.Convert();
-            var graph = tfc.Graph;
-            Transform.Process(graph, new Transform[] {
-            });
-            var ctx = new GraphPlanContext();
-            graph.Plan(ctx);
+            switch (options.OutputFormat.ToLowerInvariant())
+            {
+                case "tf":
+                    {
+                        var ctx = new GraphPlanContext();
+                        graph.Plan(ctx);
 
-            using (var f = File.Open(@"D:\Work\Repository\models\mbnetv1_test2.pb", FileMode.Create, FileAccess.Write))
-                await ctx.SaveAsync(f);
-#else
-            var tfc = new PaddleToGraphConverter(@"D:\Work\Repository\models\MobileNetV1_pretrained");
-            tfc.Convert(0);
-            var graph = tfc.Graph;
-            Transform.Process(graph, new Transform[] {
-            });
-            var ctx = new GraphPlanContext();
-            graph.Plan(ctx);
+                        using (var f = File.Open(options.Output, FileMode.Create, FileAccess.Write))
+                            await ctx.SaveAsync(f);
+                        break;
+                    }
+                case "k210code":
+                    {
+                        Transform.Process(graph, new Transform[] {
+                            new K210SeprableConv2dTransform(),
+                            new K210SpaceToBatchNdAndValidConv2dTransform(),
+                            new K210SameConv2dTransform(),
+                            new K210Stride2Conv2dTransform(),
+                            new K210GlobalAveragePoolTransform(),
+                            new K210FullyConnectedTransform(),
+                            new K210Conv2dWithMaxPoolTransform()
+                        });
 
-            using (var f = File.Open(@"D:\Work\Repository\models\mbnetv1_test_paddle.pb", FileMode.Create, FileAccess.Write))
-                await ctx.SaveAsync(f);
-#endif
-
-            Console.WriteLine("Hello World!");
+                        var ctx = new GraphPlanContext();
+                        graph.Plan(ctx);
+                        var dim = graph.Inputs.First().Output.Dimensions.ToArray();
+                        var k210c = new GraphToK210Converter(graph);
+                        await k210c.ConvertAsync(new ImageDataset(
+                            options.Dataset,
+                            new[] { dim[1], dim[2], dim[3] },
+                            1,
+                            PreprocessMethods.None,
+                            PostprocessMethods.Normalize0To1),
+                            ctx,
+                            Path.GetDirectoryName(options.Output),
+                            Path.GetFileNameWithoutExtension(options.Output));
+                        break;
+                    }
+                default:
+                    throw new ArgumentException("output-format");
+            }
         }
     }
 }
