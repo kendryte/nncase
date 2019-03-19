527ab5880fe60394e43655c0bfa66012f5bbac3d
sunnycase <sunnycase@live.cn>
2019-03-19 16:10:55 +0800

    Add emulator

---
+++ src/NnCase.Converter.K210/Emulator/K210Emulator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NnCase.Converter.K210.Emulator
+{
+    public class K210Emulator
+    {
+        private readonly byte[] _kmodel;
+
+        public K210Emulator(byte[] kmodel)
+        {
+            _kmodel = kmodel;
+        }
+    }
+}
