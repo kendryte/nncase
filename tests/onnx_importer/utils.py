30c2da5e157a7680f5f79742ccbefa0ccf979eb2
Alexey Chernov <4ernov@gmail.com>
2020-03-04 22:57:19 +0300

    Initial suite of test cases for ONNX importer
    Binary and 2D convolution operators are covered for now.

---
+++ tests/onnx_importer/utils.py
@@ -0,0 +1,29 @@
+import subprocess
+
+import torch
+import onnxruntime as ort
+
+from ncc import ncc, kmodel_export_file
+
+
+onnx_export_file = "./tmp/test.onnx"
+
+
+def save(model, input):
+	torch.onnx.export(model, input, onnx_export_file)
+
+
+def compile(args=[]):
+	retcode = subprocess.call([ncc, 'compile', onnx_export_file, kmodel_export_file,
+	 '-i', 'onnx', *args])
+	print('retcode', retcode)
+	assert retcode is 0
+
+
+def run(input):
+	print("onnx_export_file:", onnx_export_file)
+	sess = ort.InferenceSession(onnx_export_file)
+	input_name = sess.get_inputs()[0].name
+	outputs = sess.run(None, { input_name: input })
+
+	return outputs
