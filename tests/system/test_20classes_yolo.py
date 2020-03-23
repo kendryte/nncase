cdcbae0fbe623a4bad1f534559371b2cf5ece4a2
sunnycase <sunnycase@live.cn>
2020-03-23 12:03:25 +0800

    Fix test

--- tests/system/test_20classes_yolo.py
+++ tests/system/test_20classes_yolo.py
@@ -8,11 +8,13 @@ import sys
 [sys.path.append(i) for i in ['.', '..']]
 import ncc
 
+dir_prefix = os.path.realpath(os.path.dirname(os.path.realpath(__file__)) + "/../../examples/20classes_yolo")
+
 def normalize(x):
     return (x.astype(np.float32) - 127.5) / 128.0
 
 def init_values():
-	input = [normalize(plt.imread('../examples/20classes_yolo/k210/kpu_20classes_example/dog.bmp'))]
+	input = [normalize(plt.imread(dir_prefix + '/k210/kpu_20classes_example/dog.bmp'))]
 	expect = ncc.run_tflite(input)
 	expect = np.transpose(expect, [0,3,1,2])
 	ncc.save_expect_array('test', expect)
@@ -21,7 +23,7 @@ def init_values():
 
 def test_simple():
 	ncc.clear()
-	ncc.copy_tflite('../examples/20classes_yolo/model/20classes_yolo.tflite')
+	ncc.copy_tflite(dir_prefix + '/model/20classes_yolo.tflite')
 	init_values()
 	ncc.compile(['--inference-type', 'float', '--max-allocator-solve-secs', '0'])
 
@@ -30,7 +32,7 @@ def test_simple():
 	
 def test_simple_quant():
 	ncc.clear()
-	ncc.copy_tflite('../examples/20classes_yolo/model/20classes_yolo.tflite')
+	ncc.copy_tflite(dir_prefix + '/model/20classes_yolo.tflite')
 	init_values()
 	ncc.compile(['--inference-type', 'uint8', '-t', 'cpu',
 	 '--dataset', ncc.input_dir + '/test.bin', '--dataset-format', 'raw',
@@ -41,7 +43,7 @@ def test_simple_quant():
 	
 def test_simple_k210():
 	ncc.clear()
-	ncc.copy_tflite('../examples/20classes_yolo/model/20classes_yolo.tflite')
+	ncc.copy_tflite(dir_prefix + '/model/20classes_yolo.tflite')
 	init_values()
 	ncc.compile(['--inference-type', 'uint8', '-t', 'k210',
 	 '--dataset', ncc.input_dir + '/test.bin', '--dataset-format', 'raw',
