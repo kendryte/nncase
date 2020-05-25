9069b0782f7b36d6811b0733e3e039e4d30e70ce
Alexey Chernov <4ernov@gmail.com>
2020-05-25 23:48:58 +0300

    ONNX: add global pooling operators
    Namely, `GlobalAveragePool` and `GlobalMaxPool`.

--- src/importer/onnx/ops/pool.cpp
+++ src/importer/onnx/ops/pool.cpp
@@ -52,15 +52,25 @@ namespace
 
 void onnx_importer::convert_op_AveragePool(const NodeProto& node)
 {
-    convert_pool(node, reduce_mean, 0.f);
+    convert_pool<>(node, reduce_mean, 0.f);
+}
+
+void onnx_importer::convert_op_GlobalAveragePool(const NodeProto& node)
+{
+    convert_pool<true>(node, reduce_mean, 0.f);
 }
 
 void onnx_importer::convert_op_MaxPool(const NodeProto& node)
 {
-    convert_pool(node, reduce_max, numeric_limits<float>::max());
+    convert_pool<>(node, reduce_max, 0.f);
 }
 
-void onnx_importer::convert_pool(const NodeProto& node, const reduce_op_t reduce_op, const float init_value)
+void onnx_importer::convert_op_GlobalMaxPool(const NodeProto& node)
+{
+    convert_pool<true>(node, reduce_max, 0.f);
+}
+
+template<bool global> void onnx_importer::convert_pool(const NodeProto& node, const reduce_op_t reduce_op, const float init_value)
 {
     const auto &input { node.input()[0] };
     const auto &output { node.output()[0] };
@@ -77,7 +87,13 @@ void onnx_importer::convert_pool(const NodeProto& node, const reduce_op_t reduce
 
     array<size_t, 2> dilations { 1, 1 };
 
-    const auto &kernel_shape { get_attribute<vector<int>>(node, "kernel_shape").value() };
+    if (input_shape.size() < 4)
+	    throw invalid_argument("Image with 4-dimensional shape is expected on the input of pooling operators.");
+
+    const auto &kernel_shape
+    {
+        global ? vector<int> { static_cast<int>(input_shape[2]), static_cast<int>(input_shape[3]) } : get_attribute<vector<int>>(node, "kernel_shape").value()
+    };
 
     array<size_t, 2> strides { 1, 1 };
 
