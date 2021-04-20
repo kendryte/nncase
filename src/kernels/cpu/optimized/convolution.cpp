6ee8911d6e178545e9b8de9dbccf34cc643c839c
FusionBolt <RealFusionBolt@gmail.com>
2021-04-20 21:35:45 +0800

    boilerplate:add optimized dictory and empty optimized convolution

---
+++ src/kernels/cpu/optimized/convolution.cpp
@@ -0,0 +1,30 @@
+/* Copyright 2019-2020 Canaan Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <nncase/kernels/cpu/optimized/convolution.h>
+#include <nncase/kernels/kernel_utils.h>
+
+using namespace nncase;
+using namespace nncase::runtime;
+using namespace nncase::kernels;
+using namespace nncase::kernels::cpu;
+using namespace nncase::kernels::cpu::optimized;
+
+result<void> optimized::conv2d(const float *input, const float *weights, const float *bias, float *output,
+    const runtime_shape_t &in_shape, const runtime_shape_t &in_strides, const runtime_shape_t &w_shape, const runtime_shape_t &w_strides,
+    const runtime_shape_t &bias_strides, const runtime_shape_t &out_strides, const padding &padding_h, const padding &padding_w,
+    int32_t groups, int32_t stride_h, int32_t stride_w, int32_t dilation_h, int32_t dilation_w, value_range<float> fused_activation) noexcept
+{
+    return ok();
+}
