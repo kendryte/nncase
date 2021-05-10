e23ef2a54f7dcef48c2b1922630ba9100ffe9cb4
FusionBolt <RealFusionBolt@gmail.com>
2021-05-10 13:53:10 +0800

    remove useless include and change function name in concat.cpp

--- src/kernels/cpu/optimized/concat.cpp
+++ src/kernels/cpu/optimized/concat.cpp
@@ -13,7 +13,6 @@
  * limitations under the License.
  */
 #include <nncase/kernels/cpu/optimized/tensor_compute.h>
-#include <nncase/kernels/cpu/reference/tensor_compute.h>
 #include <nncase/kernels/kernel_utils.h>
 #include <nncase/runtime/runtime_op_utility.h>
 
@@ -26,7 +25,7 @@ using namespace nncase::kernels::cpu::optimized;
 namespace
 {
 template <class T>
-result<void> concat_opt_impl(gsl::span<const gsl::byte *const> inputs, T *output, const runtime_shape_t &out_shape,
+result<void> concat_impl(gsl::span<const gsl::byte *const> inputs, T *output, const runtime_shape_t &out_shape,
     gsl::span<const runtime_shape_t> &in_strides, NNCASE_UNUSED const runtime_shape_t &out_strides, size_t axis, const runtime_shape_t &concat_dims, NNCASE_UNUSED kernel_context &context) noexcept
 {
     runtime_shape_t in_shape(out_shape);
@@ -99,7 +98,7 @@ result<void> concat_opt_impl(gsl::span<const gsl::byte *const> inputs, T *output
 
 #define CONCAT_IMPL(size, type) \
     case size:                  \
-        return concat_opt_impl(inputs, reinterpret_cast<type *>(output), out_shape, in_strides, out_strides, axis, concat_dims, context)
+        return concat_impl(inputs, reinterpret_cast<type *>(output), out_shape, in_strides, out_strides, axis, concat_dims, context)
 
 result<void> optimized::concat(datatype_t type, gsl::span<const gsl::byte *const> inputs, gsl::byte *output, const runtime_shape_t &out_shape,
     gsl::span<const runtime_shape_t> in_strides, const runtime_shape_t &out_strides, size_t axis, const runtime_shape_t &concat_dims, kernel_context &context) noexcept
