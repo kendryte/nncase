dea627dcdce4ddaf1ac608dc9bf60fa69fac1ab1
zhangyang <zhangyang@canaan-creative.com>
2021-05-19 09:24:24 +0800

    Fix LpNormalization.
     - pass both l1norm and l2norm with onnx models built by onnx.helper.

--- src/importer/onnx/ops/lp_normalization.cpp
+++ src/importer/onnx/ops/lp_normalization.cpp
@@ -44,11 +44,14 @@ void onnx_importer::convert_op_LpNormalization(const NodeProto &node)
     {
         auto abs = graph_.emplace<unary>(unary_abs, input_shape);
         auto sum = graph_.emplace<reduce>(reduce_sum, abs->output().shape(), reduce_axis, 0.f, true);
+        auto div = graph_.emplace<binary>(binary_div, input_shape, sum->output().shape(), value_range<float>::full());
 
         sum->input().connect(abs->output());
+        div->input_b().connect(sum->output());
 
         input_tensors_.emplace(&abs->input(), input);
-        output_tensors_.emplace(output, &sum->output());
+        input_tensors_.emplace(&div->input_a(), input);
+        output_tensors_.emplace(output, &div->output());
         break;
     }
     case 2:
@@ -57,18 +60,18 @@ void onnx_importer::convert_op_LpNormalization(const NodeProto &node)
         auto sum = graph_.emplace<reduce>(reduce_sum, square->output().shape(), reduce_axis, 0.f, true);
         auto epsilon = graph_.emplace<constant>(1e-10f);
         auto max = graph_.emplace<binary>(binary_max, sum->output().shape(), epsilon->output().shape(), value_range<float>::full());
-        auto rsqrt = graph_.emplace<unary>(unary_rsqrt, max->output().shape());
-        auto mul = graph_.emplace<binary>(binary_mul, input_shape, rsqrt->output().shape(), value_range<float>::full());
+        auto sqrt = graph_.emplace<unary>(unary_sqrt, max->output().shape());
+        auto div = graph_.emplace<binary>(binary_div, input_shape, sqrt->output().shape(), value_range<float>::full());
 
         sum->input().connect(square->output());
         max->input_a().connect(sum->output());
         max->input_b().connect(epsilon->output());
-        rsqrt->input().connect(max->output());
-        mul->input_b().connect(rsqrt->output());
+        sqrt->input().connect(max->output());
+        div->input_b().connect(sqrt->output());
 
         input_tensors_.emplace(&square->input(), input);
-        input_tensors_.emplace(&mul->input_a(), input);
-        output_tensors_.emplace(output, &mul->output());
+        input_tensors_.emplace(&div->input_a(), input);
+        output_tensors_.emplace(output, &div->output());
         break;
     }
     default:
