755cc5e5dbc1b492b507cd779775832b61bfeddc
yanghaoqi <yanghaoqi_intern@canaan-creative.com>
2021-06-25 17:05:20 +0800

    fix lstm transform

--- src/targets/neutral_target.cpp
+++ src/targets/neutral_target.cpp
@@ -31,6 +31,7 @@
 #include <nncase/transforms/neutral/fuse_unary.h>
 #include <nncase/transforms/neutral/fused_unary_to_lookup1d.h>
 #include <nncase/transforms/neutral/global_reduce_window_to_reduce.h>
+#include <nncase/transforms/neutral/lstm_transform.h>
 #include <nncase/transforms/neutral/matmul_to_conv2d.h>
 #include <nncase/transforms/neutral/quantize_motion.h>
 #include <nncase/transforms/neutral/simplify_reduce.h>
@@ -166,12 +167,19 @@ void neutral_target::register_target_independent_passes(const module_type_t &typ
 
     if (type == runtime::stackvm::stackvm_module_type)
     {
+        {
+            pass p("split lstm node");
+            p.emplace<lstm_transform>();
+            pass_mgr.add_pass(std::move(p));
+        }
+
         //matmul to conv2d
         {
             pass p("matmul_to_conv2d");
             p.emplace<matmul_to_conv2d_transform>();
             pass_mgr.add_pass(std::move(p));
         }
+
         //fold_pad_conv
         {
             pass p("fold_pad_conv");
