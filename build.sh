41bfd072b1efe6947d9a281077e8623aef369897
郑启航 <597323109@qq.com>
2021-07-01 12:49:43 +0800

    refactor cmake building system

--- build.sh
+++ build.sh
@@ -3,3 +3,23 @@ sudo update-alternatives --set gcc /usr/bin/gcc-8
 cmake .. -DNNCASE_TARGET=k210 -DCMAKE_BUILD_TYPE=Release
 make -j
 strip bin/ncc
+
+cmake -G Ninja . -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=ON -B build
+
+cmake --build build
+
+
+# install k210 toolchain
+# wget https://github.com/kendryte/kendryte-gnu-toolchain/releases/download/v8.2.0-20190409/kendryte-toolchain-ubuntu-amd64-8.2.0-20190409.tar.xz -O ../kendryte-toolchain.tar.xz
+# tar xf ../kendryte-toolchain.tar.xz -C .. && sync && export RISCV_ROOT_PATH="$(realpath ../kendryte-toolchain)"
+
+# install k210 sdk
+# wget https://github.com/kendryte/kendryte-standalone-sdk/archive/refs/heads/develop.tar.gz -O ../k210-sdk.tar.gz
+# tar -xvf ../k210-sdk.tar.gz -C .. && sync
+
+cmake -G Ninja . -DCMAKE_BUILD_TYPE=Release -DBUILDING_RUNTIME=ON -DK210_SDK_DIR=$(realpath ../kendryte-standalone-sdk-develop) -DCMAKE_TOOLCHAIN_FILE=toolchains/k210.toolchain.cmake -B out
+
+cmake --build out
+
+rm out
+rm build
--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,40 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 
 
 class nncaseConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake", "cmake_find_package", "cmake_paths"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "runtime": [True, False],
+        "tests": [True, False]
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "runtime": False,
+        "tests": False
+    }
 
     def requirements(self):
-        self.requires('flatbuffers/1.12.0')
+        self.requires('flatbuffers/2.0.0')
         self.requires('fmt/7.1.3')
         self.requires('gsl-lite/0.37.0')
-        self.requires('gtest/1.10.0')
         self.requires('lyra/1.5.0')
         self.requires('magic_enum/0.7.0')
         self.requires('nlohmann_json/3.9.1')
         self.requires('opencv/4.5.1')
-        self.requires('protobuf/3.13.0')
+        self.requires('protobuf/3.17.1')
         self.requires('pybind11/2.6.1')
         self.requires('xtensor/0.21.5')
         self.requires('mpark-variant/1.4.0')
         self.requires('spdlog/1.8.2')
+        if self.options.tests:
+            self.requires('gtest/1.10.0')
 
     def build_requirements(self):
-        self.build_requires("flatc/1.12.0")
+        pass
 
     def configure(self):
         self.options["opencv"].contrib = False
@@ -30,5 +43,19 @@ class nncaseConan(ConanFile):
         self.options["opencv"].with_eigen = False
         self.options["opencv"].with_quirc = False
         self.options["opencv"].dnn = False
+        self.options["flatbuffers"].options_from_context = False
+        self.options["xtensor"].xsimd = False
         if self.settings.os == 'Linux':
             self.options["opencv"].with_gtk = False
+
+    def cmake_configure(self):
+        cmake = CMake(self)
+        cmake.definitions['BUILDING_RUNTIME'] = self.options.runtime
+        if self.options.runtime:
+            cmake.definitions["CMAKE_CXX_STANDARD"] = 14
+        return cmake
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
