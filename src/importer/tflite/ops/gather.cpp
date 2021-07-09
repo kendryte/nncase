ca2d143feb2cd81ff1d38e2320fbce0fd3c785f3
FusionBolt <RealFusionBolt@gmail.com>
2021-07-09 15:06:03 +0800

    refactor tflite gather importer and add some test

--- src/importer/tflite/ops/gather.cpp
+++ src/importer/tflite/ops/gather.cpp
@@ -22,7 +22,10 @@ using namespace nncase;
 using namespace nncase::importer;
 using namespace nncase::ir;
 
-shape_t get_gather_nd_shape(const shape_t& input_shape, const shape_t &indices_shape, int32_t batch_dims)
+namespace
+{
+// indices_shape[:-1] + input_shape[indices_shape[-1] + batch_dims:]
+shape_t get_gather_nd_shape(const shape_t &input_shape, const shape_t &indices_shape, int32_t batch_dims)
 {
     shape_t out_shape(indices_shape.begin(), indices_shape.end() - 1);
     for (size_t i = 1 + batch_dims; i < input_shape.size(); ++i)
@@ -30,6 +33,58 @@ shape_t get_gather_nd_shape(const shape_t& input_shape, const shape_t &indices_s
         out_shape.push_back(input_shape[i]);
     }
     return out_shape;
+}
+
+axis_t get_init_perm(size_t rank)
+{
+    axis_t perm(rank);
+    std::iota(perm.begin(), perm.end(), 0);
+    return perm;
+}
+
+axis_t get_tr_perm(size_t data_rank, int32_t axis, int32_t batch_dims)
+{
+    auto perm = get_init_perm(data_rank);
+    // perm = perm[:batch_dims] + perm[axis:axis+1] + perm[batch_dims:axis] + perm[axis+1:]
+    perm[batch_dims] = axis;
+    size_t perm_index = batch_dims + 1;
+    for (auto i = batch_dims; i < axis; ++i, ++perm_index)
+    {
+        perm[perm_index] = i;
+    }
+    for (auto i = axis + 1; i < data_rank; ++i, ++perm_index)
+    {
+        perm[perm_index] = i;
+    }
+    return perm;
+}
+
+axis_t get_un_tr_perm(size_t data_rank, int32_t axis, int32_t batch_dims, const shape_t &indices_shape)
+{
+    const auto indices_rank = indices_shape.size();
+    const auto shift_amt = axis - batch_dims;
+    const auto result_rank = data_rank + indices_rank - 1 - batch_dims;
+    auto un_perm = get_init_perm(result_rank);
+    const auto j = indices_rank + shift_amt;
+    // un_perm = un_perm[:batch_dims] + un_perm[indices_rank:j] + un_perm[batch_dims:indices_rank] + un_perm[j:]
+    size_t perm_index = batch_dims + 1;
+    perm_index = batch_dims;
+    for (auto i = indices_rank; i < j; ++i, ++perm_index)
+    {
+        un_perm[perm_index] = i;
+    }
+    for (auto i = batch_dims; i < indices_rank; ++i, ++perm_index)
+    {
+        un_perm[perm_index] = i;
+    }
+    return un_perm;
+}
+
+shape_t get_after_bitcast_shape(shape_t shape)
+{
+    shape.push_back(1);
+    return shape;
+}
 }
 
 DEFINE_TFLITE_LOWER(GATHER)
@@ -52,113 +107,73 @@ DEFINE_TFLITE_LOWER(GATHER)
         axis = axis + static_cast<int32_t>(in_shape.size());
     }
 
-
-
-
-
-
-
-
-
-    input_connector *in_connector, *input_indices_connector;
-    output_connector *out_connector;
-    // TODO:when int 32
+    input_connector *g_input, *g_indices;
+    output_connector *g_output;
     if (batch_dims == 0)
     {
         auto ga = graph_.emplace<gather>(in_type, in_shape, indices_shape, out_shape, axis);
         ga->name(get_tensor(op.outputs(), 0).name()->string_view());
         link_input_tensor(&ga->input(), op.inputs()->Get(0));
-        link_input_tensor(&ga->indices(), op.inputs()->Get(1));
+        if (indices_type != dt_int32)
+        {
+            auto ct = graph_.emplace<convert>(indices_type, indices_shape, dt_int32);
+            ga->indices().connect(ct->output());
+            link_input_tensor(&ct->input(), op.inputs()->Get(1));
+        }
+        else
+        {
+            link_input_tensor(&ga->indices(), op.inputs()->Get(1));
+        }
         link_output_tensor(op.outputs()->Get(0), &ga->output());
-        // convert_indices
         return;
     }
 
-
-
-    shape_t after_bitcast_indices_shape(indices_shape);
-    after_bitcast_indices_shape.push_back(1);
-    auto bc = graph_.emplace<bitcast>(to_data_type(indices.type()), indices_shape, after_bitcast_indices_shape);
+    auto after_bitcast_indices_shape = get_after_bitcast_shape(indices_shape);
+    auto bc = graph_.emplace<bitcast>(dt_int32, indices_shape, after_bitcast_indices_shape);
 
     gather_nd *ga;
     if (indices_type != dt_int32)
     {
         auto ct = graph_.emplace<convert>(indices_type, indices_shape, dt_int32);
-        input_indices_connector = &ct->input();
+        g_indices = &ct->input();
         // indices -> convert -> bitcast
         bc->input().connect(ct->output());
     }
     else
     {
         // indices -> bitcast
-        input_indices_connector = &bc->input();
+        g_indices = &bc->input();
     }
-
 
     if (axis != batch_dims)
     {
         // before transpose
         const auto data_rank = in_shape.size();
-        axis_t perm(data_rank);
-        std::iota(perm.begin(), perm.end(), 0);
-
-        const auto indices_rank = indices_shape.size();
-        const auto shift_amt = axis - batch_dims;
-
-        // perm = perm[:batch_dims] + perm[axis:axis+1] + perm[batch_dims:axis] + perm[axis+1:]
-        perm[batch_dims] = axis;
-        size_t perm_index = batch_dims + 1;
-        for (size_t i = batch_dims; i < axis; ++i, ++perm_index)
-        {
-            perm[perm_index] = i;
-        }
-        for (size_t i = axis + 1; i < data_rank; ++i, ++perm_index)
-        {
-            perm[perm_index] = i;
-        }
-
-        auto tr = graph_.emplace<transpose>(in_type, in_shape, perm);
-
+        auto tr = graph_.emplace<transpose>(in_type, in_shape, get_tr_perm(data_rank, axis, batch_dims));
         // gather nd
         auto after_tr_shape = tr->output().shape();
         auto cast_out_shape = get_gather_nd_shape(after_tr_shape, after_bitcast_indices_shape, batch_dims);
         ga = graph_.emplace<gather_nd>(in_type, after_tr_shape, after_bitcast_indices_shape, cast_out_shape, batch_dims);
-
-
         // after transpose
-        const auto result_rank = data_rank + indices_rank - 1 - batch_dims;
-        axis_t un_perm(result_rank);
-        std::iota(un_perm.begin(), un_perm.end(), 0);
-        const auto j = indices_rank + shift_amt;
-        // un_perm = un_perm[:batch_dims] + un_perm[indices_rank:j] + un_perm[batch_dims:indices_rank] + un_perm[j:]
-        perm_index = batch_dims;
-        for (size_t i = indices_rank; i < j; ++i, ++perm_index)
-        {
-            un_perm[perm_index] = i;
-        }
-        for (size_t i = batch_dims; i < indices_rank; ++i, ++perm_index)
-        {
-            un_perm[perm_index] = i;
-        }
-        auto un_tr = graph_.emplace<transpose>(in_type, cast_out_shape, un_perm);
-
+        auto un_tr = graph_.emplace<transpose>(in_type, cast_out_shape, get_un_tr_perm(data_rank, axis, batch_dims, indices_shape));
         // connect node
-        in_connector = &tr->input();
+        // input -> transpose -> gather_nd -> transpose -> output
+        g_input = &tr->input();
         ga->input().connect(tr->output());
         un_tr->input().connect(ga->output());
-        out_connector = &un_tr->output();
+        g_output = &un_tr->output();
     }
     else
     {
         auto bitcast_output_shape = get_gather_nd_shape(in_shape, after_bitcast_indices_shape, batch_dims);
         ga = graph_.emplace<gather_nd>(in_type, in_shape, after_bitcast_indices_shape, bitcast_output_shape, batch_dims);
-        in_connector = &ga->input();
-        out_connector = &ga->output();
+        g_input = &ga->input();
+        g_output = &ga->output();
     }
 
     ga->indices().connect(bc->output());
-    link_input_tensor(in_connector, op.inputs()->Get(0));
-    link_input_tensor(input_indices_connector, op.inputs()->Get(1));
-    link_output_tensor(op.outputs()->Get(0), out_connector);
+    link_input_tensor(g_input, op.inputs()->Get(0));
+    link_input_tensor(g_indices, op.inputs()->Get(1));
+    link_output_tensor(op.outputs()->Get(0), g_output);
     ga->name(get_tensor(op.outputs(), 0).name()->string_view());
 }
--- tests/importer/tflite/basic/test_gather_nd.py
+++ tests/importer/tflite/basic/test_gather_nd.py
@@ -19,34 +19,40 @@ import tensorflow as tf
 import numpy as np
 from test_runner import TfliteTestRunner
 
-def _make_module(in_shape, indice):
+def _make_module(in_shape, indice, batch_dims):
     class GatherModule(tf.Module):
         def __init__(self):
             super(GatherModule).__init__()
 
         @tf.function(input_signature=[tf.TensorSpec(in_shape, tf.float32)])
         def __call__(self, x):
-            return tf.gather_nd(x, indice, batch_dims=0)
+            return tf.gather_nd(x, indice, batch_dims=batch_dims)
     return GatherModule()
 
 
-in_shape_indice = [
-    ([11], [[0], [7], [5]]),
-    ([3, 5], [[[0, 2], [0, 4]]]),
-    ([2, 3, 1], [[0], [1]]),
-    ([2, 3, 1], [[[0], [0], [0]], [[0], [0], [0]]]),
-    ([5, 7, 5], [1, 4, 3]),
-    ([2, 2, 2], [[0, 1], [1, 0]]),
-    ([2, 2, 2], [[1], [0]]),
-    ([5, 4, 3, 2], [[1, 0, 2], [1, 2, 2]]),
-    ([5, 5, 7, 7], [[1, 2, 3], [1, 2, 3]]),
-    ([5, 5, 7, 7], [[1, 2, 3, 1], [1, 2, 3, 1]]),
-    ([5, 4, 3, 2], [[1, 0, 2], [1, 2, 2]]),
+in_shape_indices_batch_dims = [
+    ([11], [[0], [7], [5]], 0),
+    ([3, 5], [[[0, 2], [0, 4]]], 0),
+    ([3, 5], [[1], [4], [3]], 1),
+    ([2, 3, 1], [[0], [1]], 0),
+    ([2, 3, 1], [[[0], [0], [0]], [[0], [0], [0]]], 0),
+    ([5, 7, 5], [1, 4, 3], 0),
+    ([2, 3, 5], [[0, 1], [1, 0]], 0),
+    ([2, 3, 5], [[[0, 4]], [[2, 0]]], 1),
+    ([2, 3, 5], [[[4], [3], [0]], [[2], [1], [0]]], 2),
+    ([5, 4, 3, 2], [[1, 0, 2], [1, 2, 2]], 0),
+    ([5, 5, 7, 7], [[1, 2, 3], [1, 2, 3]], 0),
+    ([5, 5, 7, 7], [[1, 2, 3, 1], [1, 2, 3, 1]], 0),
+    ([5, 4, 3, 2], [[1, 0, 2], [1, 2, 2]], 0),
+    ([2, 4, 3, 5], [[1, 0, 2], [1, 2, 2]], 1),
+    ([2, 3, 3, 5], [[[2, 1], [0, 1], [1, 0]], [[0, 1], [2, 2], [1, 1]]], 2),
+    ([2, 3, 3, 5], [[[[4], [1], [3]], [[2], [0], [1]], [[4], [2], [3]]],
+                    [[[3], [1], [4]], [[1], [0], [2]], [[3], [2], [4]]]], 3)
 ]
 
-@pytest.mark.parametrize('in_shape,indice', in_shape_indice)
-def test_gather_nd(in_shape, indice, request):
-    module = _make_module(in_shape, indice)
+@pytest.mark.parametrize('in_shape,indices,batch_dims', in_shape_indices_batch_dims)
+def test_gather_nd(in_shape, indices, batch_dims, request):
+    module = _make_module(in_shape, indices, batch_dims)
     runner = TfliteTestRunner(request.node.name)
     model_file = runner.from_tensorflow(module)
     runner.run(model_file)
