eb9555ab86b03fc8ec9f7db29a85ef0e1a262de4
zhangyang2057 <83401373+zhangyang2057@users.noreply.github.com>
2021-07-23 10:27:42 +0800

    Fix offset issue of bitcast and concat. (#296)
    * Fix begin shape issue of bitcast.
    
    * Add bfs visitor and transfer bitcast begin.
    
    * Move concat into pass to handle.
    
    * Fix clang format.
    
    * Rename bfs_ir_visitor as bfs_ir_pre_order_visitor.

--- include/nncase/transforms/neutral/optimize_allocation.h
+++ include/nncase/transforms/neutral/optimize_allocation.h
@@ -80,12 +80,12 @@ protected:
     void run_core(graph &graph, nncase::target &target, const run_pass_options &options) override;
 };
 
-//class NNCASE_API alias_concat_buffer_pass : public graph_pass
-//{
-//public:
-//    using graph_pass::graph_pass;
-//
-//protected:
-//    void run_core(graph &graph, nncase::target &target, const run_pass_options &options) override;
-//};
+class NNCASE_API alias_concat_buffer_pass : public graph_pass
+{
+public:
+    using graph_pass::graph_pass;
+
+protected:
+    void run_core(graph &graph, nncase::target &target, const run_pass_options &options) override;
+};
 }
