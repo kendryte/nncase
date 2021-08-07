e7c8b7200b08778f7ef20768ed57a234448f2611
sunnycase <sunnycase@live.cn>
2021-08-07 15:03:34 +0800

    Fix workflow

--- modules/k210/cmake/kendryte_lib.cmake
+++ modules/k210/cmake/kendryte_lib.cmake
@@ -3,10 +3,11 @@ function(target_link_kendryte TARGET)
     get_target_property(TARGET_TYPE ${TARGET} TYPE)
     if (TARGET_TYPE STREQUAL "EXECUTABLE")
         get_target_property(TARGET_OUT_NAME ${TARGET} OUTPUT_NAME)
-        set(BIN_NAME $<TARGET_FILE_PREFIX:${TARGET}>$<TARGET_FILE_PREFIX:${TARGET}>$<TARGET_FILE_BASE_NAME:${TARGET}>.bin)
+        set(BIN_NAME $<TARGET_FILE_DIR:${TARGET}>/$<TARGET_FILE_PREFIX:${TARGET}>$<TARGET_FILE_PREFIX:${TARGET}>$<TARGET_FILE_BASE_NAME:${TARGET}>.bin)
         add_custom_command(TARGET ${TARGET} POST_BUILD
-            COMMAND ${CMAKE_OBJCOPY} --output-format=binary $<TARGET_FILE:${TARGET}> $<TARGET_FILE_DIR:${TARGET}>/${BIN_NAME}
+            COMMAND ${CMAKE_OBJCOPY} --output-format=binary $<TARGET_FILE:${TARGET}> ${BIN_NAME}
             DEPENDS ${TARGET}
             COMMENT "Generating .bin file for ${TARGET}...")
+        install(FILES ${BIN_NAME} DESTINATION bin)
     endif()
 endfunction()
