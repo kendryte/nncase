dc2ae927d7c9d86fa9b4bbec0aa9f845ecbeb9d1
zhangjizhao <zhangjizhao@canaan-creative.com>
2021-08-18 15:02:50 +0800

    add w_quant_type, just for k230 now

--- targets/k210/k210_target.cpp
+++ targets/k210/k210_target.cpp
@@ -120,7 +120,7 @@ void k210_target::register_quantize_annotation_passes(const module_type_t &type,
     }
 }
 
-void k210_target::register_quantize_passes(const module_type_t &type, ir::transforms::pass_manager &pass_mgr, [[maybe_unused]] datatype_t quant_type)
+void k210_target::register_quantize_passes(const module_type_t &type, ir::transforms::pass_manager &pass_mgr, [[maybe_unused]] datatype_t quant_type, [[maybe_unused]] datatype_t w_quant_type)
 {
     {
         transform_pass p("lowering_kpu_conv2d");
@@ -141,7 +141,7 @@ void k210_target::register_quantize_passes(const module_type_t &type, ir::transf
         pass_mgr.add_pass(std::move(p));
     }
     {
-        neutral_target::register_quantize_passes(type, pass_mgr, quant_type);
+        neutral_target::register_quantize_passes(type, pass_mgr, quant_type, w_quant_type);
 
         transform_pass p("fold_kpu_data_exchg2");
         //p.emplace<fuse_kpu_download_transform>();
