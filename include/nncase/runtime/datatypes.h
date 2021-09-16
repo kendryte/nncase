ec371657bc5a4d72402244a65cf66b008a7bc125
FusionBolt <FusionBolt@users.noreply.github.com>
2021-09-16 11:07:44 +0000

    apply code-format changes

--- include/nncase/runtime/datatypes.h
+++ include/nncase/runtime/datatypes.h
@@ -41,7 +41,7 @@ typedef enum _datatype : uint8_t
 inline datatype_t parse_datatype_str([[maybe_unused]] std::string_view name)
 {
 #define DEFINE_DATATYPE(id, t, n, value) \
-    if (name == ""#id"")                 \
+    if (name == "" #id "")               \
         return datatype_t::dt_##id;
 #include "datatypes.def"
 #undef DEFINE_DATATYPE
