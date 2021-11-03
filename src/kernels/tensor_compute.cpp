fb724b80ddafe5604206ca1464768d3998d58d3d
zhangyang2057 <zhangyang@canaan-creative.com>
2021-11-03 13:37:56 +0800

    Fix build issue on Windows.

--- src/kernels/tensor_compute.cpp
+++ src/kernels/tensor_compute.cpp
@@ -276,7 +276,7 @@ result<void> kernels::hardmax(const T *input, const runtime_shape_t &in_shape, c
 template result<void> kernels::random_normal<float>(float *output, const runtime_shape_t &out_shape, float mean, float std, float seed) noexcept;
 
 template <typename T>
-NNCASE_API result<void> kernels::random_normal(T *output, const runtime_shape_t &out_shape, float mean, float std, float seed) noexcept
+result<void> kernels::random_normal(T *output, const runtime_shape_t &out_shape, float mean, float std, float seed) noexcept
 {
     return cpu::reference::random_normal(output, out_shape, mean, std, seed);
 }
