723274f62fbd2e0e7e5c5d606c654dad79e20ecc
FusionBolt <RealFusionBolt@gmail.com>
2021-11-11 13:45:50 +0800

    add concat type infer

--- src/Nncase.IR/IR/Tensors/Concat.cs
+++ src/Nncase.IR/IR/Tensors/Concat.cs
@@ -18,17 +18,65 @@ namespace Nncase.IR.Tensors
         /// <summary>
         /// Gets input.
         /// </summary>
-        public static readonly ParameterInfo Input = new(typeof(Concat), 0, "input");
+        public static readonly ParameterInfo Input = new(typeof(Concat), 0, "inputs");
 
         /// <summary>
         /// Gets axis.
         /// </summary>
         public static readonly ParameterInfo Axis = new(typeof(Concat), 1, "axis");
 
-        /// <inheritdoc/>
-        public IRType InferInvokeResultType(ITypeInferenceContext context)
+
+        // axis: if one of inputs shape[axis] is unknown
+        // then dims axis is known
+        // else get sum of dims
+        private Dimension AxisDim(TupleType inputs, int axisValue)
         {
-            throw new NotImplementedException();
+            var allAxisDimIsFixed = inputs.Fields.Aggregate(
+                true,
+                (prod, next) => prod && (next as TensorType).Shape[axisValue].IsFixed);
+            if (allAxisDimIsFixed)
+            {
+                return inputs.Fields.Aggregate(
+                    0,
+                    (prod, next) => prod + (next as TensorType).Shape[axisValue].FixedValue);
+            }
+            else
+            {
+                return Dimension.Unknown;
+            }
+        }
+        
+        /// <inheritdoc/>
+        public IRType InferInvokeResultType(ITypeInferenceContext context, TupleType inputs, TensorType axis)
+        {
+            var input0 = inputs[0] as TensorType;
+            InvalidType? invalidType = null;
+            var axisValue = (context.GetArgument(this, Axis) as Const).ToScalar<int>();
+            var shapeValue = Enumerable.Range(0, input0.Shape.Rank).Select(i =>
+            {
+                if (i == axisValue)
+                {
+                    return AxisDim(inputs, axisValue);
+                }
+                // if all input shape[dim] is not same, return invalid
+                else
+                {
+                    var allAxisDimIsSame = inputs.Fields.Aggregate(
+                        true,
+                        (prod, next) => prod && (next as TensorType).Shape[i].IsFixed);
+                    if (allAxisDimIsSame)
+                    {
+                        return (inputs[0] as TensorType).Shape[0];
+                    }
+                    else
+                    {
+                        invalidType = new InvalidType("Concat dims that except the shape of axis dim are different");
+                        return Dimension.Unknown;
+                    }
+                }
+            });
+            var shape = new Shape(shapeValue);
+            return (IRType) invalidType?? new TensorType(input0.DType, shape);
         }
     }
 }
