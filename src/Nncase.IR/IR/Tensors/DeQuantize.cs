1284c28e5401ab40e6b149dedc1fcff7d973b026
FusionBolt <RealFusionBolt@gmail.com>
2021-11-11 17:19:07 +0800

    add dequantize type infer

--- src/Nncase.IR/IR/Tensors/DeQuantize.cs
+++ src/Nncase.IR/IR/Tensors/DeQuantize.cs
@@ -12,11 +12,21 @@ namespace Nncase.IR.Tensors
 
         public static ParameterInfo ZeroPoint = new(typeof(DeQuantize), 1, "zero_point");
         
-        public static ParameterInfo Scale = new(typeof(DeQuantize), 1, "scale");
+        public static ParameterInfo Scale = new(typeof(DeQuantize), 2, "scale");
 
         /// <inheritdoc/>
         public IRType InferInvokeResultType(ITypeInferenceContext context, TensorType input, TensorType zero_point, TensorType scale)
         {
+            if (!ZeroPoint.CheckType(zero_point))
+            {
+                return new InvalidType("DeQuantize zero_point must be scalar");
+            }
+
+            if (!Scale.CheckType(scale))
+            {
+                return new InvalidType("DeQuantize scale must be scalar");
+            }
+            
             return new TensorType(TargetType, input.Shape);
         }
     }
