6baef3a0915284daca7b4a50fe1a6ad7b956e217
FusionBolt <RealFusionBolt@gmail.com>
2021-11-11 17:18:49 +0800

    add broadcast type infer

--- src/Nncase.IR/IR/Tensors/Broadcast.cs
+++ src/Nncase.IR/IR/Tensors/Broadcast.cs
@@ -26,9 +26,27 @@ namespace Nncase.IR.Tensors
         public static readonly ParameterInfo Shape = new(typeof(Broadcast), 1, "shape");
 
         /// <inheritdoc/>
-        public IRType InferInvokeResultType(ITypeInferenceContext context)
+        public IRType InferInvokeResultType(ITypeInferenceContext context, TensorType input, TensorType shape)
         {
-            throw new NotImplementedException();
+            var shapeValue = context.GetArgument(this, Shape);
+            if (shapeValue is Const && input.Shape.IsFixed)
+            {
+                for (var i = input.Shape.Rank; i > 0; --i)
+                {
+                    var shapeShapeValue = shape.Shape[shape.Shape.Rank - i].FixedValue;
+                    var inputShapeValue = input.Shape[input.Shape.Rank - i].FixedValue;
+                    if (shapeShapeValue != inputShapeValue && inputShapeValue != 1)
+                    {
+                        return new InvalidType("Broadcast input shape and param shape is not match");
+                    }
+                }
+
+                return shape;
+            }
+            else
+            {
+                return new InvalidType("Broadcast shape is unknown");
+            }
         }
     }
 }
