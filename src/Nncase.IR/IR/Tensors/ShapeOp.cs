d13cab38e2fbd8d1831819935784783f5f84a9bb
FusionBolt <RealFusionBolt@gmail.com>
2021-11-29 15:13:00 +0800

    fix shape op type infer

--- src/Nncase.IR/IR/Tensors/ShapeOp.cs
+++ src/Nncase.IR/IR/Tensors/ShapeOp.cs
@@ -23,7 +23,7 @@ namespace Nncase.IR.Tensors
         /// <inheritdoc/>
         public IRType InferInvokeResultType(ITypeInferenceContext context, TensorType input)
         {
-            return new TensorType(DataType.Int32, new Shape(new[] { 1 }));
+            return new TensorType(DataType.Int32, new Shape(input.Shape.Rank));
         }
     }
 }
