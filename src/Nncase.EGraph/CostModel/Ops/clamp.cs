bb2b51a1187c2919905e647003c86e599b2f713f
curioyang <curioyhq@gmail.com>
2021-12-28 10:22:27 +0800

    update comment

--- src/Nncase.EGraph/CostModel/Ops/clamp.cs
+++ src/Nncase.EGraph/CostModel/Ops/clamp.cs
@@ -9,16 +9,14 @@ namespace Nncase.CostModel
     public sealed partial class ExprCostModelVisitor
     {
         /// <summary>
-        /// Compute binary cost by broadcast rule, if can broadcast the min dimension will have more memory loctiaity, so arithm should reduce.
+        /// Compute clamp cost ，size * 2
         /// </summary>
         /// <param name="binary"></param>
         /// <returns></returns>
         private Cost VisitClamp(Clamp clamp)
         {
-            // todo:broadcast
             var shape = _context.GetArgumentType(clamp, Clamp.Input) as TensorType;
             var arithm = (shape.Shape.Prod() * 2).FixedValue;
-            // var rhsValue = _context.GetArgumentConst(binary, Binary.Rhs);
             return new Cost(arithm);
         }
     }
