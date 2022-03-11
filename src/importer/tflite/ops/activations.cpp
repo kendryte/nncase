2de314520d3f7ac3d5a44ac0cd6d55f09fae3f33
huochenghai <huochenghai@canaan-creative.com>
2022-03-11 16:39:11 +0800

    tflite hardswish importer

--- src/importer/tflite/ops/activations.cpp
+++ src/importer/tflite/ops/activations.cpp
@@ -156,3 +156,56 @@ DEFINE_TFLITE_LOWER(LEAKY_RELU)
         link_output_tensor(op.outputs()->Get(0), &max->output());
     }
 }
+
+DEFINE_TFLITE_LOWER(HARD_SWISH)
+{
+    auto &input = get_tensor(op.inputs(), 0);
+    [[maybe_unused]] auto &output = get_tensor(op.outputs(), 0);
+    [[maybe_unused]] auto &options = *op.builtin_options_as_HardSwishOptions();
+    auto in_shape = get_shape(input.shape());
+    auto input_type = to_data_type(input.type());
+
+    // y = x * max(0, min(1, alpha * x + beta)) where alpha = 1/6 and beta = 0.5
+    const auto &alpha = graph_.emplace<constant>(1.0f / 6);
+    alpha->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/alpha");
+    auto mul_1 = graph_.emplace<binary>(binary_mul, input_type, in_shape, alpha->output().shape(), value_range<float>::full());
+    mul_1->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/mul_1");
+
+    const auto &beta = graph_.emplace<constant>(0.5f);
+    beta->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/beta");
+    auto add = graph_.emplace<binary>(binary_add, input_type, mul_1->output().shape(), beta->output().shape(), value_range<float>::full());
+    add->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/add");
+
+    auto one = graph_.emplace<constant>(1.f);
+    one->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/one");
+    auto min = graph_.emplace<binary>(binary_min, input_type, add->output().shape(), one->output().shape(), value_range<float>::full());
+    min->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/min");
+
+    auto zero = graph_.emplace<constant>(0.f);
+    zero->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/zero");
+    auto max = graph_.emplace<binary>(binary_max, input_type, min->output().shape(), zero->output().shape(), value_range<float>::full());
+    max->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/max");
+
+    auto mul_2 = graph_.emplace<binary>(binary_mul, input_type, in_shape, max->output().shape(), value_range<float>::full());
+    mul_2->name(std::string(get_tensor(op.outputs(), 0).name()->string_view()) + "/mul_2");
+
+    if (input.type() != tflite::TensorType_FLOAT32)
+    {
+        // TODO: support quantization
+    }
+    else
+    {
+        mul_1->input_b().connect(alpha->output());
+        add->input_a().connect(mul_1->output());
+        add->input_b().connect(beta->output());
+        min->input_a().connect(add->output());
+        min->input_b().connect(one->output());
+        max->input_a().connect(min->output());
+        max->input_b().connect(zero->output());
+        mul_2->input_b().connect(max->output());
+
+        link_input_tensor(&mul_1->input_a(), op.inputs()->Get(0));
+        link_input_tensor(&mul_2->input_a(), op.inputs()->Get(0));
+        link_output_tensor(op.outputs()->Get(0), &mul_2->output());
+    }
+}
