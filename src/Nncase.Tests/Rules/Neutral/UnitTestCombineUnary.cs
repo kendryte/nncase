d0c9ba25196969af9752dbd551edd7f0926867ce
curioyang <curioyhq@gmail.com>
2022-04-12 15:01:33 +0800

    add combineUnary test

--- src/Nncase.Tests/Rules/Neutral/UnitTestCombineUnary.cs
+++ src/Nncase.Tests/Rules/Neutral/UnitTestCombineUnary.cs
@@ -25,142 +25,95 @@ using Tuple = System.Tuple;
 
 namespace Nncase.Tests.Rules.NeutralTest;
 
-public class UnitTestCombineTranspose
+public class UnitTestCombineUnary
 {
     private readonly RunPassOptions passOptions;
 
-    public UnitTestCombineTranspose()
+    public UnitTestCombineUnary()
     {
         passOptions = new RunPassOptions(null, 3, Testing.GetDumpDirPath(this.GetType()));
     }
 
-    public static IEnumerable<object[]> TestCombineTransposeBinaryPositiveData =>
+    // TODO: CombinePadUnary
+    public static IEnumerable<object[]> TestCombinePadUnaryPositiveData =>
         new[]
         {
-            new object[] {new[] {4}, new[] {4}, new[] {0}},
-            new object[] {new[] {4, 4}, new[] {4, 4}, new[] {1, 0}},
-            new object[] {new[] {1, 3, 4}, new[] {1, 3, 4}, new[] {0, 2, 1}},
-            new object[] {new[] {1, 3, 2, 4}, new[] {1, 3, 2, 4}, new[] {0, 2, 3, 1}},
-            // new object[] {new[] {5, 4, }, new[] {4, 6}, new[] {1, 0}},
+            // TODO : pad support change padMode
+            // new object[] { UnaryOp.Exp, new[] {1, 3, 4, 5},  new[,] {{ 1, 1 }, { 2, 2 }, { 1, 1 }, { 3, 3 }}, PadMode.Symmetric, 0f},
+            // new object[] { UnaryOp.Abs, new[] {1, 3, 4, 5},  new[,] {{ 1, 1 }, { 2, 2 }, { 1, 1 }, { 3, 3 }}, PadMode.Reflect, 0f},
+            new object[] { UnaryOp.Exp, new[] {1, 3, 4, 5},  new[,] {{ 1, 1 }, { 2, 2 }, { 1, 1 }, { 3, 3 }},0f},
+            new object[] { UnaryOp.Abs, new[] {1, 3, 4, 5},  new[,] {{ 1, 1 }, { -1, -1 }, { 1, 1 }, { 3, 3 }},0f},
+            new object[] { UnaryOp.Floor, new[] {1, 3, 4, 5},  new[,] {{ 1, 1 }, { 0, 0 }, { 1, 1 }, { 0, 0 }},0f},
         };
 
     [Theory]
-    [MemberData(nameof(TestCombineTransposeBinaryPositiveData))]
-    public void TestCombineTransposeBinaryPositive(int[] lShape, int[] rShape, int[] perm)
+    [MemberData(nameof(TestCombinePadUnaryPositiveData))]
+    public void TestCombinePadUnaryPositive(UnaryOp opType, int[] inShape, int[,] paddings, float padValue)
     {
-        var caseOptions = passOptions.IndentDir($"{(perm.Length)}D_binary_motion");
+        var caseOptions = passOptions.IndentDir($"{(inShape.Length)}D_{opType}_pad_motion");
         var a = new Var();
-        var b = new Var();
-
         var Normal = new Dictionary<Var, IValue>();
-
-        Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, lShape).Evaluate());
-        Normal.Add(b, Random.Normal(DataTypes.Float32, 0, 1, 0, rShape).Evaluate());
-
-        // TODO: Rely cse to fold same constant. 
-        var rootPre = Math.Binary(BinaryOp.Add, Tensors.Transpose(a, perm), Tensors.Transpose(b, perm));
+        Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, inShape).Evaluate());
+        var rootPre = IR.F.Math.Unary(opType, Pad(a, paddings, PadMode.Constant, padValue));
         var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
         {
-            // new FoldConstCall(),
-            new CombineTransposeBinary(),
+            new CombinePadUnary(),
         }, caseOptions);
 
         Assert.NotEqual(rootPre, rootPost);
         Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
     }
-    
-    public static IEnumerable<object[]> TestCombineTransposeConcatPositiveData =>
+
+    // TODO: CombineSliceUnary
+    public static IEnumerable<object[]> TestCombineSliceUnaryPositiveData =>
         new[]
         {
-            // new object[] {new[] {4, 4}, new[] {1, 0}, 1, 2},
-            new object[] {new[] {1, 3, 4}, new[] {0, 2, 1}, 1, 6},
-            // new object[] {new[] {1, 3, 2, 4}, new[] {0, 2, 3, 1}, 2, 1},
+            new object[] { UnaryOp.Exp, new[] {6},  new[] {0}, new[] {4}, new[] {0}, new[] {1}},
+            new object[] { UnaryOp.Abs, new[] {4, 5},  new[] {0, 0}, new[] {2, 3}, new[] {0,1}, new[] {1, 2}},
+            new object[] { UnaryOp.Sqrt, new[] {3, 4, 5},  new[] {0, 0, 1}, new[] {2, 3, 4}, new[] { 0, 1, 2}, new[] {1, 2, 3}},
+            new object[] { UnaryOp.Square, new[] {3,2, 4, 5},  new[] {0, 0,1, 1}, new[] {-1, 2, 3, 4}, new[] {-4, 1, 2, 3 }, new[] {1, 2, 3, 2}},
         };
 
     [Theory]
-    [MemberData(nameof(TestCombineTransposeConcatPositiveData))]
-    public void TestCombineTransposeConcatPositive(int[] inShape, int[] perm, int axis, int concatNum)
-    {
-        var caseOptions = passOptions.IndentDir($"{(perm.Length)}D_concat_motion");
-        var inputList = new List<Var>();
-        for(int i = 0;  i < concatNum; i++)
-        {
-            inputList.Add(new Var());
-        }
-        var Normal = new Dictionary<Var, IValue>();
-        var tpList = new List<Call>();
-        foreach (Var a in inputList)
-        {
-            // TODO:  Rely type infer and cse
-            // Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, inShape).Evaluate());
-            // tpList.Add(Tensors.Transpose(a, perm));
-            var b = Random.Normal(DataTypes.Float32, 0, 1, 0, inShape);
-            tpList.Add(Tensors.Transpose(b, perm ));
-        }
-
-        var input = Enumerable.Range(0, concatNum).Select(i => tpList[i]);
-        var rootPre = Tensors.Concat(new IR.Tuple(input), axis );
-        var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
-        {
-            new CombineTransposeConcat(),
-            // Should not open constant fold.
-            new FoldConstCall(),
-        }, caseOptions);
-
-        Assert.NotEqual(rootPre, rootPost);
-        Assert.Equal(CompilerServices.Evaluate(rootPre), CompilerServices.Evaluate(rootPost));
-        // Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
-    }
-    
-    // TODO? ??TestCombineTransposePadPositiveData
-    
-    public static IEnumerable<object[]> TestCombineTransposeReducePositiveData =>
-        new[]
-        {
-            new object[] {new[] {1, 3, 4}, new[] {0, 2, 1}, 1, 0, false},
-            new object[] {new[] {1, 3, 4, 5}, new[] {0, 2, 3, 1}, 2, 1, true},
-        };
-
-    [Theory]
-    [MemberData(nameof(TestCombineTransposeReducePositiveData))]
-    public void TestCombineTransposeReducePositive(int[] inShape, int[] perm, int axis, int initValue, bool keepDims)
-    {
-        var caseOptions = passOptions.IndentDir($"{(perm.Length)}D_reduce_motion");
+    [MemberData(nameof(TestCombineSliceUnaryPositiveData))]
+    public void TestCombineSliceUnaryPositive(UnaryOp opType, int[] inShape, int[] begins, int[] ends, int[] axes, int[] strides)
+    { 
+        var caseOptions = passOptions.IndentDir($"{(inShape.Length)}D_{opType}_Slice_motion");
         var a = new Var();
         var Normal = new Dictionary<Var, IValue>();
         Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, inShape).Evaluate());
-        var rootPre = IR.F.Tensors.Reduce(ReduceOp.Mean, Tensors.Transpose(a, perm), axis, initValue, keepDims);
+        var rootPre = IR.F.Math.Unary(opType, Tensors.Slice(a, begins, ends, axes, strides));
         var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
         {
-            new CombineTransposeReduce(),
+            new CombineSliceUnary(),
         }, caseOptions);
 
         Assert.NotEqual(rootPre, rootPost);
         Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
     }
-    
-    // TODO : CombineTransposeUnary
-    public static IEnumerable<object[]> TestCombineTransposeUnaryPositiveData =>
+
+    // TODO: CombineReshapeUnary
+    public static IEnumerable<object[]> TestCombineReshapeUnaryPositiveData =>
         new[]
         {
-            new object[] { UnaryOp.Exp, new[] {1, 3, 4}, new[] {0, 2, 1}},
-            new object[] { UnaryOp.Sqrt, new[] {1, 3, 4}, new[] {0, 2, 1}},
-            new object[] { UnaryOp.Log, new[] {1, 3, 4, 5}, new[] {0, 2, 3, 1}},
-            new object[] { UnaryOp.Abs, new[] {1, 3, 4, 5}, new[] {0, 2, 3, 1}},
+            new object[] { UnaryOp.Exp, new[] {6}, new[] {2,3}},
+            new object[] { UnaryOp.Abs, new[] {4, 5}, new[] {2, 2, 5}, },
+            new object[] { UnaryOp.Sqrt, new[] {3, 4, 5}, new[] {3, 2, 2, 5} },
+            new object[] { UnaryOp.Square, new[] {3,2, 4, 5}, new[] {3,4, 1, 10}  },
         };
 
     [Theory]
-    [MemberData(nameof(TestCombineTransposeUnaryPositiveData))]
-    public void TestCombineTransposeUnaryPositive(UnaryOp opType, int[] inShape, int[] perm)
-    {
-        var caseOptions = passOptions.IndentDir($"{(perm.Length)}D_{opType}_unary_motion");
+    [MemberData(nameof(TestCombineReshapeUnaryPositiveData))]
+    public void TestCombineReshapeUnaryPositive(UnaryOp opType, int[] inShape, int[] outShape)
+    { 
+        var caseOptions = passOptions.IndentDir($"{(inShape.Length)}D_{opType}_Reshape_motion");
         var a = new Var();
         var Normal = new Dictionary<Var, IValue>();
         Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, inShape).Evaluate());
-        var rootPre = IR.F.Math.Unary(opType, Tensors.Transpose(a, perm));
+        var rootPre = IR.F.Math.Unary(opType, Tensors.Reshape(a, outShape));
         var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
         {
-            new CombineTransposeUnary(),
+            new CombineReshapeUnary(),
         }, caseOptions);
 
         Assert.NotEqual(rootPre, rootPost);
