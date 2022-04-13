b90015201edc14765a4a75696373a1f1c9cb5c6a
curioyang <curioyhq@gmail.com>
2022-04-13 16:58:15 +0800

    add SimplifyBinary test

--- src/Nncase.Tests/Rules/Neutral/UnitTestSimplifyBinary.cs
+++ src/Nncase.Tests/Rules/Neutral/UnitTestSimplifyBinary.cs
@@ -18,70 +18,118 @@ using Random = Nncase.IR.F.Random;
 
 namespace Nncase.Tests.Rules.NeutralTest;
 
-public class UnitTestFoldBinary
+public class UnitTestSimplifyBinary
 {
     private readonly RunPassOptions passOptions;
 
-    public UnitTestFoldBinary()
+    public UnitTestSimplifyBinary()
     {
         passOptions = new RunPassOptions(null, 3, Testing.GetDumpDirPath(this.GetType()));
     }
 
-    public static IEnumerable<object[]> TestFoldNopBinaryNegativeData =>
+    public static IEnumerable<object[]> TestReassociateMulPositiveData =>
         new[]
         {
-            new object[] {BinaryOp.Add, new[] {3}, 1f},
-            new object[] {BinaryOp.Sub, new[] {3, 4}, 1f},
-            new object[] {BinaryOp.Mul, new[] {3}, 2f},
-            new object[] {BinaryOp.Div, new[] {3}, 2f},
-            // new object[] { BinaryOp.Mod ,new[] { 3}, 2f},
-            new object[] {BinaryOp.Pow, new[] {3},2f},
-        }.Select((o, i) => o.Concat(new object[] {i}).ToArray());
+            new object[] {new[] {3}},
+        }.Select((o, i) => o.Concat(new object[] { i }).ToArray());
 
     [Theory]
-    [MemberData(nameof(TestFoldNopBinaryNegativeData))]
-    public void TestFoldNopBinaryNegative(BinaryOp binaryOp, int[] aShape, float bValue, int index)
+    [MemberData(nameof(TestReassociateMulPositiveData))]
+    public void TestReassociateMulPositive(int[] aShape, int index)
     {
-        var caseOptions = passOptions.IndentDir($"Negative_case_{index}");
+        var caseOptions = passOptions.IndentDir($"TestReassociateMulPositive_case_{index}");
         var a = new Var();
+        var b = new Var();
+        var c = new Var();
         var Normal = new Dictionary<Var, IValue>();
         Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
+        Normal.Add(b, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
+        Normal.Add(c, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
 
-        var rootPre = Math.Binary(binaryOp, Math.Binary(binaryOp, a, bValue), bValue);
+        var rootPre = a * b * c; //Math.Binary(binaryOp, Math.Binary(binaryOp, a, bValue), bValue);
         var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
         {
-            new FoldNopBinary(),
+            new ReassociateMul(),
         }, caseOptions);
         // rootPre.InferenceType();
-        Assert.Equal(rootPre, rootPost);
+        Assert.NotEqual(rootPre, rootPost);
         Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
     }
 
-    public static IEnumerable<object[]> TestFoldNopBinaryPositiveData =>
+    public static IEnumerable<object[]> TestReassociateDivPositiveData =>
         new[]
         {
-            new object[] {BinaryOp.Add, new[] {3}, 0f},
-            new object[] {BinaryOp.Sub, new[] {3, 4}, 0f},
-            new object[] {BinaryOp.Mul, new[] {3}, 1f},
-            new object[] {BinaryOp.Div, new[] {3}, 1f},
-            // new object[] { BinaryOp.Mod ,new[] { 3}, 1f},
-            new object[] {BinaryOp.Pow, new[] {3}, 1f},
-        }.Select((o, i) => o.Concat(new object[] {i}).ToArray());
+            new object[] {new[] {3}},
+        }.Select((o, i) => o.Concat(new object[] { i }).ToArray());
 
     [Theory]
-    [MemberData(nameof(TestFoldNopBinaryPositiveData))]
-    public void TestFoldNopBinaryPositive(BinaryOp binaryOp, int[] aShape, float bValue, int index)
+    [MemberData(nameof(TestReassociateDivPositiveData))]
+    public void TestReassociateDivPositive(int[] aShape, int index)
     {
-        var caseOptions = passOptions.IndentDir($"Positive_case_{index}");
+        var caseOptions = passOptions.IndentDir($"TestReassociateDivPositive_case_{index}");
+        var a = new Var();
+        var b = new Var();
+        var c = Random.Normal(DataTypes.Float32, 0, 1, 0, aShape); // Can't get Var's datatype. Pattern will not pass 
+        var Normal = new Dictionary<Var, IValue>();
+        Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
+        Normal.Add(b, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
+
+        var rootPre = (a * b) / c;
+        var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
+        {
+            new ReassociateDiv(),
+        }, caseOptions);
+        Assert.NotEqual(rootPre, rootPost);
+        Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
+    }
+
+
+    public static IEnumerable<object[]> TestXDivXPositiveData =>
+        new[]
+        {
+            new object[] {new[] {3}},
+        }.Select((o, i) => o.Concat(new object[] { i }).ToArray());
+
+    [Theory]
+    [MemberData(nameof(TestXDivXPositiveData))]
+    public void TestXDivXPositive(int[] aShape, int index)
+    {
+        var caseOptions = passOptions.IndentDir($"TestXDivXPositive_case_{index}");
         var a = new Var();
         var Normal = new Dictionary<Var, IValue>();
         Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
-        var rootPre = Math.Binary(binaryOp, Math.Binary(binaryOp, a, bValue), bValue);
+
+        var rootPre = a / a;
         var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
         {
-            new FoldNopBinary(),
+            new XDivX(),
         }, caseOptions);
-        // rootPre.InferenceType();
+        Assert.NotEqual(rootPre, rootPost);
+        Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
+    }
+
+    public static IEnumerable<object[]> TestCommutateMulPositiveData =>
+        new[]
+        {
+            new object[] {new[] {3}},
+        }.Select((o, i) => o.Concat(new object[] { i }).ToArray());
+
+    [Theory]
+    [MemberData(nameof(TestCommutateMulPositiveData))]
+    public void TestCommutateMulPositive(int[] aShape, int index)
+    {
+        var caseOptions = passOptions.IndentDir($"TestCommutateMulPositive_case_{index}");
+        caseOptions = caseOptions.SetRewriteOnce(true);
+        var a = new Var();
+        var b = new Var();
+        var Normal = new Dictionary<Var, IValue>();
+        Normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
+        Normal.Add(b, Random.Normal(DataTypes.Float32, 0, 1, 0, aShape).Evaluate());
+        var rootPre = a * b;
+        var rootPost = CompilerServices.Rewrite(rootPre, new IRewriteRule[]
+        {
+            new CommutateMul(),
+        }, caseOptions);
         Assert.NotEqual(rootPre, rootPost);
         Assert.Equal(CompilerServices.Evaluate(rootPre, Normal), CompilerServices.Evaluate(rootPost, Normal));
     }
