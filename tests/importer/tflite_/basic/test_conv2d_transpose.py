cb48fdb17eb4a6d8efe11750b08766c600d1db75
Lua <huochenghai@gmail.com>
2022-04-28 06:00:18 +0000

    update conv2d_transpose pytest

--- tests/importer/tflite_/basic/test_conv2d_transpose.py
+++ tests/importer/tflite_/basic/test_conv2d_transpose.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""System test: test transpose"""
+"""System test: test conv2d transpsoe"""
 # pylint: disable=invalid-name, unused-argument, import-outside-toplevel
 
 import pytest
@@ -20,48 +20,93 @@ import numpy as np
 from tflite_test_runner import TfliteTestRunner
 
 
-def _make_module(in_shape, perm):
-    class TransposeModule(tf.Module):
+def _make_module(n, i_channels, i_size, k_size, o_channels, strides, padding, dilations, bias):
+    class Conv2DTransposeModule(tf.Module):
         def __init__(self):
-            super(TransposeModule).__init__()
+            super(Conv2DTransposeModule).__init__()
+            self.w = tf.constant(np.random.rand(
+                *k_size, o_channels, i_channels).astype(np.float32) - 0.5)
+            self.b = tf.constant(np.random.rand(o_channels).astype(np.float32) - 0.5)
 
-        @tf.function(input_signature=[tf.TensorSpec(in_shape, tf.float32)])
+        @tf.function(input_signature=[tf.TensorSpec([n, *i_size, i_channels], tf.float32)])
         def __call__(self, x):
-            return tf.transpose(x, perm=perm)
-    return TransposeModule()
+            output_shape = [n, i_size[0] * strides[0], i_size[1] * strides[1], o_channels]
+            if padding == "VALID":
+                output_shape[1] = (i_size[0] - 1) * strides[0] + k_size[0]
+                output_shape[2] = (i_size[1] - 1) * strides[1] + k_size[1]
+            out = tf.nn.conv2d_transpose(x, self.w, output_shape, strides, padding,
+                                         dilations=dilations)
+            if bias:
+                out = out + self.b
+
+            return out
+    return Conv2DTransposeModule()
 
 
-in_shapes = [
-    [3, 2],
-    [64, 3],
-    [5, 9, 32],
-    [8, 3, 64, 3]
+n = [
+    1,
+    3
 ]
 
-perms = [
-    [1, 0],
-    [0, 2, 1],
-    [1, 0, 2],
-    [2, 0, 1],
-    [2, 1, 0],
-    [0, 2, 1, 3],
-    [0, 1, 3, 2],
-    [3, 1, 0, 2],
-    [3, 2, 0, 1],
-    [3, 2, 1, 0]
+i_channels = [
+    1,
+    16
+]
+
+i_sizes = [
+    [1, 1],
+    [33, 65]
+]
+
+k_sizes = [
+    [1, 1],
+    [3, 3],
+    [5, 5]
+]
+
+o_channels = [
+    1,
+    16
+]
+
+strides = [
+    [1, 1],
+    [1, 3],
+    [5, 5]
+]
+
+paddings = [
+    'SAME',
+    'VALID'
+]
+
+dilations = [
+    [1, 1],
+]
+
+biases = [
+    True,
+    False
 ]
 
 
-@pytest.mark.parametrize('in_shape', in_shapes)
-@pytest.mark.parametrize('perm', perms)
-def test_transpose(in_shape, perm, request):
-    if len(perm) == len(in_shape):
-        module = _make_module(in_shape, perm)
+@pytest.mark.parametrize('n', n)
+@pytest.mark.parametrize('i_channels', i_channels)
+@pytest.mark.parametrize('i_size', i_sizes)
+@pytest.mark.parametrize('k_size', k_sizes)
+@pytest.mark.parametrize('o_channels', o_channels)
+@pytest.mark.parametrize('strides', strides)
+@pytest.mark.parametrize('padding', paddings)
+@pytest.mark.parametrize('dilations', dilations)
+@pytest.mark.parametrize('bias', biases)
+def test_conv2d_transpose(n, i_channels, i_size, k_size, o_channels, strides, padding, dilations, bias, request):
+    module = _make_module(n, i_channels, i_size, k_size, o_channels,
+                          strides, padding, dilations, bias)
 
-        runner = TfliteTestRunner(request.node.name)
-        model_file = runner.from_tensorflow(module)
-        runner.run(model_file)
+    runner = TfliteTestRunner(request.node.name)
+    model_file = runner.from_tensorflow(module)
+    runner.run(model_file)
 
 
 if __name__ == "__main__":
-    pytest.main(['-vv', 'test_transpose.py'])
+    pytest.main(['-vv', 'test_conv2d_transpose.py'])
