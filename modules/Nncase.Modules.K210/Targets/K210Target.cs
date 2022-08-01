6d874c095f132b11276a0901f0259be831ca1169
郑启航 <597323109@qq.com>
2022-08-01 15:40:54 +0800

    fix k210 build

--- modules/Nncase.Modules.K210/Targets/K210Target.cs
+++ modules/Nncase.Modules.K210/Targets/K210Target.cs
@@ -44,7 +44,7 @@ public class K210Target : ITarget
     }
 
     /// <inheritdoc/>
-    public Dictionary<ENode, List<Tuple<List<DataType>, float>>> BindQuantMethodCosine(ICalibrationDatasetProvider calibrationDataset, ITarget target, List<ENode> rangeOfs, List<ENode> childrenOfRangeOfs, RunPassOptions runPassOptions)
+    public Task<Dictionary<ENode, List<Tuple<List<DataType>, float>>>> BindQuantMethodCosine(ICalibrationDatasetProvider calibrationDataset, ITarget target, List<ENode> rangeOfs, List<ENode> childrenOfRangeOfs, RunPassOptions runPassOptions)
     {
         return null;
     }
