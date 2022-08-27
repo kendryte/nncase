9c57d7da433af6f68ca8c9d072f39b8bca6bb152
zhangyang2057 <zhangyang@canaan-creative.com>
2022-08-27 15:20:28 +0800

    Fix empty port and app_full_name

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -776,12 +776,12 @@ class TestRunner(metaclass=ABCMeta):
 
         else:
             kpu_target = os.getenv('KPU_TARGET')
-            port = int(os.getenv('PORT'))
+            port = os.getenv('PORT')
             app_full_name = os.getenv('NNCASE_TEST_CI')
-            if self.in_ci and kwargs['target'] == kpu_target:
+            if self.in_ci and kwargs['target'] == kpu_target and port is not None and app_full_name is not None:
                 # connect server
                 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                client_socket.connect(('localhost', port))
+                client_socket.connect(('localhost', int(port)))
 
                 # send file_num
                 file_num_dict = {}
