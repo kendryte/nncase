e65097578af9395b220df7e58720e4982b830eea
zhangyang2057 <zhangyang@canaan-creative.com>
2022-09-26 10:18:41 +0800

    Add logging for ci_proxy (#646)
    * Add logging for ci_proxy
    
    * Set level for both logger and handler.

--- tests/ci_proxy.py
+++ tests/ci_proxy.py
@@ -6,50 +6,57 @@ import json
 import threading
 import queue
 import logging
+import logging.handlers
 import telnetlib
-import time
 
 class TelnetClient():
-    def __init__(self,):
+    def __init__(self, mylogger):
         self.tn = telnetlib.Telnet()
+        self.logger = mylogger
+        self.ip = '10.99.105.216'
 
     def login(self, ip, username, password):
         try:
             self.tn.open(ip, port=23)
         except:
-            logging.info('telnet %s failed' % ip)
+            self.logger.error('telnet {0} failed'.format(ip))
             return False
 
+        self.ip = ip
         self.tn.read_until(b'login: ', timeout=10)
         self.tn.write(username.encode() + b'\n')
 
         command_result = self.tn.read_very_eager().decode()
         if 'Login incorrect' not in command_result:
-            logging.info('%s login succeed' % ip)
+            self.logger.info('{0} login succeed'.format(ip))
             return True
         else:
-            logging.error('%s login failed' % ip)
+            self.logger.error('{0} login failed'.format(ip))
             return False
 
     def execute(self, command, flag):
+        self.logger.info('execute: cmd = {0}'.format(command))
         self.tn.write(command.encode() + b'\n')
         cmd_result = self.tn.read_until(flag.encode()).decode()
 
         self.tn.write('echo $?'.encode() + b'\n')
         cmd_status = self.tn.read_until(flag.encode()).decode()
         if cmd_status.find('\r\n0\r\n') == -1:
+            self.logger.error('execute {0} failed: {1}'.format(command, cmd_result))
             return cmd_result, False
         else:
             return cmd_result, True
 
     def logout(self):
         self.tn.write(b"exit\n")
+        self.logger.info('{0} logout succeed'.format(self.ip))
 
-def get_file(conn, target_root):
+def recv_file(conn, target_root, mylogger):
     header = conn.recv(1024)
     file_dict = json.loads(header.decode())
     file_name = file_dict['file_name']
     file_size = file_dict['file_size']
+    mylogger.info('recv: file = {0}, size = {1}'.format(file_name, file_size))
     conn.sendall(f"pls send {file_name}".encode())
 
     full_file = os.path.join(target_root, file_name)
@@ -64,32 +71,33 @@ def get_file(conn, target_root):
     os.chmod(full_file, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
     return file_name
 
-def Consumer(kpu_target, kpu_ip, kpu_username, kpu_password, nfsroot, q):
+def Consumer(kpu_target, kpu_ip, kpu_username, kpu_password, nfsroot, q, mylogger):
+    # create target root
+    target_root = os.path.join(nfsroot, kpu_target)
+    if not os.path.exists(target_root):
+        os.makedirs(target_root)
+
     while True:
         cmd = './'
         conn = q.get()
 
-        # create target root
-        target_root = os.path.join(nfsroot, kpu_target)
-        if not os.path.exists(target_root):
-            os.makedirs(target_root)
-
-        # recv file_num
+        # recv header
         header = conn.recv(1024)
-        file_num_dict = json.loads(header.decode())
-        file_num = file_num_dict['app'] + file_num_dict['kmodel'] + file_num_dict['inputs']
+        header_dict = json.loads(header.decode())
+        mylogger.info("test case = {0}".format(header_dict['case']))
+        file_num = header_dict['app'] + header_dict['kmodel'] + header_dict['inputs']
         conn.sendall(f"pls send {file_num} files".encode())
 
         # recv all kinds of files(app + kmodel + inputs)
         for i in range(file_num):
-            file = get_file(conn, target_root)
+            file = recv_file(conn, target_root, mylogger)
             if i == 0:
                 cmd = cmd + file
             else:
                 cmd = cmd + ' ' + file
 
         # telnet target devcie to infer
-        telnet_client = TelnetClient()
+        telnet_client = TelnetClient(mylogger)
         telnet_client.login(kpu_ip, kpu_username, kpu_password)
         flag = f'/mnt/{kpu_target} ]$'
         telnet_client.execute(f'cd /mnt/{kpu_target}', flag)
@@ -102,7 +110,7 @@ def Consumer(kpu_target, kpu_ip, kpu_username, kpu_password, nfsroot, q):
             dummy = conn.recv(1024)
 
             # send outputs
-            for i in range(file_num_dict['outputs']):
+            for i in range(header_dict['outputs']):
                 file = os.path.join(target_root, f'nncase_result_{i}.bin')
                 file_size = os.path.getsize(file)
                 conn.sendall(str(file_size).encode())
@@ -111,6 +119,7 @@ def Consumer(kpu_target, kpu_ip, kpu_username, kpu_password, nfsroot, q):
                 with open(file, 'rb') as f:
                     conn.sendall(f.read())
                 dummy = conn.recv(1024)
+                mylogger.info('send: file = {0}, size = {1}'.format(file, file_size))
         else:
             conn.sendall(f'infer failed on {kpu_target} board: {cmd_result}'.encode())
 
@@ -120,6 +129,7 @@ def Consumer(kpu_target, kpu_ip, kpu_username, kpu_password, nfsroot, q):
         conn.close()
 
 def main():
+    # args
     parser = argparse.ArgumentParser(prog="kendryte_ci_proxy")
     parser.add_argument("--kpu_target", help='kpu device target', type=str, default='k510')
     parser.add_argument("--kpu_ip", help='kpu deivce ip address', type=str, default='10.99.105.216')
@@ -128,11 +138,21 @@ def main():
     parser.add_argument("--nfsroot", help='nfsroot on pc', type=str, default='/data/nfs')
     parser.add_argument("--port", help='listenning port of ci_proxy', type=int, default=51000)
     args = parser.parse_args()
+
+    # logging
+    mylogger = logging.getLogger()
+    mylogger.setLevel(logging.DEBUG)
+    rf_handler = logging.handlers.RotatingFileHandler(f'ci_proxy_{args.kpu_target}.log', mode='a', maxBytes=32 * 1024 * 1024, backupCount=10)
+    rf_handler.setLevel(logging.INFO)
+    rf_handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
+    mylogger.addHandler(rf_handler)
+
+    # producer
     size = 32
     q = queue.Queue(maxsize=size)
 
-    # start comsumer thread
-    t_consumer = threading.Thread(target=Consumer, args=(args.kpu_target, args.kpu_ip, args.kpu_username, args.kpu_password, args.nfsroot, q))
+    # comsumer
+    t_consumer = threading.Thread(target=Consumer, args=(args.kpu_target, args.kpu_ip, args.kpu_username, args.kpu_password, args.nfsroot, q, mylogger))
     t_consumer.start()
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -140,7 +160,6 @@ def main():
     server_socket.listen(size)
     while True:
         conn, addr = server_socket.accept()
-        # print('connnected by {0}'.format(addr))
         q.put(conn)
 
 if __name__ == '__main__':
