cab6f877c6c4d7fdb4ba753b53bb6fec4cede36a
Curio Yang <curioyhq@gmail.com>
2022-10-13 16:04:29 +0800

    support RV_ternary [init] (#656)
    * init support RV_ternary
    
    * apply code-format changes
    
    Co-authored-by: curioyang <curioyang@users.noreply.github.com>

---
+++ src/kernels/cpu/optimized/riscv64/ternary.cpp
@@ -0,0 +1,61 @@
+/* Copyright 2019-2021 Canaan Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+#include <nncase/kernels/cpu/optimized/tensor_compute.h>
+#include <nncase/kernels/cpu/reference/tensor_compute.h>
+#include <nncase/kernels/kernel_utils.h>
+#include <nncase/runtime/runtime_op_utility.h>
+
+using namespace nncase;
+using namespace nncase::runtime;
+using namespace nncase::kernels;
+using namespace nncase::kernels::cpu;
+using namespace nncase::kernels::cpu::optimized;
+
+#if __riscv_vector
+
+result<void> ternary_impl(const float *input_a, const float *input_b, const float *input_c, float *output,
+    const runtime_shape_t &in_a_shape, const runtime_shape_t &in_a_strides, const runtime_shape_t &in_b_shape,
+    const runtime_shape_t &in_b_strides, const runtime_shape_t &in_c_shape, const runtime_shape_t &in_c_strides,
+    const runtime_shape_t &out_strides)
+{
+
+    return ok();
+}
+#endif
+
+template result<void> optimized::layernorm<float>(const float *input_a, const float *input_b, const float *input_c, float *output,
+    const runtime_shape_t &in_a_shape, const runtime_shape_t &in_a_strides, const runtime_shape_t &in_b_shape,
+    const runtime_shape_t &in_b_strides, const runtime_shape_t &in_c_shape, const runtime_shape_t &in_c_strides,
+    const runtime_shape_t &out_strides) noexcept;
+
+template result<void> optimized::layernorm<int64_t>(const float *input_a, const int64_t *input_b, const int64_t *input_c, int64_t *output,
+    const runtime_shape_t &in_a_shape, const runtime_shape_t &in_a_strides, const runtime_shape_t &in_b_shape,
+    const runtime_shape_t &in_b_strides, const runtime_shape_t &in_c_shape, const runtime_shape_t &in_c_strides,
+    const runtime_shape_t &out_strides) noexcept;
+
+template <typename T>
+result<void> optimized::layernorm(const float *input_a, const T *input_b, const T *input_c, T *output,
+    const runtime_shape_t &in_a_shape, const runtime_shape_t &in_a_strides, const runtime_shape_t &in_b_shape,
+    const runtime_shape_t &in_b_strides, const runtime_shape_t &in_c_shape, const runtime_shape_t &in_c_strides,
+    const runtime_shape_t &out_strides) noexcept
+{
+#if __riscv_vector
+    return tenary_impl(input_a, input_b, input_c, output, in_a_shape, in_a_strides, in_b_shape, in_b_strides, in_c_shape, in_c_strides, out_strides);
+#else
+    return cpu::reference::ternary(input_a, input_b, input_c, output, in_a_shape, in_a_strides, in_b_shape, in_b_strides, in_c_shape, in_c_strides, out_strides);
+#endif
+}
