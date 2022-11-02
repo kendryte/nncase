635e2ce11c895b91090d5f01cb323ea5d0f15814
sunnycase <sunnycase@live.cn>
2022-11-02 14:09:59 +0800

    Update

---
+++ src/Nncase.Studio/Nncase.Studio/Pages/Open.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Nncase.Studio.Pages;
+
+public partial class Open
+{
+    [Parameter]
+    public string WorkingDirectory { get; set; }
+}
