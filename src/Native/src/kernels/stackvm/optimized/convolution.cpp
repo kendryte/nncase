fc87ba057b13556f841ea15c2a2c1460f5bbc634
sunnycase <sunnycase@live.cn>
2022-11-09 15:21:26 +0800

    Fix

--- src/Native/src/kernels/stackvm/optimized/convolution.cpp
+++ src/Native/src/kernels/stackvm/optimized/convolution.cpp
@@ -82,7 +82,7 @@ result<void> conv2d_1x1_s1(const float *input, const float *weights, const float
 #ifdef NNCASE_OPENMP
 #pragma omp parallel for num_threads(context.num_threads)
 #endif
-        for (int oc = 0; (size_t)oc < out_channels; oc++)
+        for (int oc = 0; oc < out_channels; oc++)
         {
             const auto out_c = oc;
             const float *now_weights = weights + out_c * w_strides[0];
@@ -169,7 +169,7 @@ result<void> conv2d_1x1_s2(const float *input, const float *weights, const float
 #ifdef NNCASE_OPENMP
 #pragma omp parallel for num_threads(context.num_threads)
 #endif
-        for (int oc = 0; (size_t)oc < out_channels; oc++)
+        for (int oc = 0; oc < out_channels; oc++)
         {
             float *out = output + (b * out_strides[0] + oc * out_strides[1]);
 
@@ -399,7 +399,7 @@ result<void> conv2d_nxm(const float *input, const float *weights, const float *b
 #ifdef NNCASE_OPENMP
 #pragma omp parallel for num_threads(context.num_threads)
 #endif
-        for (size_t oc = 0; (size_t)oc < out_channels; oc++) // out channel
+        for (size_t oc = 0; oc < out_channels; oc++) // out channel
         {
             std::array<float *, Parallel> outptr;
             std::array<const float *, compute_rsize<Parallel, Stride_h, Filter_h>()> r;
@@ -448,7 +448,7 @@ result<void> conv2d_depthwise_nxm(const float *input, const float *weights, cons
 #ifdef NNCASE_OPENMP
 #pragma omp parallel for num_threads(context.num_threads)
 #endif
-        for (int c = 0; (size_t)c < channels; c++) // channel
+        for (size_t c = 0; c < channels; c++) // channel
         {
             std::array<float *, Parallel> outptr;
             std::array<const float *, compute_rsize<Parallel, Stride_h, Filter_h>()> r;
