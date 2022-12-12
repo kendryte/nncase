dd1a531ef138b216130dbb2275b2fb4b4d9fe87a
sunnycase <sunnycase@live.cn>
2022-12-12 15:54:11 +0800

    Add coredump

--- src/Native/src/compiler/compiler.cpp
+++ src/Native/src/compiler/compiler.cpp
@@ -22,7 +22,10 @@
 #include <nncase/runtime/error.h>
 
 #ifdef WIN32
+// clang-format off
 #include <Windows.h>
+#include <DbgHelp.h>
+// clang-format on
 #elif defined(__unix__) || defined(__APPLE__)
 #include <dlfcn.h>
 #endif
@@ -146,6 +149,70 @@ void *load_symbol(void *module, const char *name) {
 #define _T(x) x
 #endif
 
+inline BOOL IsDataSectionNeeded(const WCHAR *pModuleName) {
+    if (pModuleName == 0) {
+        return FALSE;
+    }
+
+    WCHAR szFileName[_MAX_FNAME] = L"";
+    _wsplitpath(pModuleName, NULL, NULL, szFileName, NULL);
+
+    if (_wcsicmp(szFileName, L"ntdll") == 0)
+        return TRUE;
+
+    return FALSE;
+}
+
+inline BOOL CALLBACK MiniDumpCallback(PVOID pParam,
+                                      const PMINIDUMP_CALLBACK_INPUT pInput,
+                                      PMINIDUMP_CALLBACK_OUTPUT pOutput) {
+    if (pInput == 0 || pOutput == 0)
+        return FALSE;
+
+    switch (pInput->CallbackType) {
+    case ModuleCallback:
+        if (pOutput->ModuleWriteFlags & ModuleWriteDataSeg)
+            if (!IsDataSectionNeeded(pInput->Module.FullPath))
+                pOutput->ModuleWriteFlags &= (~ModuleWriteDataSeg);
+    case IncludeModuleCallback:
+    case IncludeThreadCallback:
+    case ThreadCallback:
+    case ThreadExCallback:
+        return TRUE;
+    default:;
+    }
+
+    return FALSE;
+}
+
+inline void CreateMiniDump(PEXCEPTION_POINTERS pep, LPCTSTR strFileName) {
+    HANDLE hFile =
+        CreateFile(strFileName, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE,
+                   NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+    if ((hFile != NULL) && (hFile != INVALID_HANDLE_VALUE)) {
+        MINIDUMP_EXCEPTION_INFORMATION mdei;
+        mdei.ThreadId = GetCurrentThreadId();
+        mdei.ExceptionPointers = pep;
+        mdei.ClientPointers = NULL;
+
+        MINIDUMP_CALLBACK_INFORMATION mci;
+        mci.CallbackRoutine = (MINIDUMP_CALLBACK_ROUTINE)MiniDumpCallback;
+        mci.CallbackParam = 0;
+
+        ::MiniDumpWriteDump(::GetCurrentProcess(), ::GetCurrentProcessId(),
+                            hFile, MiniDumpNormal, (pep != 0) ? &mdei : 0, NULL,
+                            &mci);
+
+        CloseHandle(hFile);
+    }
+}
+
+LONG __stdcall MyUnhandledExceptionFilter(PEXCEPTION_POINTERS pExceptionInfo) {
+    CreateMiniDump(pExceptionInfo, "core.dmp");
+    return EXCEPTION_EXECUTE_HANDLER;
+}
+
 struct premain {
     premain() {
         absl::FailureSignalHandlerOptions failure_signal_handler_options;
@@ -154,6 +221,8 @@ struct premain {
         failure_signal_handler_options.alarm_on_failure_secs = 5;
         failure_signal_handler_options.call_previous_handler = true;
         absl::InstallFailureSignalHandler(failure_signal_handler_options);
+        
+        SetUnhandledExceptionFilter(MyUnhandledExceptionFilter);
     }
 } premain_v;
 
@@ -200,6 +269,8 @@ c_api_mt g_c_api_mt;
 } // namespace
 
 int nncase_clr_initialize(const char *root_assembly_path) {
+    int *p = 0;
+    *p = 1;
     if (!g_c_api_mt.handle_free) {
         auto init = load_compiler_c_api_initializer(root_assembly_path);
         init(&g_c_api_mt);
