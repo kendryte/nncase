71e4e312870be46aab7c1f9431c02c2b099adb28
huochenghai <huochenghai@canaan-creative.com>
2023-02-22 16:32:53 +0800

    modify ReduceArg DestType

--- src/Nncase.Core/IR/Math/ReduceArg.cs
+++ src/Nncase.Core/IR/Math/ReduceArg.cs
@@ -16,7 +16,7 @@ namespace Nncase.IR.Math;
 /// ReduceArg expression.
 /// </summary>
 [PatternFunctionalGenerator]
-public sealed record ReduceArg(ReduceArgOp ReduceArgOp, PrimType DestType) : Op
+public sealed record ReduceArg(ReduceArgOp ReduceArgOp, DataType DestType) : Op
 {
     /// <summary>
     /// Gets input.
