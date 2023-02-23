35b78fbb09dcd01d0265538c5af3631c9d0e2db1
FusionBolt <realfusionbolt@gmail.com>
2023-02-23 17:17:55 +0800

    fix build

--- src/Native/src/kernels/stackvm/tensor_ops.cpp
+++ src/Native/src/kernels/stackvm/tensor_ops.cpp
@@ -1005,19 +1005,6 @@ result<value_t> nncase::kernels::stackvm::fake_quantize(
     return err(std::errc::not_supported);
 }
 
-result<value_t> nncase::kernels::stackvm::condition(
-    [[maybe_unused]] bool can_fold_const_call,
-    [[maybe_unused]] value_t predicate, [[maybe_unused]] value_t value,
-    [[maybe_unused]] value_t output, [[maybe_unused]] kernel_context &context) {
-    return err(std::errc::not_supported);
-}
-
-result<value_t> nncase::kernels::stackvm::bitcast(
-    [[maybe_unused]] prim_type_t type, [[maybe_unused]] prim_type_t new_type,
-    [[maybe_unused]] value_t input, [[maybe_unused]] value_t new_shape,
-    [[maybe_unused]] value_t output, [[maybe_unused]] kernel_context &context) {
-    return err(std::errc::not_supported);
-}
 // result<value_t> nncase::kernels::stackvm::uninitialized(
 //    NNCASE_UNUSED typecode_t dtype,
 //    NNCASE_UNUSED runtime::stackvm::memory_location_t memory_location,
