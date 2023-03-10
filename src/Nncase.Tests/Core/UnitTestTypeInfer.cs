b99a41ea75bbd1389c7c35c0d5b9110b83220c71
huochenghai <huochenghai@canaan-creative.com>
2023-03-10 14:20:15 +0800

    add more matmul type infer case

--- src/Nncase.Tests/Core/UnitTestTypeInfer.cs
+++ src/Nncase.Tests/Core/UnitTestTypeInfer.cs
@@ -53,6 +53,7 @@ public class UnitTestTypeInfer : UnitTypeInferBase
             new object[] { new[] { 3, 10, 128 }, new[] { 128, 128 }, new[] { 3, 10, 128 } },
             new object[] { new[] { 10, 128 }, new[] { 2, 128, 128 }, new[] { 2, 10, 128 } },
             new object[] { new[] { 4, 10, 128 }, new[] { 1, 128, 128 }, new[] { 4, 10, 128 } },
+            new object[] { new[] { 1, 10, 128 }, new[] { 4, 128, 128 }, new[] { 4, 10, 128 } },
         };
 
     [Fact]
