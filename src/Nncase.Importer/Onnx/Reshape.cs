a08c5cd620f2abd34b7456ab4c1ff98b6d4ad062
huochenghai <huochenghai@canaan-creative.com>
2023-03-23 10:17:45 +0800

    fix importer of onnx reshape

--- src/Nncase.Importer/Onnx/Reshape.cs
+++ src/Nncase.Importer/Onnx/Reshape.cs
@@ -3,6 +3,7 @@
 
 using System;
 using Nncase.IR;
+using Nncase.IR.Tensors;
 using Onnx;
 using F = Nncase.IR.F;
 
@@ -13,6 +14,45 @@ namespace Nncase.Importer
         private Expr VisitReshape(in NodeProto op)
         {
             var (input, shape) = GetInputExprs(op, 0, 1);
+            var inputShape = F.Tensors.ShapeOf(input);
+            var shapeValue = ((TensorConst)shape).Value.ToArray<long>();
+            var actualShape = new Expr[shapeValue.Length];
+            var negAxis = shapeValue.Length;
+            for (int i = 0; i < actualShape.Length; i++)
+            {
+                if (shapeValue[i] == 0L)
+                {
+                    actualShape[i] = inputShape[i];
+                }
+                else if (shapeValue[i] == -1L)
+                {
+                    negAxis = i;
+                }
+                else
+                {
+                    actualShape[i] = shapeValue[i];
+                }
+            }
+
+            if (negAxis < shapeValue.Length)
+            {
+                Expr productOut = 1L;
+                for (int i = 0; i < shapeValue.Length; i++)
+                {
+                    if (i != negAxis)
+                    {
+                        productOut *= actualShape[i];
+                    }
+                }
+
+                Expr productIn = 1L;
+                for (int i = 0; i < input.CheckedShape.Rank; i++)
+                {
+                    productIn *= inputShape[i];
+                }
+
+                actualShape[negAxis] = productIn / productOut;
+            }
 
             // allowzero has been avaliable since opset 14
             var allowZero = GetBoolAttribute(op, "allowzero", false);
@@ -21,7 +61,7 @@ namespace Nncase.Importer
                 throw new NotSupportedException("Not support reshape attribute: allowzero");
             }
 
-            return F.Tensors.Reshape(input, shape);
+            return F.Tensors.Reshape(input, F.Tensors.Stack(new IR.Tuple(actualShape), 0));
         }
     }
 }
