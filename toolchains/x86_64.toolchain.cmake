09195c69f7e458ecc35248259802e86d8af14485
sunnycase <sunnycase@live.cn>
2023-03-28 17:46:40 +0800

    Disable simd on apple

--- toolchains/x86_64.toolchain.cmake
+++ toolchains/x86_64.toolchain.cmake
@@ -1,7 +1,7 @@
 if (MSVC)
     add_compile_options(/arch:AVX)
     add_compile_options(/arch:AVX2)
-else()
+elseif(NOT APPLE)
     add_compile_options(-mfma -msse -msse2 -msse3 -mssse3 -msse4 -msse4a -msse4.1 -msse4.2 -mavx -mavx2)
 endif()
 
