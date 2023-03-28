5dca2822112e360c7ea9183537aac0818be48f10
sunnycase <sunnycase@live.cn>
2023-03-28 15:49:01 +0800

    Fix

--- toolchains/x86_64.toolchain.cmake
+++ toolchains/x86_64.toolchain.cmake
@@ -1,8 +1,10 @@
 if (MSVC)
-    add_definitions(/DX86_64_SIMD_ON)
     add_compile_options(/arch:AVX)
     add_compile_options(/arch:AVX2)
 else()
-    add_definitions(-DX86_64_SIMD_ON)
     add_compile_options(-mfma -msse -msse2 -msse3 -mssse3 -msse4 -msse4a -msse4.1 -msse4.2 -mavx -mavx2)
+endif()
+
+if(ENABLE_X86SIMD)
+    add_definitions(-DX86_64_SIMD_ON)
 endif()
