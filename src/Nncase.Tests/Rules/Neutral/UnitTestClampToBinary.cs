f4c8b9a9f8bf8499096dc045c0e4fc4bbf0734fb
Hejunchao6 <Hejunchao6@users.noreply.github.com>
2023-04-06 09:05:14 +0000

    Apply code-format changes

--- src/Nncase.Tests/Rules/Neutral/UnitTestClampToBinary.cs
+++ src/Nncase.Tests/Rules/Neutral/UnitTestClampToBinary.cs
@@ -24,7 +24,7 @@ public class UnitTestClampToBinary : TransformTestBase
         new[]
         {
             new object[] { new[] { 1, 32, 24, 24 }, new[] { float.MinValue, }, new[] { float.MaxValue }, },
-            new object[] { new[] { 1, 32, 24, 24 }, new [] { -1.0f }, new[] { 1.0f }, },
+            new object[] { new[] { 1, 32, 24, 24 }, new[] { -1.0f }, new[] { 1.0f }, },
         };
 
     [Theory]
