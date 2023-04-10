5913d55210683b250803bbd373dc4eae6a9b9ca4
hejunchao <hejunchao@canaan-creative.com>
2023-04-10 15:14:24 +0800

    add unitTest

---
+++ src/Nncase.Tests/Core/UnitTestWeakList.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+using Nncase;
+using Nncase.Collections;
+using Nncase.IR;
+using Nncase.Utilities;
+using Xunit;
+
+namespace Nncase.Tests.CoreTest;
+
+public sealed class UnitTestWeakList
+{
+    [Fact]
+    public void TestWeakList()
+    {
+        var weakList = new WeakList<object>();
+        Assert.Equal(0, weakList.WeakCount);
+        Assert.Throws<ArgumentOutOfRangeException>(() => weakList.GetWeakReference(-1));
+        weakList.Add(1);
+        Assert.Equal(new WeakReference<object>(1).TryGetTarget(out _), weakList.GetWeakReference(0).TryGetTarget(out _));
+        weakList.Add(2);
+        using var iEnumerator = weakList.GetEnumerator();
+    }
+}
