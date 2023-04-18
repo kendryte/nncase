9c70e246f59feeecab5a39f83cfe7201ed55318f
hejunchao <hejunchao@canaan-creative.com>
2023-04-18 18:06:36 +0800

    PatternPrinter's unitTest

---
+++ src/Nncase.Tests/Core/UnitTestPatternPrinter.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Text;
+using Nncase;
+using Nncase.PatternMatch;
+using Nncase.Tests.TestFixture;
+using Xunit;
+
+namespace Nncase.Tests.CoreTest;
+
+public sealed class UnitTestPatternPrinter
+{
+    [Fact]
+    public void TestDumpAsIL()
+    {
+        IPattern pattern = null!;
+        Assert.Throws<NullReferenceException>(() => PatternPrinter.DumpAsIL(pattern));
+    }
+}
