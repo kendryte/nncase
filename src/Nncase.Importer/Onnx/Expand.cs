ac36e74b18d9ef78667b9feb3507dfbf6fe922e4
huochenghai <huochenghai@canaan-creative.com>
2023-04-18 10:06:16 +0800

    fix expand with smaller shape

--- src/Nncase.Importer/Onnx/Expand.cs
+++ src/Nncase.Importer/Onnx/Expand.cs
@@ -13,7 +13,22 @@ namespace Nncase.Importer
         private Expr VisitExpand(in NodeProto op)
         {
             var (input, shape) = GetInputExprs(op, 0, 1);
-            return F.Tensors.Expand(input, shape);
+
+            var maxLen = System.Math.Max(input.CheckedShape.Rank, shape.CheckedShape.Size);
+            var outputShape = new Expr[maxLen];
+            for (var i = 0; i < maxLen; i++)
+            {
+                if (maxLen == input.CheckedShape.Rank)
+                {
+                    outputShape[i] = F.Math.Max(F.Tensors.ShapeOf(input)[i], i < input.CheckedShape.Rank - shape.CheckedShape.Size ? 1 : shape[i]);
+                }
+                else
+                {
+                    outputShape[i] = F.Math.Max(i < shape.CheckedShape.Size - input.CheckedShape.Rank ? 1 : F.Tensors.ShapeOf(input)[i], shape[i]);
+                }
+            }
+
+            return F.Tensors.Expand(input, F.Tensors.Stack(new IR.Tuple(outputShape), 0));
         }
     }
 }
