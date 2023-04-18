f257253ccfa68c7d2633cab8228fbfa491ed12b0
Hejunchao6 <Hejunchao6@users.noreply.github.com>
2023-04-18 10:09:36 +0000

    Apply code-format changes

--- src/Nncase.Tests/Core/UnitTestPatternPrinter.cs
+++ src/Nncase.Tests/Core/UnitTestPatternPrinter.cs
@@ -3,7 +3,6 @@
 
 // Copyright (c) Canaan Inc. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
-
 using System;
 using System.IO;
 using System.Text;
