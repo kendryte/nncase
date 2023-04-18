0daf08595770529b4bad42850735cfe777e54d2f
hejunchao <hejunchao@canaan-creative.com>
2023-04-18 18:15:57 +0800

    Fix small issue

--- src/Nncase.Tests/Core/UnitTestPatternPrinter.cs
+++ src/Nncase.Tests/Core/UnitTestPatternPrinter.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Canaan Inc. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
-// Copyright (c) Canaan Inc. All rights reserved.
-// Licensed under the Apache license. See LICENSE file in the project root for full license information.
 using System;
 using System.IO;
 using System.Text;
