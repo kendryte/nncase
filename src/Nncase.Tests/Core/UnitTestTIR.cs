ee22be745bca33b76b92ef0d6c57faea74b370df
hejunchao <hejunchao@canaan-creative.com>
2023-04-19 16:30:40 +0800

    UnitTestTIR

---
+++ src/Nncase.Tests/Core/UnitTestTIR.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nncase;
+using Nncase.Evaluator;
+using Nncase.IR;
+using Nncase.Schedule;
+using Nncase.TIR;
+using OrtKISharp;
+using Xunit;
+
+namespace Nncase.Tests.CoreTest;
+
+public sealed class UnitTestTIR
+{
+    [Fact]
+    public void TestLogicalBuffer()
+    {
+        var logicalBuffer = new LogicalBuffer("logicalBuffer", default, new TensorConst(new Tensor<int>(new[] { 1 })));
+        Assert.Equal(logicalBuffer.Length, logicalBuffer.Length);
+        Assert.Equal("LogicalBuffer(logicalBuffer, i32, MemLocation)", logicalBuffer.ToString());
+    }
+
+    [Fact]
+    public void TestScheduler()
+    {
+        var input = OrtKI.Random(new long[] { 1, 3, 16, 16 });
+        var alpha = 0.8F;
+        var original = IR.F.NN.Celu(input.ToTensor(), alpha);
+        var function = new Function(original);
+        var scheduler = new Scheduler(new Function("Celu", original));
+        Assert.Equal(function, scheduler.Entry);
+        Assert.Throws<InvalidOperationException>(() => scheduler.GetBlock("test"));
+        scheduler.GetLoops(new Block("block"));
+    }
+}
