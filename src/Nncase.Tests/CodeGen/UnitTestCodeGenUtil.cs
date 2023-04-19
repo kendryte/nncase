3b1395ded1a6d67b11c9898e1d430886d524231e
hejunchao <hejunchao@canaan-creative.com>
2023-04-19 12:04:18 +0800

    UnitTestBufferAllocation

---
+++ src/Nncase.Tests/CodeGen/UnitTestCodeGenUtil.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+using Nncase;
+using Nncase.CodeGen;
+using Nncase.IR;
+using Nncase.Utilities;
+using Xunit;
+
+namespace Nncase.Tests.CoreTest;
+
+public struct enm
+{
+    public string title;
+    public string author;
+    public string subject;
+    public int book_id;
+}
+
+public class UnitTestCodeGenUtil
+{
+    [Fact]
+    public void TestCodeGenUtil()
+    {
+        CodeGenUtil.GetTempFileName();
+    }
+}
