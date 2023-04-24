91b852b189bd335b46dcd0503a55713ace8ca8fa
huochenghai <huochenghai@canaan-creative.com>
2023-04-24 15:58:37 +0800

    add range of marker for Tile

--- src/Nncase.Passes/Rules/Neutral/AddRangeOfAndMarker.cs
+++ src/Nncase.Passes/Rules/Neutral/AddRangeOfAndMarker.cs
@@ -59,6 +59,7 @@ public partial class AddRangeOfAndMarker : RewriteRule<Pattern>
         { typeof(Compare).TypeHandle, 2 },
         { typeof(Binary).TypeHandle, 2 },
         { typeof(Clamp).TypeHandle, 1 },
+        { typeof(Tile).TypeHandle, 1 },
     };
 
     private static readonly Dictionary<RuntimeTypeHandle, int[]> _DictList = new() { { typeof(LSTM).TypeHandle, new[] { 0, 1, 2, 5, 6 } }, };
