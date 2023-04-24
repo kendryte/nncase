de1f2fa455cda57f94e44ad02f5fda7cdad49067
guodongliang <guodongliang@canaan-creative.com>
2023-04-24 20:01:52 +0800

    cast type for the second input for binary

--- src/Nncase.Importer/Onnx/Binary.cs
+++ src/Nncase.Importer/Onnx/Binary.cs
@@ -13,7 +13,7 @@ namespace Nncase.Importer
         private Expr VisitBinary(NodeProto op, BinaryOp binaryOp)
         {
             var (lhs, rhs) = GetInputExprs(op, 0, 1);
-            return F.Math.Binary(binaryOp, lhs, rhs);
+            return F.Math.Binary(binaryOp, lhs, IR.F.Tensors.Cast(rhs, lhs.CheckedDataType));
         }
     }
 }
