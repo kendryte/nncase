8ab5149b3b68f5caa1f0367f54fe61facb31a182
huochenghai <huochenghai@canaan-creative.com>
2023-04-25 14:21:12 +0800

    fix add range of marker

--- src/Nncase.Passes/Rules/Neutral/AddRangeOfAndMarker.cs
+++ src/Nncase.Passes/Rules/Neutral/AddRangeOfAndMarker.cs
@@ -148,7 +148,7 @@ public partial class AddRangeOfAndMarker : RewriteRule<Pattern>
         Call newCall;
         if (pairs.Count != 0)
         {
-            newCall = ReplaceCallParams(op, callParams, list.Where(i => callParams[i] is not Marker).Select(i => (call: callParams[i], pairs[callParams[i]])).ToArray());
+            newCall = ReplaceCallParams(op, callParams, list.Where(i => callParams[i] is not Marker).Select(i => (call: i, pairs[callParams[i]])).ToArray());
             if (call.Metadata.OutputNames != null)
             {
                 newCall.Metadata.OutputNames = call.Metadata.OutputNames;
