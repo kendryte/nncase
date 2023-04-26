09e360443632ed34dd0b09100f6f06525cc6ad57
hejunchao <hejunchao@canaan-creative.com>
2023-04-26 11:50:45 +0800

    Fix UnitTestGetReplaceUtility

--- src/Nncase.Tests/Core/UnitTestGetReplaceUtility.cs
+++ src/Nncase.Tests/Core/UnitTestGetReplaceUtility.cs
@@ -2,14 +2,19 @@
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Security.Cryptography;
 using Nncase;
 using Nncase.Evaluator;
 using Nncase.IR;
+using Nncase.IR.NN;
+using Nncase.IR.Tensors;
 using Nncase.Passes;
 using Nncase.Tests.TestFixture;
 using Nncase.TIR;
 using OrtKISharp;
+using Tensorflow;
 using Xunit;
+using Fx = System.Func<Nncase.IR.Expr, Nncase.IR.Expr>;
 
 namespace Nncase.Tests.CoreTest;
 
@@ -18,22 +23,54 @@ public sealed class UnitTestGetReplaceUtility
     [Fact]
     public void TestGetReplaceUtility()
     {
-        var input = OrtKI.Random(1, 4, 5, 5);
-        var weight = OrtKI.Random(8, 4, 3, 3);
-        var bias = OrtKI.Random(8);
-        var expr = IR.F.NN.Conv2D(
-            input.ToTensor(),
-            weight.ToTensor(),
-            bias.ToTensor(),
-            stride: new[] { 1, 1 },
-            padding: Tensor.From<int>(new int[] { 1, 1, 1, 1 }, new[] { 2, 2 }),
-            dilation: new[] { 1, 1 },
-            PadMode.Constant,
-            1);
-        _ = Utility.WithTmpBF16(_ => expr);
-        _ = Utility.WithTmpType(_ => expr, DataTypes.Float32);
-        _ = Utility.WithTmp4DShape(_ => expr, new[] { 1, 1, 1, 1 });
-        _ = Utility.Get4DGNNEShape(new[] { 0, 1, 2, 3 });
         Assert.Throws<InvalidOperationException>(() => Utility.Get4DGNNEShape(new[] { 0, 1, 2, 3, 4 }));
+    }
+
+    [Fact]
+    public void WithTmp4DShape_WhenGivenFunctionAndOutputShape_ShouldInsertRehsapeBeforeAndAfterCall()
+    {
+        var input = IR.F.Random.Normal(DataTypes.UInt8, new[] { 1, 2, 3, 4 });
+        Fx inputCtor = expr => IR.F.Tensors.Flatten(expr, 1);
+        var originOutShape = new[] { 1, 2, 3, 4 };
+
+        var output = Utility.WithTmp4DShape(inputCtor, originOutShape)(input);
+
+        Assert.Equal(input.CheckedType, output.CheckedType);
+        Assert.Equal(originOutShape, output.CheckedShape.ToValueArray());
+    }
+
+    [Fact]
+    public void WithTmpType_WhenGivenFunctionAndDataType_ShouldInsertCastBeforeAndAfterCall()
+    {
+        var input = IR.F.Random.Normal(DataTypes.UInt8, new[] { 1, 2, 3, 4 });
+        Fx inputCtor = expr => IR.F.Tensors.Flatten(expr, 1);
+        var dataType = DataTypes.Float32;
+
+        var output = Utility.WithTmpType(inputCtor, dataType)(input);
+
+        Assert.Equal(new[] { 1, 24 }, output.CheckedShape);
+    }
+
+    [Fact]
+    public void WithTmpBF16_WhenGivenFunction_ShouldInsertCastBeforeAndAfterCall()
+    {
+        var input = IR.F.Random.Normal(DataTypes.Float32, new[] { 1, 2, 3, 4 });
+        Fx inputCtor = expr => IR.F.Tensors.Flatten(expr, 1);
+
+        var output = Utility.WithTmpBF16(inputCtor)(input);
+
+        Assert.Equal(new[] { 1, 24 }, output.CheckedShape);
+    }
+
+    [Fact]
+    public void Apply_WhenGivenFunction_ShouldApplyFunctionToInput()
+    {
+        var input = IR.F.Random.Normal(DataTypes.UInt8, new[] { 1, 2, 3, 4 });
+        Fx inputCtor = expr =>
+            IR.F.NN.Pad(expr, new[,] { { 0, 0 }, { 0, 0 }, { 1, 1 }, { 1, 1 } }, PadMode.Constant, (byte)0);
+
+        var output = Utility.Apply(f => expr => f(expr), inputCtor)(input);
+
+        Assert.Equal(new[] { 1, 2, 5, 6 }, output.CheckedShape);
     }
 }
