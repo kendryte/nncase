3614d4e031140b6fd1ed76c129bb03b060eeeeaf
huochenghai <huochenghai@canaan-creative.com>
2023-05-04 17:25:24 +0800

    fix expand

--- src/Nncase.Importer/Onnx/Expand.cs
+++ src/Nncase.Importer/Onnx/Expand.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Canaan Inc. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
+using System;
 using Nncase.IR;
 using Nncase.IR.Tensors;
 using Onnx;
@@ -34,7 +35,24 @@ namespace Nncase.Importer
                 return F.Tensors.Expand(input, F.Tensors.Stack(new IR.Tuple(outputShape), 0));
             }
 
-            return F.Tensors.Expand(input, shape);
+            var rhs = input.CheckedDataType switch
+            {
+                var x when x == DataTypes.UInt8 => F.Tensors.ConstantOfShape(shape, (byte)1),
+                var x when x == DataTypes.UInt16 => F.Tensors.ConstantOfShape(shape, (ushort)1),
+                var x when x == DataTypes.UInt32 => F.Tensors.ConstantOfShape(shape, (uint)1),
+                var x when x == DataTypes.UInt64 => F.Tensors.ConstantOfShape(shape, (ulong)1),
+                var x when x == DataTypes.Int8 => F.Tensors.ConstantOfShape(shape, (sbyte)1),
+                var x when x == DataTypes.Int16 => F.Tensors.ConstantOfShape(shape, (short)1),
+                var x when x == DataTypes.Int32 => F.Tensors.ConstantOfShape(shape, (int)1),
+                var x when x == DataTypes.Int64 => F.Tensors.ConstantOfShape(shape, (long)1),
+                var x when x == DataTypes.Float16 => F.Tensors.ConstantOfShape(shape, (Half)1),
+                var x when x == DataTypes.Float32 => F.Tensors.ConstantOfShape(shape, (float)1),
+                var x when x == DataTypes.Float64 => F.Tensors.ConstantOfShape(shape, (double)1),
+                var x when x == DataTypes.BFloat16 => F.Tensors.ConstantOfShape(shape, (BFloat16)1),
+                var x when x == DataTypes.Boolean => F.Tensors.ConstantOfShape(shape, true),
+                _ => throw new NotSupportedException("not supported expand type"),
+            };
+            return F.Tensors.Expand(input, F.Tensors.ShapeOf(F.Math.Mul(input, rhs)));
         }
     }
 }
