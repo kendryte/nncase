528d81d2349240ab253b866c04ea35ab6d961d0b
huochenghai <huochenghai@canaan-creative.com>
2023-05-06 11:51:38 +0800

    bn to binary

---
+++ src/Nncase.Passes/Rules/Neutral/BatchNormToBinary.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Nncase.IR;
+using Nncase.Passes;
+using Nncase.PatternMatch;
+using static Nncase.IR.F.NN;
+using static Nncase.IR.F.Tensors;
+using static Nncase.IR.TypePatternUtility;
+using static Nncase.PatternMatch.F.Math;
+using static Nncase.PatternMatch.F.NN;
+using static Nncase.PatternMatch.Utility;
+
+namespace Nncase.Passes.Rules.Neutral;
+
+[RuleGenerator]
+public sealed partial class BatchNormToBinary : IRewriteRule
+{
+    /// <inheritdoc/>
+    public IPattern Pattern { get; } = IsBatchNormalization(
+        "bn",
+        "bnCall",
+        _ => true,
+        IsWildcard("input") with { TypePattern = HasFixedShape() },
+        IsTensorConst("gamma"),
+        IsTensorConst("beta"),
+        IsTensorConst("mean"),
+        IsTensorConst("var"),
+        IsTensorConst("eps"));
+
+    private Expr? GetReplace(Expr input, Tensor<float> gamma, Tensor<float> beta, Tensor<float> mean, Tensor<float> var, Tensor<float> eps)
+    {
+        if (input.CheckedShape.Rank <= 1)
+        {
+            return null;
+        }
+
+        var shape = input.CheckedShape.ToValueArray();
+        var bnShape = Enumerable.Repeat(1, shape.Length - 1).ToArray();
+        bnShape[0] = shape[1];
+        var scaleBn = IR.F.Math.Div(gamma, IR.F.Math.Sqrt(IR.F.Math.Add(var, eps)));
+        var biasBn = IR.F.Math.Sub(beta, IR.F.Math.Mul(gamma, IR.F.Math.Div(mean, IR.F.Math.Sqrt(IR.F.Math.Add(var, eps)))));
+
+        return IR.F.Math.Add(IR.F.Math.Mul(input, Reshape(scaleBn, bnShape)), Reshape(biasBn, bnShape));
+    }
+}
---
+++ src/Nncase.Tests/Rules/Neutral/UnitTestBatchNormToBinary.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using NetFabric.Hyperlinq;
+using Nncase.IR;
+using Nncase.IR.Math;
+using Nncase.IR.Tensors;
+using Nncase.Passes;
+using Nncase.Passes.Rules.Neutral;
+using Nncase.PatternMatch;
+using Tensorflow;
+using Xunit;
+using static Nncase.IR.F.NN;
+using ITuple = Nncase.IR.ITuple;
+using Math = Nncase.IR.F.Math;
+using Random = Nncase.IR.F.Random;
+using Tensors = Nncase.IR.F.Tensors;
+using Tuple = System.Tuple;
+
+namespace Nncase.Tests.Rules.NeutralTest;
+
+public class UnitTestBatchNormToBinary : TransformTestBase
+{
+    public static readonly TheoryData<int[]> FoldMatmulAddBNPositiveData = new()
+    {
+        new[] { 56, 56 },
+        new[] { 1, 64, 112, 112 },
+    };
+
+    public static readonly TheoryData<int[]> FoldMatmulAddBNNegativeData = new()
+    {
+        new[] { 56 },
+    };
+
+    [Theory]
+    [MemberData(nameof(FoldMatmulAddBNPositiveData))]
+    public void TestFoldMatmulAddBNPositive(int[] shape)
+    {
+        var a = new Var("input", new TensorType(DataTypes.Float32, shape));
+        var normal = new Dictionary<Var, IValue>();
+        normal.Add(a, Random.Normal(DataTypes.Float32, 0, 1, 0, shape).Evaluate());
+        var oc = shape[1];
+        var bn = IR.F.NN.BatchNormalization(
+            a,
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            0.001f,
+            0.001f);
+        TestMatched<BatchNormToBinary>(bn, normal);
+    }
+
+    [Theory]
+    [MemberData(nameof(FoldMatmulAddBNNegativeData))]
+    public void TestFoldMatmulAddBNNegative(int[] shape)
+    {
+        var a = new Var("input", new TensorType(DataTypes.Float32, shape));
+        var oc = 1;
+        var bn = IR.F.NN.BatchNormalization(
+            a,
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            IR.F.Random.Normal(DataTypes.Float32, 0, 1, 1, new[] { oc }).Evaluate().AsTensor(),
+            0.001f,
+            0.001f);
+        TestNotMatch<BatchNormToBinary>(bn);
+    }
+}
