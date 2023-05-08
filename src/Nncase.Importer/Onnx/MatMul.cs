aa9174008d5f744cf4ab645e3f8162c88f91a508
huochenghai <huochenghai@canaan-creative.com>
2023-05-08 17:52:37 +0800

    update matmul importer

--- src/Nncase.Importer/Onnx/MatMul.cs
+++ src/Nncase.Importer/Onnx/MatMul.cs
@@ -13,7 +13,11 @@ namespace Nncase.Importer
         private Expr VisitMatMul(in NodeProto op)
         {
             var (a, b) = GetInputExprs(op, 0, 1);
-            return F.Tensors.MatMul(a, b);
+            var shapeA = IR.F.Tensors.ShapeOf(a);
+            var shapeB = IR.F.Tensors.ShapeOf(b);
+            var newShapeA = new Expr[] { -1, shapeA[-2], shapeA[-1] };
+            var newShapeB = new Expr[] { -1, shapeB[-2], shapeB[-1] };
+            return F.Tensors.MatMul(IR.F.Tensors.Reshape(a, IR.F.Tensors.Stack(new IR.Tuple(newShapeA), 0)), IR.F.Tensors.Reshape(b, IR.F.Tensors.Stack(new IR.Tuple(newShapeB), 0)));
         }
     }
 }
