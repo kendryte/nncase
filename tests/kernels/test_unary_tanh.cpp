14c10318e0db50259e0cd5f9804500f84b09cc43
hejunchao <hejunchao@canaan-creative.com>
2023-05-16 18:16:39 +0800

    add unary_test

---
+++ tests/kernels/test_unary_tanh.cpp
@@ -0,0 +1,81 @@
+/* Copyright 2019-2021 Canaan Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "kernel_test.h"
+#include <c_api.h>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <nncase/kernels/stackvm/tensor_ops.h>
+#include <nncase/runtime/datatypes.h>
+#include <nncase/runtime/runtime_tensor.h>
+#include <nncase/runtime/simple_types.h>
+#include <nncase/runtime/stackvm/opcode.h>
+#include <operators.h>
+
+using namespace nncase;
+using namespace nncase::runtime;
+using namespace ortki;
+
+class UnaryTest : public KernelTest,
+                   public ::testing::TestWithParam<
+                       std::tuple<nncase::typecode_t, dims_t>> {
+  public:
+    void SetUp() override {
+        auto &&[typecode, i_shape] = GetParam();
+
+        input = hrt::create(typecode, i_shape, host_runtime_tensor::pool_cpu_only)
+                  .expect("create tensor failed");
+        init_tensor(input);
+    }
+
+    void TearDown() override {}
+
+  protected:
+    runtime_tensor input;
+};
+
+INSTANTIATE_TEST_SUITE_P(Unary, UnaryTest,
+                         testing::Combine(testing::Values(dt_float32),
+                                          testing::Values(dims_t{1})));
+
+TEST_P(UnaryTest, tanh) {
+    OrtKITensor *orts[1];
+    orts[0] = runtime_tensor_2_ort_tensor(input);
+
+    // expected
+    auto output_ort = ortki_Tanh(orts[0]);
+    size_t size = 0;
+    void *ptr_ort = tensor_buffer(output_ort, &size);
+    dims_t shape(tensor_rank(output_ort));
+    tensor_shape(output_ort, reinterpret_cast<int64_t *>(shape.data()));
+    auto expected = hrt::create(input.datatype(), shape,
+                                {reinterpret_cast<gsl::byte *>(ptr_ort), size},
+                                true, host_runtime_tensor::pool_cpu_only)
+                        .expect("create tensor failed");
+
+    // actual
+    auto output =
+        kernels::stackvm::unary(nncase::runtime::stackvm::unary_op_t::tanh,
+                                 input.impl())
+            .expect("binary failed");
+    runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
+
+    // compare todo false
+    EXPECT_FALSE(is_same_tensor(expected, actual));
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
