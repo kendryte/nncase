8b9474a0f00963afee59f621d1fc499e48a9ab2e
hejunchao <hejunchao@canaan-creative.com>
2023-05-24 20:06:30 +0800

    fix test's case

--- tests/kernels/test_cum_sum.cpp
+++ tests/kernels/test_cum_sum.cpp
@@ -29,47 +29,37 @@ using namespace ortki;
 
 class CumSumTest : public KernelTest,
                    public ::testing::TestWithParam<
-                       std::tuple<nncase::typecode_t, dims_t, dims_t>> {
+                       std::tuple<nncase::typecode_t, dims_t>> {
   public:
     void SetUp() override {
-        auto &&[typecode, l_shape, r_shape] = GetParam();
+        auto &&[typecode, l_shape] = GetParam();
 
         lhs = hrt::create(typecode, l_shape, host_runtime_tensor::pool_cpu_only)
                   .expect("create tensor failed");
         init_tensor(lhs);
-
-        rhs = hrt::create(typecode, r_shape, host_runtime_tensor::pool_cpu_only)
-                  .expect("create tensor failed");
-        init_tensor(rhs);
     }
 
     void TearDown() override {}
 
   protected:
     runtime_tensor lhs;
-    runtime_tensor rhs;
 };
 
 INSTANTIATE_TEST_SUITE_P(cum_sum, CumSumTest,
-                         testing::Combine(testing::Values(dt_float32, dt_int32,
-                                                          dt_int64),
-                                          testing::Values(dims_t{1, 3, 16, 16},
-                                                          /*dims_t { 3, 16, 16
-                                                          }, dims_t { 16, 16 },
-                                                          dims_t { 16 },*/
-                                                          dims_t{1}),
-                                          testing::Values(dims_t{1, 3, 16, 16},
-                                                          /*dims_t { 3, 16, 16
-                                                          }, dims_t { 16, 16 },
-                                                          dims_t { 16 },*/
-                                                          dims_t{1})));
+                         testing::Combine(testing::Values(dt_float32),
+                                          testing::Values(dims_t{1, 3, 16, 16})));
 
 TEST_P(CumSumTest, cum_sum) {
     auto l_ort = runtime_tensor_2_ort_tensor(lhs);
-    auto r_ort = runtime_tensor_2_ort_tensor(rhs);
 
     // expected
-    auto output_ort = ortki_Add(l_ort, r_ort);
+    float axis[] = {0};
+    auto axis_ptr = hrt::create(nncase::dt_float32, {2},
+                                {reinterpret_cast<gsl::byte *>(axis), sizeof(float)},
+                                true, host_runtime_tensor::pool_cpu_only)
+                        .expect("create tensor failed");
+    auto axis_ort = runtime_tensor_2_ort_tensor(axis_ptr);
+    auto output_ort = ortki_CumSum(l_ort, axis_ort, 0, 0);
     size_t size = 0;
     void *ptr_ort = tensor_buffer(output_ort, &size);
     dims_t shape(tensor_rank(output_ort));
@@ -80,9 +70,18 @@ TEST_P(CumSumTest, cum_sum) {
                         .expect("create tensor failed");
 
     // actual
+    float exclusive[] = {0};
+    auto exclusive_ptr = hrt::create(nncase::dt_float32, {2},
+                                {reinterpret_cast<gsl::byte *>(exclusive), sizeof(float)},
+                                true, host_runtime_tensor::pool_cpu_only)
+                        .expect("create tensor failed");
+    float reverse[] = {0};
+    auto reverse_ptr = hrt::create(nncase::dt_float32, {2},
+                                {reinterpret_cast<gsl::byte *>(reverse), sizeof(float)},
+                                true, host_runtime_tensor::pool_cpu_only)
+                        .expect("create tensor failed");
     auto output =
-        kernels::stackvm::binary(nncase::runtime::stackvm::binary_op_t::add,
-                                 lhs.impl(), rhs.impl())
+        kernels::stackvm::cum_sum(lhs.impl(), axis_ptr.impl(), exclusive_ptr.impl(), reverse_ptr.impl())
             .expect("cum_sum failed");
     runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
 
