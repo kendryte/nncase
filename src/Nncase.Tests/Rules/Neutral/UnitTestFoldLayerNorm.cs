35f99250f05ed94da8cb7e5c005aa8355f29b348
HeJunchao <73169088+Hejunchao6@users.noreply.github.com>
2023-06-21 17:06:18 +0800

    fix FoldLayerNorm (#971)
    * add FoldLayerNormPattern4
    
    * Fix FoldLayerNormPattern4
    
    * fold_layer_norm4's unit test
    
    * debug fold_layer_norm
    
    * registor FoldLayerNormPattern4
    
    ---------
    
    Co-authored-by: hejunchao <hejunchao@canaan-creative.com>
    Co-authored-by: huochenghai <huochenghai@canaan-creative.com>

--- src/Nncase.Tests/Rules/Neutral/UnitTestFoldLayerNorm.cs
+++ src/Nncase.Tests/Rules/Neutral/UnitTestFoldLayerNorm.cs
@@ -215,4 +215,69 @@ public class UnitTestFoldLayerNorm : TransformTestBase
                 new FoldConstCall(),
             });
     }
+
+    [Theory]
+    [MemberData(nameof(FoldLayerNormData))]
+    public void TestFoldLayerNormPositive4(int[] shape)
+    {
+        // note shape is nchw
+        var input = IR.F.Random.Normal(DataTypes.Float32, 0, 1, 4, shape);
+        long[] axes = { 0 };
+        float initValue = 0F;
+        long keepDims = 1;
+        Expr rootPre;
+        {
+            var v0 = input;
+            var v1 = IR.F.Math.Binary(BinaryOp.Add, v0, v0);
+            var v2 = IR.F.Tensors.Reduce(ReduceOp.Mean, v1, Tensor.From(axes, new[] { 1 }), initValue, keepDims);
+            var v3 = IR.F.Math.Binary(BinaryOp.Sub, v1, v2);
+            var v4 = IR.F.Math.Binary(BinaryOp.Mul, v3, v3);
+            var v5 = IR.F.Tensors.Reduce(ReduceOp.Mean, v4, Tensor.From(axes, new[] { 1 }), initValue, keepDims);
+            var v6 = IR.F.Math.Binary(BinaryOp.Add, v5, 1e-05f);
+            var v7 = IR.F.Math.Unary(UnaryOp.Rsqrt, v6);
+            var v8 = IR.F.Math.Binary(BinaryOp.Mul, v7, initValue);
+            var v9 = IR.F.Math.Binary(BinaryOp.Mul, v1, v8);
+            var v10 = IR.F.Math.Binary(BinaryOp.Mul, v2, v8);
+            var v11 = IR.F.Math.Binary(BinaryOp.Sub, initValue, v10);
+            var v12 = IR.F.Math.Binary(BinaryOp.Add, v9, v11);
+            rootPre = v12;
+        }
+
+        TestMatched<FoldLayerNormPattern4>(rootPre);
+    }
+
+    [Theory]
+    [MemberData(nameof(FoldLayerNormData))]
+    public void TestFoldLayerNormNegative4(int[] shape)
+    {
+        // note shape is nchw
+        var input = new Var("input", new TensorType(DataTypes.Float32, shape));
+        long[] axes = { 0 };
+        float initValue = 0F;
+        long keepDims = 1;
+        Expr rootPre;
+        {
+            var v0 = input;
+            var v3 = IR.F.Tensors.Reduce(ReduceOp.Mean, v0, axes, initValue, keepDims);
+            var v4 = IR.F.Math.Binary(BinaryOp.Sub, v0, v3);
+            var v5 = IR.F.Math.Unary(UnaryOp.Sqrt, v4);
+            var v6 = IR.F.Tensors.Reduce(ReduceOp.Mean, v5, Tensor.From(axes, new[] { 1 }), initValue, keepDims);
+            var v7 = IR.F.Math.Binary(BinaryOp.Add, v6, 1e-05f);
+            var v8 = IR.F.Math.Unary(UnaryOp.Rsqrt, v7);
+            var v9 = IR.F.Math.Binary(BinaryOp.Mul, v8, 0.5f);
+            var v10 = IR.F.Math.Binary(BinaryOp.Mul, v0, v9);
+            var v2 = IR.F.Math.Binary(BinaryOp.Mul, v3, 1e-05f);
+            var v1 = IR.F.Math.Binary(BinaryOp.Sub, 0.5f, v2);
+            var v11 = IR.F.Math.Binary(BinaryOp.Add, v10, v1);
+            rootPre = v11;
+        }
+
+        TestNotMatch(
+            rootPre,
+            new IRewriteRule[]
+            {
+                new FoldLayerNormPattern4(),
+                new FoldConstCall(),
+            });
+    }
 }
