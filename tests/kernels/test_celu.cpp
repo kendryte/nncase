60bd0e6bf127cc5d3e3c8284e52ef85c8f2770a3
Hejunchao6 <Hejunchao6@users.noreply.github.com>
2023-07-04 11:34:01 +0000

    Apply code-format changes

--- tests/kernels/test_celu.cpp
+++ tests/kernels/test_celu.cpp
@@ -45,14 +45,12 @@ class CeluTest
     runtime_tensor input;
 };
 
-INSTANTIATE_TEST_SUITE_P(Celu, CeluTest,
-                         testing::Combine(testing::Values(dt_float32),
-                                          testing::Values(dims_t{1},
-                                                          dims_t{1, 2},
-                                                          dims_t{1, 3, 16, 16},
-                                                          dims_t{16, 16},
-                                                          dims_t{3, 16},
-                                                          dims_t{1, 3, 16, 1})));
+INSTANTIATE_TEST_SUITE_P(
+    Celu, CeluTest,
+    testing::Combine(testing::Values(dt_float32),
+                     testing::Values(dims_t{1}, dims_t{1, 2},
+                                     dims_t{1, 3, 16, 16}, dims_t{16, 16},
+                                     dims_t{3, 16}, dims_t{1, 3, 16, 1})));
 
 TEST_P(CeluTest, celu) {
     auto input_ort = runtime_tensor_2_ort_tensor(input);
