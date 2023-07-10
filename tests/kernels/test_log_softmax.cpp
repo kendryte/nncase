dafa81ea6e652efc54f3f940b83bc01e2ee50111
hejunchao <hejunchao@canaan-creative.com>
2023-07-10 17:17:31 +0800

    fix LogSoftmaxTest

--- tests/kernels/test_log_softmax.cpp
+++ tests/kernels/test_log_softmax.cpp
@@ -26,35 +26,46 @@ using namespace nncase;
 using namespace nncase::runtime;
 using namespace ortki;
 
-class LogSoftmaxTest
-    : public KernelTest,
-      public ::testing::TestWithParam<std::tuple<nncase::typecode_t, dims_t>> {
+class LogSoftmaxTest : public KernelTest,
+                       public ::testing::TestWithParam<
+                           std::tuple<nncase::typecode_t, dims_t, int64_t>> {
   public:
     void SetUp() override {
-        auto &&[typecode, l_shape] = GetParam();
+        auto &&[typecode, l_shape, value] = GetParam();
 
         input =
             hrt::create(typecode, l_shape, host_runtime_tensor::pool_cpu_only)
                 .expect("create tensor failed");
         init_tensor(input);
+
+        axis_value = value;
+        int64_t axis_ptr[] = {axis_value};
+        axis = hrt::create(
+                   dt_int64, {1},
+                   {reinterpret_cast<gsl::byte *>(axis_ptr), sizeof(axis_ptr)},
+                   true, host_runtime_tensor::pool_cpu_only)
+                   .expect("create tensor failed");
     }
 
     void TearDown() override {}
 
   protected:
     runtime_tensor input;
+    runtime_tensor axis;
+    int64_t axis_value;
 };
 
 INSTANTIATE_TEST_SUITE_P(LogSoftmax, LogSoftmaxTest,
                          testing::Combine(testing::Values(dt_float32),
-                                          testing::Values(dims_t{1, 3, 16,
-                                                                 16})));
+                                          testing::Values(dims_t{1, 3, 16, 16}),
+                                          testing::Values(0, 1, 2, 3, -4, -3,
+                                                          -2, -1)));
 
 TEST_P(LogSoftmaxTest, log_softmax) {
     auto l_ort = runtime_tensor_2_ort_tensor(input);
 
     // expected
-    auto output_ort = ortki_LogSoftmax(l_ort, -1);
+    auto output_ort = ortki_LogSoftmax(l_ort, axis_value);
     size_t size = 0;
     void *ptr_ort = tensor_buffer(output_ort, &size);
     dims_t shape(tensor_rank(output_ort));
@@ -65,12 +76,7 @@ TEST_P(LogSoftmaxTest, log_softmax) {
                         .expect("create tensor failed");
 
     // actual
-    int64_t axis_ptr[] = {-1};
-    auto axis =
-        hrt::create(dt_int64, {1},
-                    {reinterpret_cast<gsl::byte *>(axis_ptr), sizeof(axis_ptr)},
-                    true, host_runtime_tensor::pool_cpu_only)
-            .expect("create tensor failed");
+
     auto output = kernels::stackvm::log_softmax(input.impl(), axis.impl())
                       .expect("log_softmax failed");
     runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
