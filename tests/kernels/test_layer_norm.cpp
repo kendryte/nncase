c1f0a845029c2b631fbdd2d7aa968c8389697b00
Hejunchao6 <Hejunchao6@users.noreply.github.com>
2023-07-14 08:50:27 +0000

    Apply code-format changes

--- tests/kernels/test_layer_norm.cpp
+++ tests/kernels/test_layer_norm.cpp
@@ -1,17 +1,17 @@
 /* Copyright 2019-2021 Canaan Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #include "kernel_test.h"
 #include <gtest/gtest.h>
 #include <iostream>
@@ -27,79 +27,79 @@ using namespace nncase::runtime;
 using namespace ortki;
 
 class LayerNormTest
-   : public KernelTest,
-     public ::testing::TestWithParam<
-         std::tuple<nncase::typecode_t, dims_t, dims_t, dims_t>> {
- public:
-   void SetUp() override {
-       auto &&[typecode, l_shape, scale_shape, b_shape] = GetParam();
+    : public KernelTest,
+      public ::testing::TestWithParam<
+          std::tuple<nncase::typecode_t, dims_t, dims_t, dims_t>> {
+  public:
+    void SetUp() override {
+        auto &&[typecode, l_shape, scale_shape, b_shape] = GetParam();
 
-       input =
-           hrt::create(typecode, l_shape, host_runtime_tensor::pool_cpu_only)
-               .expect("create tensor failed");
-       init_tensor(input);
+        input =
+            hrt::create(typecode, l_shape, host_runtime_tensor::pool_cpu_only)
+                .expect("create tensor failed");
+        init_tensor(input);
 
-       scale = hrt::create(typecode, scale_shape,
-                           host_runtime_tensor::pool_cpu_only)
-                   .expect("create tensor failed");
-       init_tensor(scale);
+        scale = hrt::create(typecode, scale_shape,
+                            host_runtime_tensor::pool_cpu_only)
+                    .expect("create tensor failed");
+        init_tensor(scale);
 
-       b = hrt::create(typecode, b_shape, host_runtime_tensor::pool_cpu_only)
-               .expect("create tensor failed");
-       init_tensor(b);
-   }
+        b = hrt::create(typecode, b_shape, host_runtime_tensor::pool_cpu_only)
+                .expect("create tensor failed");
+        init_tensor(b);
+    }
 
-   void TearDown() override {}
+    void TearDown() override {}
 
- protected:
-   runtime_tensor input;
-   runtime_tensor scale;
-   runtime_tensor b;
+  protected:
+    runtime_tensor input;
+    runtime_tensor scale;
+    runtime_tensor b;
 };
 
 INSTANTIATE_TEST_SUITE_P(LayerNorm, LayerNormTest,
-                        testing::Combine(testing::Values(dt_float32),
-                                         testing::Values(dims_t{1, 3, 16, 16}),
-                                         testing::Values(dims_t{16}),
-                                         testing::Values(dims_t{16})));
+                         testing::Combine(testing::Values(dt_float32),
+                                          testing::Values(dims_t{1, 3, 16, 16}),
+                                          testing::Values(dims_t{16}),
+                                          testing::Values(dims_t{16})));
 
 TEST_P(LayerNormTest, layer_norm) {
-   auto l_ort = runtime_tensor_2_ort_tensor(input);
-   auto scale_ort = runtime_tensor_2_ort_tensor(scale);
-   auto b_ort = runtime_tensor_2_ort_tensor(b);
+    auto l_ort = runtime_tensor_2_ort_tensor(input);
+    auto scale_ort = runtime_tensor_2_ort_tensor(scale);
+    auto b_ort = runtime_tensor_2_ort_tensor(b);
 
-   //     expected
-   auto output_ort =
-       ortki_LayerNormalization(l_ort, scale_ort, b_ort, 3, 1e-05f, 1L);
-   size_t size = 0;
-   void *ptr_ort = tensor_buffer(tensor_seq_get_value(output_ort, 0), &size);
-   dims_t shape(tensor_rank(tensor_seq_get_value(output_ort, 0)));
-   tensor_shape(tensor_seq_get_value(output_ort, 0),
-                reinterpret_cast<int64_t *>(shape.data()));
-   auto expected = hrt::create(input.datatype(), shape,
-                               {reinterpret_cast<gsl::byte *>(ptr_ort), size},
-                               true, host_runtime_tensor::pool_cpu_only)
-                       .expect("create tensor failed");
+    //     expected
+    auto output_ort =
+        ortki_LayerNormalization(l_ort, scale_ort, b_ort, 3, 1e-05f, 1L);
+    size_t size = 0;
+    void *ptr_ort = tensor_buffer(tensor_seq_get_value(output_ort, 0), &size);
+    dims_t shape(tensor_rank(tensor_seq_get_value(output_ort, 0)));
+    tensor_shape(tensor_seq_get_value(output_ort, 0),
+                 reinterpret_cast<int64_t *>(shape.data()));
+    auto expected = hrt::create(input.datatype(), shape,
+                                {reinterpret_cast<gsl::byte *>(ptr_ort), size},
+                                true, host_runtime_tensor::pool_cpu_only)
+                        .expect("create tensor failed");
 
-   // actual
-   auto output = kernels::stackvm::layer_norm(3, 1e-05f, input.impl(),
-                                              scale.impl(), b.impl())
-                     .expect("layer_norm failed");
-   runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
+    // actual
+    auto output = kernels::stackvm::layer_norm(3, 1e-05f, input.impl(),
+                                               scale.impl(), b.impl())
+                      .expect("layer_norm failed");
+    runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
 
-   bool result = is_same_tensor(expected, actual) ||
-                 cosine_similarity_tensor(expected, actual);
+    bool result = is_same_tensor(expected, actual) ||
+                  cosine_similarity_tensor(expected, actual);
 
-   if (!result) {
-       print_runtime_tensor(actual);
-       print_runtime_tensor(expected);
-   }
+    if (!result) {
+        print_runtime_tensor(actual);
+        print_runtime_tensor(expected);
+    }
 
-   // compare
-   EXPECT_TRUE(result);
+    // compare
+    EXPECT_TRUE(result);
 }
 
 int main(int argc, char *argv[]) {
-   ::testing::InitGoogleTest(&argc, argv);
-   return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
