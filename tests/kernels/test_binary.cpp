db53659d2f2295a5d4ea804bd7df87466668277f
乐仁华 <2532375005@qq.com>
2023-07-14 02:08:02 +0000

    Unified binary op test

--- tests/kernels/test_binary.cpp
+++ tests/kernels/test_binary.cpp
@@ -28,14 +28,15 @@ using namespace nncase;
 using namespace nncase::runtime;
 using namespace ortki;
 using namespace nncase::runtime::stackvm;
-// using namespace nncase::runtime::kernels::stackvm;
 
 NNCASE_TEST_CLASS(BinaryTest)
 
 NNCASE_TESTSUITE_INIT(BinaryTest, Binary, 
     3, dt_float32, dt_int32, dt_int64, 
-    dims_t{1}, dims_t{16}, dims_t{1, 16}, 
-    dims_t{1, 16, 16}, dims_t{3, 3, 1, 16})
+    dims_t{1, 3, 16, 16}, dims_t{3, 16, 16},
+    dims_t{3, 16, 1}, dims_t{16, 16},
+    dims_t{16, 1}, dims_t{1, 16, 1},
+    dims_t{16}, dims_t{1}, dims_t{})
 
 NNCASE_TEST_BODY(BinaryTest, add, kernels::stackvm::binary, binary_op_t::add, 1, NORMAL, ortki_Add, l_ort, r_ort)
 NNCASE_TEST_BODY(BinaryTest, sub, kernels::stackvm::binary, binary_op_t::sub, 1, NORMAL, ortki_Sub, l_ort, r_ort)
@@ -45,14 +46,9 @@ NNCASE_TEST_BODY(BinaryTest, mod, kernels::stackvm::binary, binary_op_t::mod, 1,
 NNCASE_TEST_BODY(BinaryTest, min, kernels::stackvm::binary, binary_op_t::min, 1, VEC, ortki_Min, orts, sizeof(orts) / sizeof(orts[0]))
 NNCASE_TEST_BODY(BinaryTest, max, kernels::stackvm::binary, binary_op_t::max, 1, VEC, ortki_Max, orts, sizeof(orts) / sizeof(orts[0]))
 NNCASE_TEST_BODY(BinaryTest, pow, kernels::stackvm::binary, binary_op_t::pow, 1, NORMAL, ortki_Pow, l_ort, r_ort)
-// NNCASE_TEST_BODY(BinaryTest, bitwise_and, kernels::stackvm::binary, binary_op_t::bitwise_and, 1, NORMAL, ortki_Mul)
-// NNCASE_TEST_BODY(BinaryTest, bitwise_or, kernels::stackvm::binary, binary_op_t::bitwise_or, 1, NORMAL, ortki_Mul)
-// NNCASE_TEST_BODY(BinaryTest, bitwise_xor, kernels::stackvm::binary, binary_op_t::bitwise_xor, 1, NORMAL, ortki_Mul)
 NNCASE_TEST_BODY(BinaryTest, logical_and, kernels::stackvm::binary, binary_op_t::logical_and, 1, NORMAL, ortki_And, l_ort, r_ort)
 NNCASE_TEST_BODY(BinaryTest, logical_or, kernels::stackvm::binary, binary_op_t::logical_or, 1, NORMAL, ortki_Or, l_ort, r_ort)
 NNCASE_TEST_BODY(BinaryTest, logical_xor, kernels::stackvm::binary, binary_op_t::logical_xor, 1, NORMAL, ortki_Xor, l_ort, r_ort)
-// NNCASE_TEST_BODY(BinaryTest, left_shift, kernels::stackvm::binary, binary_op_t::left_shift, 1, NORMAL, ortki_Mul)
-// NNCASE_TEST_BODY(BinaryTest, right_shift, kernels::stackvm::binary, binary_op_t::right_shift, 1, NORMAL, ortki_Mul)
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
