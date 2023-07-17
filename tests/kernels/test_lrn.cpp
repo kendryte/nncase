1ed56f96da0a1071d3a9654bc7441cd3885033a7
Hejunchao6 <Hejunchao6@users.noreply.github.com>
2023-07-17 04:31:15 +0000

    Apply code-format changes

--- tests/kernels/test_lrn.cpp
+++ tests/kernels/test_lrn.cpp
@@ -47,7 +47,8 @@ class LrnTest
 
 INSTANTIATE_TEST_SUITE_P(lrn, LrnTest,
                          testing::Combine(testing::Values(dt_float32),
-                                          testing::Values(dims_t{1, 3, 16, 16}, dims_t{1, 3, 8, 8})));
+                                          testing::Values(dims_t{1, 3, 16, 16},
+                                                          dims_t{1, 3, 8, 8})));
 
 TEST_P(LrnTest, lrn) {
     auto l_ort = runtime_tensor_2_ort_tensor(input);
