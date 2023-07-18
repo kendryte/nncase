8230ffb557da797682d691aafe00920cf962a068
hejunchao <hejunchao@canaan-creative.com>
2023-07-18 19:31:24 +0800

    fix test_pad.cpp

--- tests/kernels/test_pad.cpp
+++ tests/kernels/test_pad.cpp
@@ -47,13 +47,15 @@ class PadTest
 
 INSTANTIATE_TEST_SUITE_P(Pad, PadTest,
                          testing::Combine(testing::Values(dt_uint8),
-                                          testing::Values(dims_t{1, 3, 24, 24})));
+                                          testing::Values(dims_t{1, 3, 24, 24},
+                                                          dims_t{1, 3, 16,
+                                                                 16})));
 
 TEST_P(PadTest, Pad) {
 
     // expected
     size_t size = 0;
-    int64_t pad_ptr[] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int64_t pad_ptr[] = {1, 0, 0, 0, 0, 0, 0, 0};
     auto pad =
         hrt::create(dt_int64, {8},
                     {reinterpret_cast<gsl::byte *>(pad_ptr), sizeof(pad_ptr)},
