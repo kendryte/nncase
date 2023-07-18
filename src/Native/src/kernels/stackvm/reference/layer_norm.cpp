ab4ac408718c2a713e36dc69cfb59ffdecef2cc6
zhiming.liu <liuzhiming@canaan-creative.com>
2023-07-18 02:46:15 +0000

    Fix layernorm on x86

--- src/Native/src/kernels/stackvm/reference/layer_norm.cpp
+++ src/Native/src/kernels/stackvm/reference/layer_norm.cpp
@@ -21,7 +21,7 @@
 using namespace nncase;
 using namespace nncase::kernels::stackvm;
 
-static void layernorm_step1(int inner_size, const float *src,
+static void layernorm_impl(int inner_size, const float *src,
                             const float *scale, const float *bias,
                             float epsilon, float *dst) {
     float mean1 = 0.f;
@@ -51,81 +51,26 @@ static void layernorm_step1(int inner_size, const float *src,
         dst[i] = div[i] * scale[i] + bias[i];
 }
 
-static void layernorm_step_not1(int inner_size, const float *src,
-                                const float *scale, const float *bias,
-                                float epsilon, float *dst, int step) {
-    float mean1 = 0.f;
-    for (auto i = 0; i < inner_size; i++)
-        mean1 += src[i * step] / inner_size;
-
-    std::vector<float> sub(inner_size, 0.f);
-    for (auto i = 0; i < inner_size; i++)
-        sub[i] = src[i * step] - mean1;
-
-    std::vector<float> pow(inner_size, 0.f);
-    for (auto i = 0; i < inner_size; i++)
-        pow[i] = sub[i] * sub[i];
-
-    float mean2 = 0.f;
-    for (auto i = 0; i < inner_size; i++)
-        mean2 += pow[i] / inner_size;
-
-    float add = mean2 + epsilon;
-    float sqrt = std::sqrt(add);
-
-    std::vector<float> div(inner_size, 0.f);
-    for (auto i = 0; i < inner_size; i++)
-        div[i] = sub[i] / sqrt;
-
-    for (auto i = 0; i < inner_size; i++)
-        dst[i * step] = div[i] * scale[i * step] + bias[i * step];
-
-    printf("mean1:%f, mean2:%f\n", mean1, mean2);
-    (void)scale;
-    (void)bias;
-}
-
 result<void> nncase::kernels::stackvm::reference::layer_norm(
     const float *input, float *output, const float *scale, const float *bias,
     gsl::span<const size_t> in_shape, int32_t axis, float epsilon) {
 
     int ndim = in_shape.size();
     int positive_axis = axis < 0 ? ndim + axis : axis;
-    int axis_dim = in_shape[positive_axis];
+    int axis_dim = 1; // in_shape[positive_axis];
 
     size_t out_side = 1;
     for (size_t i = 0; i < positive_axis; i++)
         out_side *= in_shape[i];
 
-    size_t in_side = 1;
-    for (size_t i = positive_axis + 1; i < ndim; i++) {
-        in_side *= in_shape[i];
-    }
-    printf(
-        "----------------layer norm axis: %d, dim:%d,inner:%d,outter: %d, %f\n",
-        positive_axis, (int)axis_dim, (int)in_side, (int)out_side, epsilon);
-    if (positive_axis == (ndim - 1)) {
-        for (size_t i = 0; i < out_side; i++) {
-            layernorm_step1(axis_dim, input, scale, bias, epsilon, output);
-            input += axis_dim;
-            output += axis_dim;
-        }
-    } else {
-        const float *ptr_input = input;
-        float *ptr_output = output;
-        for (size_t i = 0; i < out_side; i++) {
-            const float *in = ptr_input;
-            float *out = ptr_output;
-            for (int i = 0; i < in_side; ++i) {
-                layernorm_step_not1(axis_dim, in, scale, bias, epsilon, out,
-                                    in_side);
-                in += 1;
-                out += 1;
-            }
-            ptr_input += axis_dim * in_side;
-            ptr_output += axis_dim * in_side;
-        }
+    for (size_t i = positive_axis; i < ndim; i++) {
+        axis_dim *= in_shape[i];
     }
 
+    for (size_t i = 0; i < out_side; i++) {
+        layernorm_impl(axis_dim, input, scale, bias, epsilon, output);
+        input += axis_dim;
+        output += axis_dim;
+    }
     return ok();
 }
