ebe45fd4555762832b96f9b5d1956b01b272bade
huochenghai <huochenghai@canaan-creative.com>
2023-07-25 15:52:50 +0800

    add bianry method table

--- modules/Nncase.Modules.CPU/CodeGen/CSourceBuiltn.cs
+++ modules/Nncase.Modules.CPU/CodeGen/CSourceBuiltn.cs
@@ -16,6 +16,7 @@ public static class CSourceBuiltn
 } buffer_t;";
 
     public const string MethodTable = @"typedef struct nncase_method_table {
+    // float unary
     float (*float_unary_abs)(float);
     float (*float_unary_acos)(float);
     float (*float_unary_acosh)(float);
@@ -37,9 +38,44 @@ public static class CSourceBuiltn
     float (*float_unary_sqrt)(float);
     float (*float_unary_square)(float);
     float (*float_unary_tanh)(float);
+    // float bianry
+    float (*float_binary_add)(float, float);
+    float (*float_binary_sub)(float, float);
+    float (*float_binary_mul)(float, float);
+    float (*float_binary_div)(float, float);
+    float (*float_binary_min)(float, float);
+    float (*float_binary_max)(float, float);
+    float (*float_binary_pow)(float, float);
+    float (*float_binary_logical_and)(float, float);
+    float (*float_binary_mod)(float, float);
+    // int32 bianry
+    int32_t (*int32_binary_add)(int32_t, int32_t);
+    int32_t (*int32_binary_sub)(int32_t, int32_t);
+    int32_t (*int32_binary_mul)(int32_t, int32_t);
+    int32_t (*int32_binary_div)(int32_t, int32_t);
+    int32_t (*int32_binary_min)(int32_t, int32_t);
+    int32_t (*int32_binary_max)(int32_t, int32_t);
+    int32_t (*int32_binary_pow)(int32_t, int32_t);
+    int32_t (*int32_binary_logical_and)(int32_t, int32_t);
+    int32_t (*int32_binary_mod)(int32_t, int32_t);
+    // int64 bianry
+    int64_t (*int64_binary_add)(int64_t, int64_t);
+    int64_t (*int64_binary_sub)(int64_t, int64_t);
+    int64_t (*int64_binary_mul)(int64_t, int64_t);
+    int64_t (*int64_binary_div)(int64_t, int64_t);
+    int64_t (*int64_binary_min)(int64_t, int64_t);
+    int64_t (*int64_binary_max)(int64_t, int64_t);
+    int64_t (*int64_binary_pow)(int64_t, int64_t);
+    int64_t (*int64_binary_logical_and)(int64_t, int64_t);
+    int64_t (*int64_binary_mod)(int64_t, int64_t);
+    // bool binary
+    bool (*bool_binary_and)(bool, bool);
+    bool (*bool_binary_or)(bool, bool);
+    bool (*bool_binary_xor)(bool, bool);
 } nncase_mt_t;";
 
-    public const string Include = @"#include <stdint.h>
+    public const string Include = @"#include <stdbool.h>
+#include <stdint.h>
 #include <stddef.h>
 ";
 
--- modules/cpu/src/runtime/cpu_common.h
+++ modules/cpu/src/runtime/cpu_common.h
@@ -2,12 +2,14 @@
 #include <functional>
 #include <math.h>
 #include <nncase/runtime/cpu/compiler_defs.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
 BEGIN_NS_NNCASE_RT_MODULE(cpu)
 
 typedef struct nncase_method_table {
+    // float unary
     float (*float_unary_abs)(float);
     float (*float_unary_acos)(float);
     float (*float_unary_acosh)(float);
@@ -29,6 +31,40 @@ typedef struct nncase_method_table {
     float (*float_unary_sqrt)(float);
     float (*float_unary_square)(float);
     float (*float_unary_tanh)(float);
+    // float bianry
+    float (*float_binary_add)(float, float);
+    float (*float_binary_sub)(float, float);
+    float (*float_binary_mul)(float, float);
+    float (*float_binary_div)(float, float);
+    float (*float_binary_min)(float, float);
+    float (*float_binary_max)(float, float);
+    float (*float_binary_pow)(float, float);
+    float (*float_binary_logical_and)(float, float);
+    float (*float_binary_mod)(float, float);
+    // int32 bianry
+    int32_t (*int32_binary_add)(int32_t, int32_t);
+    int32_t (*int32_binary_sub)(int32_t, int32_t);
+    int32_t (*int32_binary_mul)(int32_t, int32_t);
+    int32_t (*int32_binary_div)(int32_t, int32_t);
+    int32_t (*int32_binary_min)(int32_t, int32_t);
+    int32_t (*int32_binary_max)(int32_t, int32_t);
+    int32_t (*int32_binary_pow)(int32_t, int32_t);
+    int32_t (*int32_binary_logical_and)(int32_t, int32_t);
+    int32_t (*int32_binary_mod)(int32_t, int32_t);
+    // int64 bianry
+    int64_t (*int64_binary_add)(int64_t, int64_t);
+    int64_t (*int64_binary_sub)(int64_t, int64_t);
+    int64_t (*int64_binary_mul)(int64_t, int64_t);
+    int64_t (*int64_binary_div)(int64_t, int64_t);
+    int64_t (*int64_binary_min)(int64_t, int64_t);
+    int64_t (*int64_binary_max)(int64_t, int64_t);
+    int64_t (*int64_binary_pow)(int64_t, int64_t);
+    int64_t (*int64_binary_logical_and)(int64_t, int64_t);
+    int64_t (*int64_binary_mod)(int64_t, int64_t);
+    // bool binary
+    bool (*bool_binary_and)(bool, bool);
+    bool (*bool_binary_or)(bool, bool);
+    bool (*bool_binary_xor)(bool, bool);
 } nncase_mt_t;
 
 typedef struct buffer {
@@ -44,6 +80,40 @@ inline float float_unary_neg(float x) { return std::negate<float>()(x); }
 inline float float_unary_rsqrt(float x) { return 1.f / sqrtf(x); }
 inline float float_unary_sign(float x) { return (0.f < x) - (x < 0.f); }
 inline float float_unary_square(float x) { return x * x; }
+
+inline float float_binary_add(float x, float y) { return x + y; }
+inline float float_binary_sub(float x, float y) { return x - y; }
+inline float float_binary_mul(float x, float y) { return x * y; }
+inline float float_binary_div(float x, float y) { return x / y; }
+inline float float_binary_min(float x, float y) { return std::min(x, y); }
+inline float float_binary_max(float x, float y) { return std::max(x, y); }
+inline float float_binary_pow(float x, float y) { return powf(x, y); }
+inline float float_binary_logical_and(float x, float y) { return x && y; }
+inline float float_binary_mod(float x, float y) { return fmod(x, y); }
+
+inline int32_t int32_binary_add(int32_t x, int32_t y) { return x + y; }
+inline int32_t int32_binary_sub(int32_t x, int32_t y) { return x - y; }
+inline int32_t int32_binary_mul(int32_t x, int32_t y) { return x * y; }
+inline int32_t int32_binary_div(int32_t x, int32_t y) { return x / y; }
+inline int32_t int32_binary_min(int32_t x, int32_t y) { return std::min(x, y); }
+inline int32_t int32_binary_max(int32_t x, int32_t y) { return std::max(x, y); }
+inline int32_t int32_binary_pow(int32_t x, int32_t y) { return std::pow(x, y); }
+inline int32_t int32_binary_logical_and(int32_t x, int32_t y) { return x && y; }
+inline int32_t int32_binary_mod(int32_t x, int32_t y) { return x % y; }
+
+inline int64_t int64_binary_add(int64_t x, int64_t y) { return x + y; }
+inline int64_t int64_binary_sub(int64_t x, int64_t y) { return x - y; }
+inline int64_t int64_binary_mul(int64_t x, int64_t y) { return x * y; }
+inline int64_t int64_binary_div(int64_t x, int64_t y) { return x / y; }
+inline int64_t int64_binary_min(int64_t x, int64_t y) { return std::min(x, y); }
+inline int64_t int64_binary_max(int64_t x, int64_t y) { return std::max(x, y); }
+inline int64_t int64_binary_pow(int64_t x, int64_t y) { return std::pow(x, y); }
+inline int64_t int64_binary_logical_and(int64_t x, int64_t y) { return x && y; }
+inline int64_t int64_binary_mod(int64_t x, int64_t y) { return x % y; }
+
+inline bool bool_binary_logical_and(bool x, bool y) { return x && y; }
+inline bool bool_binary_logical_or(bool x, bool y) { return x || y; }
+inline bool bool_binary_logical_xor(bool x, bool y) { return x ^ y; }
 
 [[maybe_unused]] static nncase_mt_t nncase_mt = {
     .float_unary_abs = fabsf,
@@ -66,6 +136,36 @@ inline float float_unary_square(float x) { return x * x; }
     .float_unary_sinh = sinhf,
     .float_unary_sqrt = sqrtf,
     .float_unary_square = float_unary_square,
-    .float_unary_tanh = tanhf};
+    .float_unary_tanh = tanhf,
+    .float_binary_add = float_binary_add,
+    .float_binary_sub = float_binary_sub,
+    .float_binary_mul = float_binary_mul,
+    .float_binary_div = float_binary_div,
+    .float_binary_min = float_binary_min,
+    .float_binary_max = float_binary_max,
+    .float_binary_pow = float_binary_pow,
+    .float_binary_logical_and = float_binary_logical_and,
+    .float_binary_mod = float_binary_mod,
+    .int32_binary_add = int32_binary_add,
+    .int32_binary_sub = int32_binary_sub,
+    .int32_binary_mul = int32_binary_mul,
+    .int32_binary_div = int32_binary_div,
+    .int32_binary_min = int32_binary_min,
+    .int32_binary_max = int32_binary_max,
+    .int32_binary_pow = int32_binary_pow,
+    .int32_binary_logical_and = int32_binary_logical_and,
+    .int32_binary_mod = int32_binary_mod,
+    .int64_binary_add = int64_binary_add,
+    .int64_binary_sub = int64_binary_sub,
+    .int64_binary_mul = int64_binary_mul,
+    .int64_binary_div = int64_binary_div,
+    .int64_binary_min = int64_binary_min,
+    .int64_binary_max = int64_binary_max,
+    .int64_binary_pow = int64_binary_pow,
+    .int64_binary_logical_and = int64_binary_logical_and,
+    .int64_binary_mod = int64_binary_mod,
+    .bool_binary_and = bool_binary_logical_and,
+    .bool_binary_or = bool_binary_logical_or,
+    .bool_binary_xor = bool_binary_logical_xor};
 
 END_NS_NNCASE_RT_MODULE
