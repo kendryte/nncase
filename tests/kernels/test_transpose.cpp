32ac7e94c2052d291658ef70adb051c002301467
hejunchao <hejunchao@canaan-creative.com>
2023-07-27 20:48:04 +0800

    resolve

--- tests/kernels/test_transpose.cpp
+++ tests/kernels/test_transpose.cpp
@@ -26,79 +26,101 @@ using namespace nncase;
 using namespace nncase::runtime;
 using namespace ortki;
 
-class TransposeTest
-    : public KernelTest,
-      public ::testing::TestWithParam<std::tuple<nncase::typecode_t, dims_t>> {
+class TransposeTest : public KernelTest,
+                       public ::testing::TestWithParam<
+                           std::tuple<nncase::typecode_t, dims_t, axes_t>> {
   public:
     void SetUp() override {
-        auto &&[typecode, l_shape] = GetParam();
+        auto &&[typecode, l_shape, value] = GetParam();
 
         input =
             hrt::create(typecode, l_shape, host_runtime_tensor::pool_cpu_only)
                 .expect("create tensor failed");
         init_tensor(input);
+
+        perm_value = value;
     }
 
     void TearDown() override {}
 
   protected:
     runtime_tensor input;
+    axes_t perm_value;
 };
 
 INSTANTIATE_TEST_SUITE_P(
     Transpose, TransposeTest,
-    testing::Combine(testing::Values(dt_float32, dt_int32, dt_int16, dt_float64,
-                                     dt_int8, dt_uint8, dt_uint16, dt_uint32,
-                                     dt_uint64, dt_int64, dt_bfloat16,
-                                     dt_float16, dt_boolean),
-                     testing::Values(dims_t{1, 3, 16, 16}, dims_t{1, 2, 4, 8},
-                                     dims_t{2, 2, 4, 4})));
+    testing::Combine(
+        testing::Values(dt_float32, dt_int32, dt_int16, dt_float64, dt_int8,
+                        dt_uint8, dt_uint16, dt_uint32, dt_uint64, dt_int64,
+                        dt_bfloat16, dt_float16, dt_boolean),
+        testing::Values(dims_t{1, 3, 16, 16}, dims_t{1, 2, 4, 8},
+                        dims_t{2, 2, 4, 4}, dims_t{1, 3, 16}, dims_t{1, 2, 4},
+                        dims_t{2, 2, 4}, dims_t{16, 16}, dims_t{2, 4},
+                        dims_t{4, 4}),
+        testing::Values(
+            axes_t{0, 1, 2, 3}, axes_t{0, 1, 3, 2}, axes_t{0, 2, 1, 3},
+            axes_t{0, 2, 3, 1}, axes_t{0, 3, 1, 2}, axes_t{0, 3, 2, 1},
+            axes_t{1, 0, 3, 2}, axes_t{1, 3, 2, 0}, axes_t{1, 0, 2, 3},
+            axes_t{1, 2, 0, 3}, axes_t{1, 2, 3, 0}, axes_t{1, 3, 0, 2},
+            axes_t{2, 0, 1, 3}, axes_t{2, 0, 3, 1}, axes_t{2, 1, 0, 3},
+            axes_t{2, 1, 3, 0}, axes_t{2, 3, 0, 1}, axes_t{2, 3, 1, 0},
+            axes_t{3, 0, 1, 2}, axes_t{3, 0, 2, 1}, axes_t{3, 1, 2, 0},
+            axes_t{3, 1, 0, 2}, axes_t{3, 2, 1, 0}, axes_t{3, 2, 0, 1},
+            axes_t{0, 1, 2}, axes_t{0, 2, 1}, axes_t{1, 0, 2}, axes_t{1, 2, 0},
+            axes_t{2, 0, 1}, axes_t{2, 1, 0}, axes_t{0, 1}, axes_t{1, 0})));
 
 TEST_P(TransposeTest, Transpose) {
-    auto input_ort = runtime_tensor_2_ort_tensor(input);
-    int64_t perm[] = {1, 0, 3, 2};
-    size_t perm_size = 4;
 
-    // expected
-    auto output_ort = ortki_Transpose(input_ort, perm, perm_size);
-    size_t size = 0;
-    void *ptr_ort = tensor_buffer(output_ort, &size);
-    dims_t shape(tensor_rank(output_ort));
-    tensor_shape(output_ort, reinterpret_cast<int64_t *>(shape.data()));
-    auto expected = hrt::create(input.datatype(), shape,
-                                {reinterpret_cast<gsl::byte *>(ptr_ort), size},
-                                true, host_runtime_tensor::pool_cpu_only)
-                        .expect("create tensor failed");
+    if (input.shape().size() == perm_value.size()) {
+        size_t perm_size = perm_value.size();
+        int64_t *perm = (int64_t *)malloc(perm_size * sizeof(int64_t));
+        std::copy(perm_value.begin(), perm_value.end(), perm);
+        auto input_ort = runtime_tensor_2_ort_tensor(input);
 
-    auto perm1 =
-        hrt::create(nncase::dt_int64, {4},
-                    {reinterpret_cast<gsl::byte *>(perm), sizeof(perm)}, true,
-                    host_runtime_tensor::pool_cpu_only)
-            .expect("create tensor failed");
+        auto perm_ptr = hrt::create(nncase::dt_int64, {perm_size},
+                                    {reinterpret_cast<gsl::byte *>(perm),
+                                     perm_size * sizeof(int64_t)},
+                                    true, host_runtime_tensor::pool_cpu_only)
+                            .expect("create tensor failed");
 
-    int32_t perm_size_ptr[] = {4};
-    auto perm_size1 = hrt::create(nncase::dt_int32, {1},
-                                  {reinterpret_cast<gsl::byte *>(perm_size_ptr),
-                                   sizeof(perm_size_ptr)},
-                                  true, host_runtime_tensor::pool_cpu_only)
-                          .expect("create tensor failed");
+        // expected
+        auto output_ort = ortki_Transpose(input_ort, perm, perm_size);
+        size_t size = 0;
+        void *ptr_ort = tensor_buffer(output_ort, &size);
+        dims_t shape(tensor_rank(output_ort));
+        tensor_shape(output_ort, reinterpret_cast<int64_t *>(shape.data()));
+        auto expected =
+            hrt::create(input.datatype(), shape,
+                        {reinterpret_cast<gsl::byte *>(ptr_ort), size}, true,
+                        host_runtime_tensor::pool_cpu_only)
+                .expect("create tensor failed");
 
-    auto output = kernels::stackvm::transpose(input.impl(), perm1.impl())
-                      .expect("transpose failed");
-    runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
+        int32_t perm_size_ptr[] = {4};
+        auto perm_size1 =
+            hrt::create(nncase::dt_int32, {1},
+                        {reinterpret_cast<gsl::byte *>(perm_size_ptr),
+                         sizeof(perm_size_ptr)},
+                        true, host_runtime_tensor::pool_cpu_only)
+                .expect("create tensor failed");
 
-    bool result = is_same_tensor(expected, actual) ||
-                  cosine_similarity_tensor(expected, actual);
+        auto output = kernels::stackvm::transpose(input.impl(), perm_ptr.impl())
+                          .expect("transpose failed");
+        runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
 
-    if (!result) {
-        std::cout << "actual ";
-        print_runtime_tensor(actual);
-        std::cout << "expected ";
-        print_runtime_tensor(expected);
+        bool result = is_same_tensor(expected, actual) ||
+                      cosine_similarity_tensor(expected, actual);
+
+        if (!result) {
+            std::cout << "actual ";
+            print_runtime_tensor(actual);
+            std::cout << "expected ";
+            print_runtime_tensor(expected);
+        }
+
+        // compare
+        EXPECT_TRUE(result);
     }
-
-    // compare
-    EXPECT_TRUE(result);
 }
 
 int main(int argc, char *argv[]) {
