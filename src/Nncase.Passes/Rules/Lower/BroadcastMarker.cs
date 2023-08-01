8a22585605ee3bd034429b3cd73464c5144b0b95
FusionBolt <realfusionbolt@gmail.com>
2023-08-01 16:14:22 +0800

    add broadcast marker

---
+++ src/Nncase.Passes/Rules/Lower/BroadcastMarker.cs
@@ -0,0 +1,62 @@
+﻿using Nncase.IR;
+using Nncase.IR.Tensors;
+using Nncase.PatternMatch;
+using Nncase.Utilities;
+using static Nncase.PatternMatch.Utility;
+using static Nncase.Utilities.ReplaceUtility;
+using static Nncase.Passes.Rules.Lower.BroadcastMarkerHelper;
+namespace Nncase.Passes.Rules.Lower;
+
+internal static class BroadcastMarkerHelper
+{
+    public static bool NotChangeRangeOp(Expr op)
+    {
+        return op is Squeeze || op is Unsqueeze || op is Reshape;
+    }
+}
+
+// e.g. matmul(reshape(marker(x))) -> matmul(marker(reshape(marker(x))))
+[RuleGenerator]
+public partial class BroadcastInputMarker : RewriteRule<Pattern>
+{
+    override public Pattern Pattern => IsCallWildcard(
+        "outer",
+        IsWildcard(),
+        IsCallWildcard(
+            "call",
+            IsWildcard(),
+            IsRangeOfMarker(
+                "marker",
+                IsWildcard(),
+                IsWildcard())));
+
+    public Expr? GetReplace(Call outer, Call call, Marker marker)
+    {
+        if (!NotChangeRangeOp(call.Target))
+        {
+            return null;
+        }
+
+        return ReplaceCallFirstParam(outer, marker.With(target: ReplaceCallFirstParam(call, marker)));
+    }
+}
+
+// e.g. marker(reshape(matmul(x))) -> marker(reshape(marker(matmul(x))))
+[RuleGenerator]
+public partial class BroadcastOutputMarker : RewriteRule<Pattern>
+{
+    override public Pattern Pattern => IsRangeOfMarker(
+        "marker",
+        IsCallWildcard("input", IsWildcard(), IsCallWildcard(null, IsWildcard())),
+        IsWildcard());
+
+    public Expr? GetReplace(Call input, Marker marker)
+    {
+        if (!NotChangeRangeOp(input.Target))
+        {
+            return null;
+        }
+
+        return ReplaceCallFirstParam(input, marker.With(target: input.Arguments[0]));
+    }
+}
---
+++ src/Nncase.Tests/Rules/UnitTestBroadcastMarker.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using Nncase.IR;
+using Nncase.Passes.Rules.Lower;
+using Nncase.Tests.TestFixture;
+using Xunit;
+using static Nncase.IR.F.Math;
+using static Nncase.IR.F.Tensors;
+
+namespace Nncase.Tests.Rules;
+
+[AutoSetupTestMethod(InitSession = true)]
+public class UnitTestBroadcastMarker : TransformTestBase
+{
+    [Fact]
+    public void TestBroadcastInputMarker()
+    {
+        var input = Testing.Rand<float>(1, 3, 24, 24);
+        var a = Abs(Reshape(new Marker(WellknownMarkerNames.RangeOf, input, new[] { -1f, 1f }), input.Shape));
+        var result = TestMatched<BroadcastInputMarker>(a);
+        TestNotMatch<BroadcastInputMarker>(result);
+    }
+
+    [Fact]
+    public void TestBroadcastOutputMarker()
+    {
+        var input = Testing.Rand<float>(1, 3, 24, 24);
+        var a = new Marker(WellknownMarkerNames.RangeOf, Reshape(Abs(input), input.Shape), new[] { -1f, 1f });
+        var result = TestMatched<BroadcastOutputMarker>(a);
+        TestNotMatch<BroadcastOutputMarker>(result);
+    }
+}
