3e06b47ae6634000abe123e1c9ed0644052e8fd3
hejunchao <hejunchao@canaan-creative.com>
2023-08-04 12:19:42 +0800

    fix

--- src/Native/src/kernels/stackvm/reference/unary.cpp
+++ src/Native/src/kernels/stackvm/reference/unary.cpp
@@ -95,7 +95,7 @@ result<void> nncase::kernels::stackvm::reference::unary(
         UNARY_IMPL_DTYPE(dt_float32, float)
         UNARY_IMPL_DTYPE(dt_float16, half)
         //        UNARY_IMPL_DTYPE(dt_bfloat16, bfloat16)
-        UNARY_IMPL_DTYPE(dt_float64, double_t)
+        UNARY_IMPL_DTYPE(dt_float64, double)
         UNARY_IMPL_DTYPE(dt_int32, int32_t)
         UNARY_IMPL_DTYPE(dt_int64, int64_t)
         // Not in onnx, input is bool
