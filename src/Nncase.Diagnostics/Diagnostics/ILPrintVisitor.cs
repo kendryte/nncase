75b406dbd31513cb28b47a992edab8afea1c0054
curioyang <curioyang@users.noreply.github.com>
2023-08-09 07:18:27 +0000

    Apply code-format changes

--- src/Nncase.Diagnostics/Diagnostics/ILPrintVisitor.cs
+++ src/Nncase.Diagnostics/Diagnostics/ILPrintVisitor.cs
@@ -265,7 +265,7 @@ internal sealed class ILPrintVisitor : ExprFunctor<string, string>
     public override string VisitType(TensorType type) => type.DType switch
     {
         PrimType ptype => ptype.GetDisplayName() + (type.Shape.IsScalar ? string.Empty : type.Shape.ToString()),
-        PointerType { ElemType: PrimType etype } ptype => $"*{etype.GetDisplayName()}",
+        PointerType { ElemType: PrimType etype } => $"*{etype.GetDisplayName()}",
         ValueType => $"{type.DType.ToString()}",
         _ => throw new NotSupportedException(type.DType.GetType().Name),
     };
