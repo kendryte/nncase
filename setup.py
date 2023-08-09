d0d167b1a3c5ced2e8ea8d914da5e63aa5abfbda
yanghaoqi <yanghaoqi_intern@canaan-creative.com>
2023-08-09 16:01:34 +0800

    test git

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ import sys
 import io
 import re
 import time
+import subprocess
+from git.repo import Repo
 # See ref: https://stackoverflow.com/a/51575996
 
 
@@ -276,21 +278,21 @@ def find_version():
     version_prefix = re.findall(r"NNCASE_VERSION \"(.+)\"", version_file)
 
     if version_prefix:
-        # repo_path = os.getcwd()
-        # repo = Repo(repo_path)
-        # latest_commit = subprocess.check_output(
-        #     ['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
-        # tagged_commit = subprocess.check_output(
-        #     ['git', 'rev-list', '-n', '1', repo.tags[-1].name]).decode('utf-8').strip()
+        repo_path = os.getcwd()
+        repo = Repo(repo_path)
+        latest_commit = subprocess.check_output(
+            ['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+        tagged_commit = subprocess.check_output(
+            ['git', 'rev-list', '-n', '1', repo.tags[-1].name]).decode('utf-8').strip()
 
-        # if latest_commit != tagged_commit:
-        #     version_suffix = time.strftime("%Y%m%d", time.localtime())
-        #     return version_prefix[0] + "." + version_suffix
-        # return version_prefix[0]
-        if os.getenv("HAS_TAG"):
-            return version_prefix[0]
-        version_suffix = time.strftime("%Y%m%d", time.localtime())
-        return version_prefix[0] + "." + version_suffix
+        if latest_commit != tagged_commit:
+            version_suffix = time.strftime("%Y%m%d", time.localtime())
+            return version_prefix[0] + "." + version_suffix
+        return version_prefix[0]
+        # if os.getenv("HAS_TAG"):
+        #     return version_prefix[0]
+        # version_suffix = time.strftime("%Y%m%d", time.localtime())
+        # return version_prefix[0] + "." + version_suffix
     raise RuntimeError("Unable to find version string.")
 
 
