6284f4eee9fe1bbe2f0ca5aba5f5e10adf980a7c
hejunchao <hejunchao@canaan-creative.com>
2023-08-16 19:49:18 +0800

    fix

--- tests/kernels/test_instance_normalization.cpp
+++ tests/kernels/test_instance_normalization.cpp
@@ -109,18 +109,6 @@ TEST_P(InstanceNormalizationTest, instance_normalization) {
 }
 
 int main(int argc, char *argv[]) {
-    READY_TEST_CASE_GENERATE()
-    FOR_LOOP(lhs_shape, i)
-    FOR_LOOP(lhs_type, j)
-    FOR_LOOP(rhs_type, k)
-    SPLIT_ELEMENT(lhs_shape, i)
-    SPLIT_ELEMENT(lhs_type, j)
-    SPLIT_ELEMENT(rhs_type, k)
-    WRITE_SUB_CASE()
-    FOR_LOOP_END()
-    FOR_LOOP_END()
-    FOR_LOOP_END()
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
