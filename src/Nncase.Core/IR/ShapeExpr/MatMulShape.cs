90263611deab67a00f4654f12dac490cc95a10f5
FusionBolt <59008347+FusionBolt@users.noreply.github.com>
2023-08-18 13:35:24 +0800

    GNNE-1891 Shape bucket opt (#1040)
    * kernel opt
    
    * ShapeBucket opt
    
    * remove unused
    
    * Fix GetItem
    
    * update Compiler.cs
    
    * add BucketFusionGroupMutator
    
    * fix build
    
    * close topk
    
    * Apply code-format changes
    
    * fix FoldGetItem
    
    * resolve review
    
    * Apply code-format changes
    
    * add op for compute shape
    
    * Refactor MergePrevCall and support multi input prev
    
    * add MergePrevCall Test
    
    * fix bug for MergePrevCall
    
    * fix TestMergeStackWithConstant
    
    * add DupVar eliminate
    
    * fix MergePrev
    
    * update compiler.cs
    
    * dotnet format
    
    * fix build
    
    * fix shape evaluator
    
    * move rule file and add some new rule
    
    * add lost helper method
    
    * fix marker
    
    * add MergeMultiUserFusion
    
    * pass TestHasSameInput
    
    * passs TestComplexExpr
    
    * fix MergePrevCall marker
    
    * fix shape expr
    
    * add trilu support
    
    * add ring limit for MergeFusion
    
    * fix build
    
    * fix reduce shape expr and slice infer
    
    * isa gen
    
    * add multi user call to fusion
    
    * fix for ring
    
    * add merge tuple fusion
    
    * update
    
    * support multiuser getItem + fusion
    
    * pass TestGetItemWithRing
    
    * fix CallToFusion when input is tuple
    
    * fix MergeUserWithSameInput
    
    * add ReshapeMatMul
    
    * Apply code-format changes
    
    * fix ConcatToFusion when tuple has constant
    
    * add trilu test
    
    * fix evaluator
    
    * Apply code-format changes
    
    * fix type infer
    
    * update compiler.cs
    
    * Apply code-format changes
    
    * add some check
    
    * fix nest if in then
    
    * remove print for fix nest if in then
    
    * fix MergeNext multi user
    
    * add Range ShapeExpr
    
    * fix TestTupleGetItemUsersLargeThanOutputs
    
    * fix dup call and target
    
    * fix TestTupleGetItemOutputIsSingle
    
    * fix ShapeEvaluatorContent
    
    * add checker and fix for const
    
    * add more check
    
    * rebuild dynamic bucket body
    
    * refactor ShapeBucket
    
    * add ShapeExprCache
    
    * fix merge
    
    * add IMetricEvaluator
    
    * add test for ShapeExprCache and FusionBucket Rebuild
    
    * fix cache
    
    * add broadcast marker
    
    * add TfConv2DTransposeToFusion
    
    * fix TfCon2DTransposeToFusion and add simplify for bucket
    
    * fix rebuild
    
    * fix shape expr
    
    * fix
    
    * fix infer and shape expr
    
    * update
    
    * fix tflite
    
    * fix
    
    * Apply code-format changes
    
    * fix space to batch
    
    * Apply code-format changes
    
    * fix build
    
    * move test
    
    * fix test
    
    * fix Compiler.cs
    
    * Apply code-format changes
    
    * add lost file
    
    * simple review
    
    * Apply code-format changes
    
    * fix test
    
    * remove rebuild
    
    ---------
    
    Co-authored-by: FusionBolt <FusionBolt@users.noreply.github.com>

---
+++ src/Nncase.Core/IR/ShapeExpr/MatMulShape.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+using static Nncase.IR.TypePatternUtility;
+
+namespace Nncase.IR.ShapeExpr;
+
+public class MatMulShape : ShapeExprOp
+{
+    public static readonly ParameterInfo Lhs = new(typeof(MatMulShape), 0, "lhs");
+
+    public static readonly ParameterInfo Rhs = new(typeof(MatMulShape), 1, "rhs");
+}
---
+++ src/Nncase.Core/IR/ShapeExpr/ShapeExprOp.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Canaan Inc. All rights reserved.
+// Licensed under the Apache license. See LICENSE file in the project root for full license information.
+
+namespace Nncase.IR.ShapeExpr;
+
+public abstract class ShapeExprOp : Op
+{
+}
