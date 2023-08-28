52af8566553ca374298cfda6e8f72e4d914d0063
hejunchao <hejunchao@canaan-creative.com>
2023-08-28 15:28:18 +0800

    add trilu test

--- tests/kernels/test_trilu.cpp
+++ tests/kernels/test_trilu.cpp
@@ -26,69 +26,67 @@ using namespace nncase;
 using namespace nncase::runtime;
 using namespace ortki;
 
-class TriluTest : public KernelTest,
-                  public ::testing::TestWithParam<
-                      std::tuple<nncase::typecode_t, dims_t, float>> {
+class TriluTest
+    : public KernelTest,
+      public ::testing::TestWithParam<
+          std::tuple<nncase::typecode_t, dims_t, int64_t, int32_t>> {
   public:
     void SetUp() override {
-        auto &&[typecode, l_shape, alpha_value] = GetParam();
+        auto &&[typecode, l_shape, k_value, upper_value] = GetParam();
 
         input =
             hrt::create(typecode, l_shape, host_runtime_tensor::pool_cpu_only)
                 .expect("create tensor failed");
         init_tensor(input);
 
-        alpha = alpha_value;
+        int64_t k_ptr[] = {k_value};
+        k = hrt::create(nncase::dt_int64, {1},
+                        {reinterpret_cast<gsl::byte *>(k_ptr), sizeof(k_ptr)},
+                        true, host_runtime_tensor::pool_cpu_only)
+                .expect("create tensor failed");
+        upper = upper_value;
     }
 
     void TearDown() override {}
 
   protected:
     runtime_tensor input;
-    int64_t alpha;
+    runtime_tensor k;
+    int32_t upper;
 };
 
 INSTANTIATE_TEST_SUITE_P(trilu, TriluTest,
                          testing::Combine(testing::Values(dt_uint8),
                                           testing::Values(dims_t{4, 5}),
-                                          testing::Values(-1)));
+                                          testing::Values(0),
+                                          testing::Values(1)));
 
 TEST_P(TriluTest, trilu) {
-    //    auto l_ort = runtime_tensor_2_ort_tensor(input);
+    auto l_ort = runtime_tensor_2_ort_tensor(input);
+    auto k_ort = runtime_tensor_2_ort_tensor(k);
 
-    int64_t a_ptr[] = {alpha};
-    auto a = hrt::create(nncase::dt_int64, {1},
-                         {reinterpret_cast<gsl::byte *>(a_ptr), sizeof(a_ptr)},
-                         true, host_runtime_tensor::pool_cpu_only)
-                 .expect("create tensor failed");
-
-    //    todo ort no implement Trilu
     // expected
-    //    auto output_ort = ortki_Trilu(l_ort, runtime_tensor_2_ort_tensor(a),
-    //    0); size_t size = 0; void *ptr_ort = tensor_buffer(output_ort, &size);
-    //    dims_t shape(tensor_rank(output_ort));
-    //    tensor_shape(output_ort, reinterpret_cast<int64_t *>(shape.data()));
-    //    auto expected = hrt::create(input.datatype(), shape,
-    //                                {reinterpret_cast<gsl::byte *>(ptr_ort),
-    //                                size}, true,
-    //                                host_runtime_tensor::pool_cpu_only)
-    //        .expect("create tensor failed");
+    auto output_ort = ortki_Trilu(l_ort, k_ort, upper);
+    size_t size = 0;
+    void *ptr_ort = tensor_buffer(output_ort, &size);
+    dims_t shape(tensor_rank(output_ort));
+    tensor_shape(output_ort, reinterpret_cast<int64_t *>(shape.data()));
+    auto expected = hrt::create(input.datatype(), shape,
+                                {reinterpret_cast<gsl::byte *>(ptr_ort), size},
+                                true, host_runtime_tensor::pool_cpu_only)
+                        .expect("create tensor failed");
 
     // actual
-    int32_t upper_ptr[] = {0};
+    int32_t upper_ptr[] = {upper};
     auto upper = hrt::create(nncase::dt_int32, {1},
                              {reinterpret_cast<gsl::byte *>(upper_ptr),
                               sizeof(upper_ptr)},
                              true, host_runtime_tensor::pool_cpu_only)
                      .expect("create tensor failed");
 
-    auto output = kernels::stackvm::trilu(input.impl(), a.impl(), upper.impl())
+    auto output = kernels::stackvm::trilu(input.impl(), k.impl(), upper.impl())
                       .expect("trilu failed");
     runtime_tensor actual(output.as<tensor>().expect("as tensor failed"));
-
-    auto output1 = kernels::stackvm::trilu(input.impl(), a.impl(), upper.impl())
-                       .expect("trilu failed");
-    runtime_tensor expected(output.as<tensor>().expect("as tensor failed"));
 
     bool result = is_same_tensor(expected, actual) ||
                   cosine_similarity_tensor(expected, actual);
