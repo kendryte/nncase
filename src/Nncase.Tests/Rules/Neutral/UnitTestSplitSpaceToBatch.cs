1e1710b819b2880009276e89638c5cf1f31ecac0
FusionBolt <RealFusionBolt@gmail.com>
2023-09-05 11:24:07 +0800

    add split space to batch

---
+++ src/Nncase.Tests/Rules/Neutral/UnitTestSplitSpaceToBatch.cs
@@ -0,0 +1,27 @@
+using Nncase.Diagnostics;
+using Nncase.IR.NN;
+using Nncase.Passes.Rules.Neutral;
+using Nncase.Tests.TestFixture;
+using Xunit;
+using static Nncase.IR.F.NN;
+
+namespace Nncase.Tests.Rules.NeutralTest;
+
+[AutoSetupTestMethod(InitSession = true)]
+public class UnitTestSpaceToBatch : TransformTestBase
+{
+    [Fact]
+    public void TestSplitSpaceToBatch()
+    {
+        CompileOptions.DumpFlags = DumpFlags.Evaluator;
+        var i = SpaceToBatch(Testing.Rand<float>(1, 206, 192), new[]{3}, new[,]{{0, 1}});
+        TestMatched<SplitSpaceToBatch>(i);
+    }
+
+    [Fact]
+    public void TestSplitBatchToSpace()
+    {
+        var i = BatchToSpace(Testing.Rand<float>(3, 192, 67), new[]{3}, new[,]{{0, 1}});
+        TestMatched<SplitBatchToSpace>(i);
+    }
+}
