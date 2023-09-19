77d41520729bb54d7331cc4b1c402877c1e5b55a
huochenghai <huochenghai@canaan-creative.com>
2023-09-19 13:58:26 +0800

    fix cpu concat

--- modules/cpu/src/runtime/cmodel/include/tdma.h
+++ modules/cpu/src/runtime/cmodel/include/tdma.h
@@ -161,20 +161,20 @@ void reduce_sum_sqr(tensor<T, ALoc> &a, tensor<T, loc_t::local> &sum,
 }
 
 template <typename T, loc_t BLoc>
-void concat(std::initializer_list<tensor<T, loc_t::local>*> inits,
+void concat(itlib::small_vector<tensor<T, loc_t::local> *> inits,
             tensor<T, BLoc> &output, size_t axis) {
     itlib::small_vector<const gsl::byte *, 8> inputs(inits.size());
     itlib::small_vector<strides_t> in_strides(inits.size());
     auto concat_dims = dims_t(inits.size(), 1);
     for (size_t i = 0; i < inits.size(); ++i) {
-        if ((*(inits.begin()+i))->dimension().size() != 0) {
-            concat_dims[i] = (*(inits.begin() + i))->dimension()[axis];
+        if (inits[i]->dimension().size() != 0) {
+            concat_dims[i] = inits[i]->dimension()[axis];
         }
     }
 
-    for (auto &in : inits) {
-        inputs.push_back((const gsl::byte *)(in->data().data()));
-        in_strides.push_back(in->strides());
+    for (size_t i = 0; i < inits.size(); ++i) {
+        inputs[i] = ((const gsl::byte *)(inits[i]->data().data()));
+        in_strides[i] = (inits[i]->strides());
     }
 
     kernels::concat(inputs, output.data().data(), output.dimension(),
