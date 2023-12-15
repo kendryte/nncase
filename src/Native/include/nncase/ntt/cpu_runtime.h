966b40cfffe68f4315c958cb9f81ed9900ef5361
sunnycase <sunnycase@live.cn>
2023-12-15 15:57:40 +0800

    Fix macos

--- src/Native/include/nncase/ntt/cpu_runtime.h
+++ src/Native/include/nncase/ntt/cpu_runtime.h
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <cstddef>
+#include <cstdint>
+
 extern "C" {
 struct nncase_runtime_cpu_mt_t {
     float (*acosf)(float v);
