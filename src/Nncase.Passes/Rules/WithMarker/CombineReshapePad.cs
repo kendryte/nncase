5c40b222196b5b0e4b067d0ac1ef79180a6a1ea9
FusionBolt <FusionBolt@users.noreply.github.com>
2024-01-19 07:26:02 +0000

    Apply code-format changes

--- src/Nncase.Passes/Rules/WithMarker/CombineReshapePad.cs
+++ src/Nncase.Passes/Rules/WithMarker/CombineReshapePad.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Canaan Inc. All rights reserved.
+﻿// Copyright (c) Canaan Inc. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
 using System;
@@ -38,7 +38,7 @@ public sealed partial class CombineReshapePad : IRewriteRule
                 _ => true,
                 HasMarker(IsPad("pad", "padCall", _ => true, HasMarker(IsWildcard("input"), "marker"), IsTensorConst("pads"), IsTensorConst("value")) with { TypePattern = HasFixedShape() }, "padOutMarker"),
                 IsWildcard("shape")) with
-            { TypePattern = HasFixedShape() };
+        { TypePattern = HasFixedShape() };
 
     private Expr? GetReplace(Reshape reshape, Call reshapeCall, Pad pad, Call padCall, Expr input, Expr shape, int[] pads, Expr value, Marker marker)
     {
