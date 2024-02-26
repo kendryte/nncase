d9805320c0439067e46d972a889e54c91aed8b7d
Curio Yang <curioyhq@gmail.com>
2024-02-26 17:24:10 +0800

    fix reshape

--- modules/Nncase.Modules.Ncnn/Passes/Rules/Ncnn/LowerReshape.cs
+++ modules/Nncase.Modules.Ncnn/Passes/Rules/Ncnn/LowerReshape.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Canaan Inc. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
+using NetFabric.Hyperlinq;
 using Nncase.IR;
 using Nncase.PatternMatch;
 using static Nncase.IR.F.Ncnn;
@@ -20,18 +21,28 @@ public partial class LowerReshape : RewriteRule<Pattern>
 
     private Expr? GetReplace(Expr input, Expr shape)
     {
-        // TODO: split input
-        // if shape.Length == input.Length means that batchSize need reshape. Ncnn can't support.
-        if (input.CheckedShape.ToList()[0] != 1 || input.CheckedShape.Count == shape.CheckedShape.Count)
+        if (input.CheckedShape.Count < 5)
         {
-            return null;
+            var inResO = new Var(input.CheckedType);
+            var outputShape = shape.Evaluate().AsTensor().ToArray<int>();
+
+            return new Call(new Fusion("ncnn", NcnnReshape(inResO, outputShape), new[] { inResO }), input);
         }
 
-        var inRes = Squeeze(input, new[] { 0 });
-        var inResO = new Var(inRes.CheckedType);
-        var outputShape = shape.Evaluate().AsTensor().ToArray<int>();
-        var reshape = new Call(new Fusion("ncnn", NcnnReshape(inResO, outputShape), new[] { inResO }), input);
+        return null;
 
-        return reshape;
+        // // TODO: split input
+        // // if shape.Length == input.Length means that batchSize need reshape. Ncnn can't support.
+        // if (input.CheckedShape.ToList()[0] != 1 || input.CheckedShape.Count == shape.CheckedShape.Count)
+        // {
+        //     return null;
+        // }
+        //
+        // var inRes = Squeeze(input, new[] { 0 });
+        // var inResO = new Var(inRes.CheckedType);
+        // var outputShape = shape.Evaluate().AsTensor().ToArray<int>();
+        // var reshape = new Call(new Fusion("ncnn", NcnnReshape(inResO, outputShape), new[] { inResO }), input);
+        //
+        // return reshape;
     }
 }
