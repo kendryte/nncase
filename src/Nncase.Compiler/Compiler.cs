2e10f656e3835c885cbfec0af524e3e73c78ae52
huochenghai <huochenghai@canaan-creative.com>
2024-08-08 15:31:48 +0800

    decompose instancenorm

--- src/Nncase.Compiler/Compiler.cs
+++ src/Nncase.Compiler/Compiler.cs
@@ -145,6 +145,7 @@ internal class Compiler : ICompiler
             p.Add<Passes.Rules.Neutral.DecomposeSoftmax>();
             p.Add<Passes.Rules.Neutral.DecomposeLayerNorm>();
             p.Add<Passes.Rules.Neutral.DecomposeSwish>();
+            p.Add<Passes.Rules.Neutral.DecomposeInstanceNorm>();
             p.Add<Passes.Rules.Neutral.BianryScalarConstToTensor>();
         });
 
