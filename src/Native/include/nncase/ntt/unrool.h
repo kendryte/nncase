4fdf4e089f5238aa53460454ceb096054db71b79
guodongliang <guodongliang@canaan-creative.com>
2024-08-23 15:00:45 +0800

    add support for unary

--- src/Native/include/nncase/ntt/unrool.h
+++ src/Native/include/nncase/ntt/unrool.h
@@ -18,21 +18,24 @@
 #include <utility>
 
 namespace nncase::ntt {
-#define THREAD(N, i, rets, input, Stride, op)                                  \
+#define THREAD(N, i, rets, input, Stride, op, isBinary)                        \
     if constexpr (UnRoolNum > N) {                                             \
-        rets[N] = op(rets[N], input[(i + N) * Stride]);                        \
+        if constexpr (isBinary)                                                \
+            rets[N] = op(rets[N], input[(i + N) * Stride]);                    \
+        else                                                                   \
+            rets[(i + N) * Stride] = op(input[(i + N) * Stride]);              \
     }
 
 // 目前只支持到最大8维度展开，如果有需要可以扩展
-#define UNROLL_LOOP(i, rets, input, Stride, op)                                \
-    THREAD(0, i, rets, input, Stride, op)                                      \
-    THREAD(1, i, rets, input, Stride, op)                                      \
-    THREAD(2, i, rets, input, Stride, op)                                      \
-    THREAD(3, i, rets, input, Stride, op)                                      \
-    THREAD(4, i, rets, input, Stride, op)                                      \
-    THREAD(5, i, rets, input, Stride, op)                                      \
-    THREAD(6, i, rets, input, Stride, op)                                      \
-    THREAD(7, i, rets, input, Stride, op)
+#define UNROLL_LOOP(i, rets, input, Stride, op, isBinary)                      \
+    THREAD(0, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(1, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(2, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(3, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(4, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(5, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(6, i, rets, input, Stride, op, isBinary)                            \
+    THREAD(7, i, rets, input, Stride, op, isBinary)
 
 template <template <typename T1, typename T2> class Op, class TElem,
           size_t UnRoolNum, size_t LoopCnt, size_t Stride>
@@ -50,8 +53,10 @@ auto loop_unrool(const TElem *input) {
         rets[i] = input[i * Stride];
     }
 
-    for (size_t i = UnRoolNum; i < LoopCnt; i += UnRoolNum) {
-        UNROLL_LOOP(i, rets, input, Stride, op)
+    constexpr size_t remainder = LoopCnt % UnRoolNum;
+    constexpr size_t integer = LoopCnt - remainder;
+    for (size_t i = UnRoolNum; i < integer; i += UnRoolNum) {
+        UNROLL_LOOP(i, rets, input, Stride, op, true)
     }
 
     ret = rets[0];
@@ -59,8 +64,8 @@ auto loop_unrool(const TElem *input) {
         ret = op(ret, rets[i]);
     }
 
-    for (size_t i = 0; i < LoopCnt % UnRoolNum; i++) {
-        ret = op(ret, input[LoopCnt * Stride - i - 1]);
+    for (size_t i = 0; i < remainder; i++) {
+        ret = op(ret, input[(integer + i) * Stride]);
     }
 
     return ret;
@@ -70,8 +75,30 @@ template <template <typename T1, typename T2> class Op, class TElem,
           size_t UnRoolNum, size_t LoopCnt, size_t Stride>
 auto loop_unrool(const TElem *input_a, const TElem *input_b) {
 
-    return Op<TElem, TElem>(
-        loop_unrool<Op, TElem, UnRoolNum, LoopCnt, Stride>(input_a),
-        loop_unrool<Op, TElem, UnRoolNum, LoopCnt, Stride>(input_b));
+    Op<TElem, TElem> op;
+    auto result_a = loop_unrool<Op, TElem, UnRoolNum, LoopCnt, Stride>(input_a);
+    auto result_b = loop_unrool<Op, TElem, UnRoolNum, LoopCnt, Stride>(input_b);
+    return op(result_a, result_b);
+}
+
+template <template <typename T> class Op, class TElem, size_t UnRoolNum,
+          size_t LoopCnt, size_t Stride>
+void loop_unrool(const TElem *input, TElem *output) {
+
+    static_assert(UnRoolNum <= LoopCnt, "UnRoolNum must be less than LoopCnt");
+    static_assert(UnRoolNum > 0, "UnRoolNum must be greater than zero");
+    static_assert(UnRoolNum < 9, "UnRoolNum must be less than 9");
+    TElem *ret = output;
+    Op<TElem> op;
+
+    constexpr size_t remainder = LoopCnt % UnRoolNum;
+    constexpr size_t integer = LoopCnt - remainder;
+    for (size_t i = 0; i < integer; i += UnRoolNum) {
+        UNROLL_LOOP(i, ret, input, Stride, op, false)
+    }
+
+    for (size_t i = 0; i < remainder; i++) {
+        ret[(integer + i) * Stride] = op(input[(integer + i) * Stride]);
+    }
 }
 } // namespace nncase::ntt
