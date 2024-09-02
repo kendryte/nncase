9aa2cc7fa2baf7b76843eea6f54947249c40c596
郑启航 <597323109@qq.com>
2024-09-02 19:27:04 +0800

    remove duplicate

--- src/Nncase.Core/CommandLine/FromAmongAttribute copy.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Canaan Inc. All rights reserved.
-// Licensed under the Apache license. See LICENSE file in the project root for full license information.
-
-namespace Nncase.CommandLine;
-
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
-public sealed class AllowMultiplePerTokenAttribute : Attribute
-{
-}
