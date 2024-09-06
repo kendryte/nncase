3760c01fc9b413786ce446fbe464ff3b2c7b0015
huochenghai <huochenghai@canaan-creative.com>
2024-09-06 16:01:29 +0800

    align buffers

--- src/Native/include/nncase/io_utils.h
+++ src/Native/include/nncase/io_utils.h
@@ -18,16 +18,54 @@
 #include <vector>
 
 namespace nncase {
-inline std::vector<uint8_t> read_stream(std::istream &stream) {
+template <typename T, std::size_t Alignment>
+class aligned_allocator {
+public:
+    using value_type = T;
+    static constexpr std::size_t alignment = Alignment;
+
+    aligned_allocator() noexcept = default;
+    template <class U> aligned_allocator(const aligned_allocator<U, Alignment>&) noexcept {}
+
+    T* allocate(std::size_t n) {
+        if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+            throw std::bad_alloc();
+        
+        if (auto ptr = static_cast<T*>(std::aligned_alloc(alignment, n * sizeof(T))))
+            return ptr;
+
+        throw std::bad_alloc();
+    }
+
+    void deallocate(T *p, std::size_t) noexcept { std::free(p); }
+
+    template <typename U>
+    struct rebind {
+        using other = aligned_allocator<U, Alignment>;
+    };
+};
+
+template <class T, class U, std::size_t Alignment>
+bool operator==(const aligned_allocator<T, Alignment>&, const aligned_allocator<U, Alignment>&) noexcept {
+    return true;
+}
+
+template <class T, class U, std::size_t Alignment>
+bool operator!=(const aligned_allocator<T, Alignment>&, const aligned_allocator<U, Alignment>&) noexcept {
+    return false;
+}
+
+
+inline std::vector<uint8_t, aligned_allocator<uint8_t, 32>> read_stream(std::istream &stream) {
     stream.seekg(0, std::ios::end);
     size_t length = stream.tellg();
     stream.seekg(0, std::ios::beg);
-    std::vector<uint8_t> data(length);
+    std::vector<uint8_t, aligned_allocator<uint8_t, 32>> data(length);
     stream.read(reinterpret_cast<char *>(data.data()), length);
     return data;
 }
 
-inline std::vector<uint8_t> read_file(const std::filesystem::path &filename) {
+inline std::vector<uint8_t, aligned_allocator<uint8_t, 32>> read_file(const std::filesystem::path &filename) {
     std::ifstream infile(filename.string(), std::ios::binary | std::ios::in);
     if (!infile.good())
         throw std::runtime_error("Cannot open file: " + filename.string());
