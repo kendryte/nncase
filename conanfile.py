db38880603414334639e8758b278a5086108bb99
Curio <curioyhq@gmail.com>
2024-09-11 10:16:46 +0800

    upgrade conan2

--- conanfile.py
+++ conanfile.py
@@ -13,59 +13,61 @@
 # limitations under the License.
 # pylint: disable=invalid-name, unused-argument, import-outside-toplevel
 
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.build import check_min_cppstd, cross_building
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, copy, export_conandata_patches, get, rmdir
 
 
 class nncaseConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "cmake_find_package", "cmake_paths"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
         "runtime": [True, False],
         "tests": [True, False],
-        "halide": [True, False],
         "python": [True, False],
         "vulkan_runtime": [True, False],
-        "openmp": [True, False]
+        "python_root": ["ANY"]
     }
     default_options = {
         "shared": False,
         "fPIC": True,
         "runtime": False,
         "tests": False,
-        "halide": True,
         "python": True,
         "vulkan_runtime": False,
-        "openmp": True
+        "python_root": ""
     }
-    
-    def imports(self):
-        if self.settings.os == 'Windows':
-            self.copy("nethost.dll", "bin", "bin")
-            self.copy("ortki.dll", "bin", "bin")
+
+    @property
+    def _min_cppstd(self):
+        return 17
+
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
         self.requires('gsl-lite/0.37.0')
-        self.requires('hkg/0.0.1')
         if self.options.tests:
             self.requires('gtest/1.10.0')
-            self.requires('ortki/0.0.2')
-            self.requires('rapidjson/1.1.x')
+            self.requires('ortki/0.0.4')
 
         if self.options.python:
-            self.requires('pybind11/2.6.1')
+            self.requires('pybind11/2.11.1')
 
         if not self.options.runtime:
-            self.requires('abseil/20220623.1')
-            self.requires('nethost/6.0.11')
-            self.requires('fmt/7.1.3')
+            self.requires('abseil/20240722.0')
             self.requires('magic_enum/0.7.0')
-            self.requires('spdlog/1.8.2')
-            self.requires('inja/3.2.0')
-            if self.options.tests:
-                self.requires('gtest/1.10.0')
+            self.requires('spdlog/1.8.5')
+            self.requires('inja/3.4.0')
+            self.requires('nethost/8.0.8')
+            self.requires('fmt/7.1.3')
 
+        if not self.options.runtime or self.options.tests:
+            self.requires('nlohmann_json/3.11.3')
+            
         if (not self.options.runtime) or self.options.vulkan_runtime:
             self.requires('vulkan-headers/1.2.182')
             self.requires('vulkan-loader/1.2.182')
@@ -74,42 +76,31 @@ class nncaseConan(ConanFile):
         pass
 
     def configure(self):
-        min_cppstd = "17" if self.options.runtime else "20"
-        tools.check_min_cppstd(self, min_cppstd)
-
-        if self.settings.os == 'Windows':
-            self.settings.compiler.toolset = 'ClangCL'
-
-        if self.settings.arch not in ("x86_64",):
-            self.options.halide = False
-            
         if not self.options.runtime:
-            if self.settings.os == 'Windows':
+            if self.settings.os == 'Windows' and self.settings.build_type == 'Debug':
                 self.options["nethost"].shared = True
-
-        if (not self.options.runtime) or self.options.vulkan_runtime:
-            if self.settings.os == 'Linux':
-                self.options["vulkan-loader"].with_wsi_xcb = False
-                self.options["vulkan-loader"].with_wsi_xlib = False
-                self.options["vulkan-loader"].with_wsi_wayland = False
-                self.options["vulkan-loader"].with_wsi_directfb = False
 
         if self.options.tests:
             self.options["ortki"].shared = True
+            self.options["date"].header_only = True
 
-    def cmake_configure(self):
-        cmake = CMake(self)
-        cmake.definitions['BUILDING_RUNTIME'] = self.options.runtime
-        cmake.definitions['ENABLE_OPENMP'] = self.options.openmp
-        cmake.definitions['ENABLE_VULKAN_RUNTIME'] = self.options.vulkan_runtime
-        cmake.definitions['ENABLE_HALIDE'] = self.options.halide
-        cmake.definitions['BUILD_PYTHON_BINDING'] = self.options.python
-        cmake.definitions['BUILD_TESTING'] = self.options.tests
-        if self.options.runtime:
-            cmake.definitions["CMAKE_CXX_STANDARD"] = 17
-        cmake.configure()
-        return cmake
+    def validate(self):
+        if self.settings.compiler.get_safe("cppstd"):
+            check_min_cppstd(self, self._min_cppstd)
+
+    def generate(self):
+        tc = CMakeToolchain(self, generator="Ninja")
+        tc.variables['BUILDING_RUNTIME'] = self.options.runtime
+        tc.variables['ENABLE_VULKAN_RUNTIME'] = self.options.vulkan_runtime
+        tc.variables['BUILD_PYTHON_BINDING'] = self.options.python
+        tc.variables['BUILD_TESTING'] = self.options.tests
+        if self.options.get_safe("python_root", default="") != "":
+            tc.variables['Python3_ROOT_DIR'] = self.options.python_root
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
-        cmake = self.cmake_configure()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
