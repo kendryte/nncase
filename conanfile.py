74fc432da92ef800fe1e71371aa0cdd9d9ea0860
yanghaoqi <yanghaoqi_intern@canaan-creative.com>
2024-09-23 20:00:40 +0800

    debug

--- conanfile.py
+++ conanfile.py
@@ -94,9 +94,9 @@ class nncaseConan(ConanFile):
         tc.variables['BUILD_PYTHON_BINDING'] = self.options.python
         tc.variables['BUILD_TESTING'] = self.options.tests
         if self.options.get_safe("python_root", default="") != "":
-            # if self.settings.os == 'Windows':
-            #     tc.variables['Python3_ROOT_DIR'] = self.options.python_root.replace('\\', '\\\\')
-            # else:
+            if self.settings.os == 'Windows':
+                tc.variables['Python3_ROOT_DIR'] = self.options.python_root.replace('\\', '/')
+            else:
             tc.variables['Python3_ROOT_DIR'] = self.options.python_root
         tc.generate()
         deps = CMakeDeps(self)
