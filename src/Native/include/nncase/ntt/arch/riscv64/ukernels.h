2ca8e934fb22cfd2bc7cdcda07ef2e770050537f
sunnycase <sunnycase@live.cn>
2024-10-11 07:39:53 +0000

    Interleave store

--- src/Native/include/nncase/ntt/arch/riscv64/ukernels.h
+++ src/Native/include/nncase/ntt/arch/riscv64/ukernels.h
@@ -166,40 +166,44 @@ struct u_matmul<ukernels::mamtul_pack_kind::pack_m, AccumulateC, 4, 4,
 
         const auto a0_strides = a.strides();
         const auto b0_strides = b.strides();
+        const auto c0_strides = c0.strides();
 
         const auto ak_strides = a0_strides[1];
         const auto bk_strides = b0_strides[0];
         const auto bn_strides = b0_strides[1];
+        const auto cm_strides = c0_strides[0];
+        const auto cn_strides = c0_strides[1];
 
-        register auto a0_0_0_p asm("t0") = a.elements().data();
-        register auto a0_1_0_p asm("t1") = a0_0_0_p + a0_strides[0];
-        register auto a0_2_0_p asm("t2") = a0_1_0_p + a0_strides[0];
-        register auto a0_3_0_p asm("t3") = a0_2_0_p + a0_strides[0];
-        register auto a0_0_1_p asm("t4") = a0_0_0_p + ak_strides;
-        register auto a0_1_1_p asm("t5") = a0_0_1_p + a0_strides[0];
-        register auto a0_2_1_p asm("t6") = a0_1_1_p + a0_strides[0];
-        register auto a0_3_1_p asm("s0") = a0_2_1_p + a0_strides[0];
-
-        register auto b0_0_x_p asm("s1") = b.elements().data();
-        register auto b0_1_x_p asm("s2") = b0_0_x_p + bk_strides;
-
-        // 1. Pre load A/B
         {
-            a0_0_0 = *a0_0_0_p;
-            a0_0_0_p += ak_strides * 2;
-            a0_1_0 = *a0_1_0_p;
-            a0_1_0_p += ak_strides * 2;
-            a0_2_0 = *a0_2_0_p;
-            a0_2_0_p += ak_strides * 2;
-            a0_3_0 = *a0_3_0_p;
-            a0_3_0_p += ak_strides * 2;
+            register auto a0_0_0_p asm("t0") = a.elements().data();
+            register auto a0_1_0_p asm("t1") = a0_0_0_p + a0_strides[0];
+            register auto a0_2_0_p asm("t2") = a0_1_0_p + a0_strides[0];
+            register auto a0_3_0_p asm("t3") = a0_2_0_p + a0_strides[0];
+            register auto a0_0_1_p asm("t4") = a0_0_0_p + ak_strides;
+            register auto a0_1_1_p asm("t5") = a0_0_1_p + a0_strides[0];
+            register auto a0_2_1_p asm("t6") = a0_1_1_p + a0_strides[0];
+            register auto a0_3_1_p asm("s0") = a0_2_1_p + a0_strides[0];
 
-            b0_0_0 = b0_0_x_p[bn_strides * 0];
-            b0_0_1 = b0_0_x_p[bn_strides * 1];
-            b0_0_2 = b0_0_x_p[bn_strides * 2];
-            b0_0_3 = b0_0_x_p[bn_strides * 3];
-            b0_0_x_p += bk_strides * 2;
-        }
+            register auto b0_0_x_p asm("a4") = b.elements().data();
+            register auto b0_1_x_p asm("a5") = b0_0_x_p + bk_strides;
+
+            // 1. Pre load A/B
+            {
+                a0_0_0 = *a0_0_0_p;
+                a0_0_0_p += ak_strides * 2;
+                a0_1_0 = *a0_1_0_p;
+                a0_1_0_p += ak_strides * 2;
+                a0_2_0 = *a0_2_0_p;
+                a0_2_0_p += ak_strides * 2;
+                a0_3_0 = *a0_3_0_p;
+                a0_3_0_p += ak_strides * 2;
+
+                b0_0_0 = b0_0_x_p[bn_strides * 0];
+                b0_0_1 = b0_0_x_p[bn_strides * 1];
+                b0_0_2 = b0_0_x_p[bn_strides * 2];
+                b0_0_3 = b0_0_x_p[bn_strides * 3];
+                b0_0_x_p += bk_strides * 2;
+            }
 
 #define NTT_MATMUL_PP(ld, calc)                                                \
     asm volatile(                                                              \
@@ -285,78 +289,127 @@ struct u_matmul<ukernels::mamtul_pack_kind::pack_m, AccumulateC, 4, 4,
           [bk_strides] "i"(bk_strides * sizeof(b0_0_0)),                       \
           [bn_strides] "i"(bn_strides * sizeof(b0_0_0)));
 
+            // 2. Pipelined
+            const size_t pipeline_count = (K - 1) / 2;
+            for (size_t k1 = 0; k1 < pipeline_count; k1++) {
+                // Ping
+                NTT_MATMUL_PP(1, 0)
+                // Pong
+                NTT_MATMUL_PP(0, 1)
+            }
+
+            if (K % 2 == 0) {
+                NTT_MATMUL_PP(1, 0)
+            }
+        }
+
+        // 3. Tail
+        {
+            register fixed_vfloat32m1_t *c0_x_0_p asm("t0");
+            register fixed_vfloat32m1_t *c0_x_1_p asm("t1");
+            register fixed_vfloat32m1_t *c0_x_2_p asm("t2");
+            register fixed_vfloat32m1_t *c0_x_3_p asm("t3");
+
 #define NTT_MATMUL_TAIL(calc)                                                  \
     asm volatile(                                                              \
         "vfmacc.vf	%[c0_0_0],%[b0_" #calc "_0],%[a0_0_" #calc "]\n"       \
+        "mv %[c0_x_0_p], %[c0_x_0_p_init]\n"                                   \
         "vfmacc.vf	%[c0_0_1],%[b0_" #calc "_1],%[a0_0_" #calc "]\n"       \
+        "addi %[c0_x_1_p], %[c0_x_0_p_init], %[cn_strides]\n"                  \
         "vfmacc.vf	%[c0_0_2],%[b0_" #calc "_2],%[a0_0_" #calc "]\n"       \
+        "addi %[c0_x_2_p], %[c0_x_0_p_init], %[cn_strides] * 2\n"              \
         "vfmacc.vf	%[c0_0_3],%[b0_" #calc "_3],%[a0_0_" #calc "]\n"       \
+        "addi %[c0_x_3_p], %[c0_x_0_p_init], %[cn_strides] * 3\n"              \
         "vfmacc.vf	%[c0_1_0],%[b0_" #calc "_0],%[a0_1_" #calc "]\n"       \
+        "vs1r.v     %[c0_0_0], (%[c0_x_0_p])\n"                                \
+        "add %[c0_x_0_p],%[c0_x_0_p],%[cm_strides] \n"                         \
         "vfmacc.vf	%[c0_1_1],%[b0_" #calc "_1],%[a0_1_" #calc "]\n"       \
+        "vs1r.v     %[c0_0_1], (%[c0_x_1_p])\n"                                \
+        "add %[c0_x_1_p],%[c0_x_1_p],%[cm_strides] \n"                         \
         "vfmacc.vf	%[c0_1_2],%[b0_" #calc "_2],%[a0_1_" #calc "]\n"       \
+        "vs1r.v     %[c0_0_2], (%[c0_x_2_p])\n"                                \
+        "add %[c0_x_2_p],%[c0_x_2_p],%[cm_strides] \n"                         \
         "vfmacc.vf	%[c0_1_3],%[b0_" #calc "_3],%[a0_1_" #calc "]\n"       \
+        "vs1r.v     %[c0_0_3], (%[c0_x_3_p])\n"                                \
+        "add %[c0_x_3_p],%[c0_x_3_p],%[cm_strides] \n"                         \
         : [c0_0_0] "+vr"(c0_0_0), [c0_0_1] "+vr"(c0_0_1),                      \
           [c0_0_2] "+vr"(c0_0_2), [c0_0_3] "+vr"(c0_0_3),                      \
           [c0_1_0] "+vr"(c0_1_0), [c0_1_1] "+vr"(c0_1_1),                      \
-          [c0_1_2] "+vr"(c0_1_2), [c0_1_3] "+vr"(c0_1_3)                       \
+          [c0_1_2] "+vr"(c0_1_2), [c0_1_3] "+vr"(c0_1_3),                      \
+          [c0_x_0_p] "=r"(c0_x_0_p), [c0_x_1_p] "=r"(c0_x_1_p),                \
+          [c0_x_2_p] "=r"(c0_x_2_p), [c0_x_3_p] "=r"(c0_x_3_p)                 \
         : [a0_0_##calc] "vr"(a0_0_##calc), [a0_1_##calc] "vr"(a0_1_##calc),    \
           [b0_##calc##_0] "f"(b0_##calc##_0),                                  \
           [b0_##calc##_1] "f"(b0_##calc##_1),                                  \
           [b0_##calc##_2] "f"(b0_##calc##_2),                                  \
-          [b0_##calc##_3] "f"(b0_##calc##_3));                                 \
+          [b0_##calc##_3] "f"(b0_##calc##_3),                                  \
+          [c0_x_0_p_init] "r"(c0.elements().data()),                           \
+          [cm_strides] "r"(cm_strides * sizeof(c0_0_0)),                       \
+          [cn_strides] "i"(cn_strides * sizeof(c0_0_0)));                      \
                                                                                \
     asm volatile(                                                              \
         "vfmacc.vf	%[c0_2_0],%[b0_" #calc "_0],%[a0_2_" #calc "]\n"       \
+        "vs1r.v     %[c0_1_0], (%[c0_x_0_p])\n"                                \
+        "add %[c0_x_0_p],%[c0_x_0_p],%[cm_strides] \n"                         \
         "vfmacc.vf	%[c0_2_1],%[b0_" #calc "_1],%[a0_2_" #calc "]\n"       \
+        "vs1r.v     %[c0_1_1], (%[c0_x_1_p])\n"                                \
+        "add %[c0_x_1_p],%[c0_x_1_p],%[cm_strides] \n"                         \
         "vfmacc.vf	%[c0_2_2],%[b0_" #calc "_2],%[a0_2_" #calc "]\n"       \
+        "vs1r.v     %[c0_1_2], (%[c0_x_2_p])\n"                                \
+        "add %[c0_x_2_p],%[c0_x_2_p],%[cm_strides] \n"                         \
         "vfmacc.vf	%[c0_2_3],%[b0_" #calc "_3],%[a0_2_" #calc "]\n"       \
-        "vfmacc.vf	%[c0_3_0],%[b0_" #calc "_0],%[a0_3_" #calc "]\n"       \
-        "vfmacc.vf	%[c0_3_1],%[b0_" #calc "_1],%[a0_3_" #calc "]\n"       \
-        "vfmacc.vf	%[c0_3_2],%[b0_" #calc "_2],%[a0_3_" #calc "]\n"       \
-        "vfmacc.vf	%[c0_3_3],%[b0_" #calc "_3],%[a0_3_" #calc "]\n"       \
+        "vs1r.v     %[c0_1_3], (%[c0_x_3_p])\n"                                \
+        "add %[c0_x_3_p],%[c0_x_3_p],%[cm_strides] \n"                         \
         : [c0_2_0] "+vr"(c0_2_0), [c0_2_1] "+vr"(c0_2_1),                      \
           [c0_2_2] "+vr"(c0_2_2), [c0_2_3] "+vr"(c0_2_3),                      \
-          [c0_3_0] "+vr"(c0_3_0), [c0_3_1] "+vr"(c0_3_1),                      \
-          [c0_3_2] "+vr"(c0_3_2), [c0_3_3] "+vr"(c0_3_3)                       \
-        : [a0_2_##calc] "vr"(a0_2_##calc), [a0_3_##calc] "vr"(a0_3_##calc),    \
-          [b0_##calc##_0] "f"(b0_##calc##_0),                                  \
+          [c0_x_0_p] "+r"(c0_x_0_p), [c0_x_1_p] "+r"(c0_x_1_p),                \
+          [c0_x_2_p] "+r"(c0_x_2_p), [c0_x_3_p] "+r"(c0_x_3_p)                 \
+        : [a0_2_##calc] "vr"(a0_2_##calc), [b0_##calc##_0] "f"(b0_##calc##_0), \
           [b0_##calc##_1] "f"(b0_##calc##_1),                                  \
           [b0_##calc##_2] "f"(b0_##calc##_2),                                  \
-          [b0_##calc##_3] "f"(b0_##calc##_3));
+          [b0_##calc##_3] "f"(b0_##calc##_3), [c0_1_0] "vr"(c0_1_0),           \
+          [c0_1_1] "vr"(c0_1_1), [c0_1_2] "vr"(c0_1_2), [c0_1_3] "vr"(c0_1_3), \
+          [cm_strides] "r"(cm_strides * sizeof(c0_0_0)));                      \
+                                                                               \
+    asm volatile(                                                              \
+        "vfmacc.vf	%[c0_3_0],%[b0_" #calc "_0],%[a0_3_" #calc "]\n"       \
+        "vs1r.v     %[c0_2_0], (%[c0_x_0_p])\n"                                \
+        "add %[c0_x_0_p],%[c0_x_0_p],%[cm_strides] \n"                         \
+        "vfmacc.vf	%[c0_3_1],%[b0_" #calc "_1],%[a0_3_" #calc "]\n"       \
+        "vs1r.v     %[c0_2_1], (%[c0_x_1_p])\n"                                \
+        "add %[c0_x_1_p],%[c0_x_1_p],%[cm_strides] \n"                         \
+        "vfmacc.vf	%[c0_3_2],%[b0_" #calc "_2],%[a0_3_" #calc "]\n"       \
+        "vs1r.v     %[c0_2_2], (%[c0_x_2_p])\n"                                \
+        "add %[c0_x_2_p],%[c0_x_2_p],%[cm_strides] \n"                         \
+        "vfmacc.vf	%[c0_3_3],%[b0_" #calc "_3],%[a0_3_" #calc "]\n"       \
+        "vs1r.v     %[c0_2_3], (%[c0_x_3_p])\n"                                \
+        "add %[c0_x_3_p],%[c0_x_3_p],%[cm_strides] \n"                         \
+        : [c0_3_0] "+vr"(c0_3_0), [c0_3_1] "+vr"(c0_3_1),                      \
+          [c0_3_2] "+vr"(c0_3_2), [c0_3_3] "+vr"(c0_3_3),                      \
+          [c0_x_0_p] "+r"(c0_x_0_p), [c0_x_1_p] "+r"(c0_x_1_p),                \
+          [c0_x_2_p] "+r"(c0_x_2_p), [c0_x_3_p] "+r"(c0_x_3_p)                 \
+        : [a0_3_##calc] "vr"(a0_3_##calc), [b0_##calc##_0] "f"(b0_##calc##_0), \
+          [b0_##calc##_1] "f"(b0_##calc##_1),                                  \
+          [b0_##calc##_2] "f"(b0_##calc##_2),                                  \
+          [b0_##calc##_3] "f"(b0_##calc##_3), [c0_2_0] "vr"(c0_2_0),           \
+          [c0_2_1] "vr"(c0_2_1), [c0_2_2] "vr"(c0_2_2), [c0_2_3] "vr"(c0_2_3), \
+          [cm_strides] "r"(cm_strides * sizeof(c0_0_0)));                      \
+                                                                               \
+    asm volatile(                                                              \
+        "vs1r.v     %[c0_3_0], (%[c0_x_0_p])\n"                                \
+        "vs1r.v     %[c0_3_1], (%[c0_x_1_p])\n"                                \
+        "vs1r.v     %[c0_3_2], (%[c0_x_2_p])\n"                                \
+        "vs1r.v     %[c0_3_3], (%[c0_x_3_p])\n" ::[c0_3_0] "vr"(c0_3_0),       \
+        [c0_3_1] "vr"(c0_3_1), [c0_3_2] "vr"(c0_3_2), [c0_3_3] "vr"(c0_3_3),   \
+        [c0_x_0_p] "r"(c0_x_0_p), [c0_x_1_p] "r"(c0_x_1_p),                    \
+        [c0_x_2_p] "r"(c0_x_2_p), [c0_x_3_p] "r"(c0_x_3_p));
 
-        // 2. Pipelined
-        const size_t pipeline_count = (K - 1) / 2;
-        for (size_t k1 = 0; k1 < pipeline_count; k1++) {
-            // Ping
-            NTT_MATMUL_PP(1, 0)
-            // Pong
-            NTT_MATMUL_PP(0, 1)
+            if (K % 2 == 0) {
+                NTT_MATMUL_TAIL(1)
+            } else {
+                NTT_MATMUL_TAIL(0)
+            }
         }
-
-        // 3. Tail
-        if (K % 2 == 0) {
-            NTT_MATMUL_PP(1, 0)
-            NTT_MATMUL_TAIL(1)
-        } else {
-            NTT_MATMUL_TAIL(0)
-        }
-
-        ntt::store(c0(0, 0), c0_0_0);
-        ntt::store(c0(0, 1), c0_0_1);
-        ntt::store(c0(0, 2), c0_0_2);
-        ntt::store(c0(0, 3), c0_0_3);
-        ntt::store(c0(1, 0), c0_1_0);
-        ntt::store(c0(1, 1), c0_1_1);
-        ntt::store(c0(1, 2), c0_1_2);
-        ntt::store(c0(1, 3), c0_1_3);
-        ntt::store(c0(2, 0), c0_2_0);
-        ntt::store(c0(2, 1), c0_2_1);
-        ntt::store(c0(2, 2), c0_2_2);
-        ntt::store(c0(2, 3), c0_2_3);
-        ntt::store(c0(3, 0), c0_3_0);
-        ntt::store(c0(3, 1), c0_3_1);
-        ntt::store(c0(3, 2), c0_3_2);
-        ntt::store(c0(3, 3), c0_3_3);
 
 #undef NTT_MATMUL_PING
 #undef NTT_MATMUL_TAIL
