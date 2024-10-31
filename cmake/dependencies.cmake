96d0f200c53c895980ab20bb18c7ad1207850d2d
sunnycase <sunnycase@live.cn>
2024-10-31 03:39:48 +0000

    Fix dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,15 +1,11 @@
-find_package(gsl-lite REQUIRED)
 if (ENABLE_OPENMP)
     find_package(OpenMP COMPONENTS CXX REQUIRED)
 endif ()
 
 if (NOT BUILDING_RUNTIME)
-    find_package(absl REQUIRED)
     find_package(nethost REQUIRED)
     find_package(fmt REQUIRED)
-    find_package(magic_enum REQUIRED)
-    find_package(spdlog REQUIRED)
-    find_package(inja REQUIRED)
+    find_package(nlohmann_json REQUIRED)
 endif ()
 
 if (BUILD_TESTING)
