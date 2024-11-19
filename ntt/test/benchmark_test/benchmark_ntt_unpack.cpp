eb7eb69a7735c29a3023cc70043fe3882448725d
guodongliang <guodongliang@canaan-creative.com>
2024-11-19 10:08:22 +0800

    revise some special unpack

--- ntt/test/benchmark_test/benchmark_ntt_unpack.cpp
+++ ntt/test/benchmark_test/benchmark_ntt_unpack.cpp
@@ -94,10 +94,10 @@ int main(int argc, char *argv[]) {
     benchmark_ntt_unpack<ntt::vector<float, P, P>, 4, 4, 3 * P, 4 * P, 2, 3>(
         "HW", 300);
 #elif __x86_64__
-    benchmark_ntt_unpack<ntt::vector<float, P>, P * 8, 2, 2, 2, 0>("N", 2000);
+    benchmark_ntt_unpack<ntt::vector<float, P>, P * 8, 2, 4, 4, 0>("N", 2000);
     benchmark_ntt_unpack<ntt::vector<float, P>, 2, 8 * P, 2, 4, 1>("C", 2000);
     benchmark_ntt_unpack<ntt::vector<float, P>, 2, 2, 8 * P, 8, 2>("H", 2000);
-    benchmark_ntt_unpack<ntt::vector<float, P>, 2, 2, 2, 8 * P, 3>("W", 2000);
+    benchmark_ntt_unpack<ntt::vector<float, P>, 2, 8, 8, 8 * P, 3>("W", 2000);
     benchmark_ntt_unpack<ntt::vector<float, P, P>, 4 * P, 8 * P, 2, 4, 0, 1>(
         "NC", 1);
     benchmark_ntt_unpack<ntt::vector<float, P, P>, 2, 4 * P, 8 * P, 8, 1, 2>(
