a5f92338b6a3698558b08b2c0f9b683c402e934c
sunnycase <sunnycase@live.cn>
2024-12-03 07:45:25 +0000

    Fix

--- ntt/include/nncase/ntt/kernels/reshard.h
+++ ntt/include/nncase/ntt/kernels/reshard.h
@@ -126,7 +126,7 @@ struct reshard_impl<SrcTensor, DestTensor> {
             constexpr auto non_split_tensor_axes = get_non_split_tensor_axes();
 #ifndef __APPLE__
             constexpr auto split_counts =
-                get_non_split_tensor_axes_split_counts(global_shape);
+                get_non_split_tensor_axes_split_counts(global_shape_type{});
             constexpr auto split_count_strides = default_strides(split_counts);
 #else
             auto split_counts =
