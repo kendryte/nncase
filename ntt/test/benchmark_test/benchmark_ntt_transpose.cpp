894bcc9e648e3962b3cc12c072ce20842461c091
zhangyang2057 <zhangyang@canaan-creative.com>
2024-12-05 14:09:44 +0800

    Update benchmark_ntt_transpose and avx/rvv roofline.

--- ntt/test/benchmark_test/benchmark_ntt_transpose.cpp
+++ ntt/test/benchmark_test/benchmark_ntt_transpose.cpp
@@ -24,19 +24,33 @@
 
 using namespace nncase;
 
-template <size_t N, size_t C, size_t H, size_t W, size_t perm_n, size_t perm_c,
-          size_t perm_h, size_t perm_w>
+template <size_t perm_n, size_t perm_c, size_t perm_h, size_t perm_w>
 void benchmark_ntt_transpose(const std::string &mode) {
-
+#if __riscv
+    constexpr size_t n = 4;
+    constexpr size_t c = 5;
+    constexpr size_t h = 6;
+    constexpr size_t w = 8;
+#elif __x86_64__
+    constexpr size_t n = 6;
+    constexpr size_t c = 6;
+    constexpr size_t h = 6;
+    constexpr size_t w = 6;
+#else
+    constexpr size_t n = 4;
+    constexpr size_t c = 5;
+    constexpr size_t h = 6;
+    constexpr size_t w = 8;
+#endif
     constexpr size_t run_size = 2000;
     constexpr size_t P = NTT_VLEN / (sizeof(float) * 8);
 
-    constexpr std::array<size_t, 4> org_dims = {N, C, H, W};
+    constexpr std::array<size_t, 4> org_dims = {n, c, h, w};
     constexpr std::array<size_t, 4> new_dims = {
         org_dims[perm_n], org_dims[perm_c], org_dims[perm_h], org_dims[perm_w]};
 
     using tensor_type1 =
-        ntt::tensor<ntt::vector<float, P>, ntt::fixed_shape<N, C, H, W>>;
+        ntt::tensor<ntt::vector<float, P>, ntt::fixed_shape<n, c, h, w>>;
     using tensor_type2 = ntt::tensor<
         ntt::vector<float, P>,
         ntt::fixed_shape<new_dims[0], new_dims[1], new_dims[2], new_dims[3]>>;
@@ -50,6 +64,9 @@ void benchmark_ntt_transpose(const std::string &mode) {
     for (size_t i = 0; i < warmup_size; i++) {
         ntt::transpose<ntt::fixed_shape<perm_n, perm_c, perm_h, perm_w>>(
             ntt_input, ntt_output);
+#if __x86_64__
+        asm volatile("" ::"g"(ntt_output));
+#endif
     }
 
     // run
@@ -63,17 +80,6 @@ void benchmark_ntt_transpose(const std::string &mode) {
     }
     auto t2 = NttTest::get_cpu_cycle();
 
-    // run
-    t1 = NttTest::get_cpu_cycle();
-    for (size_t i = 0; i < run_size; i++) {
-        ntt::transpose<ntt::fixed_shape<perm_n, perm_c, perm_h, perm_w>>(
-            ntt_input, ntt_output);
-#if __x86_64__
-        asm volatile("" ::"g"(ntt_output));
-#endif
-    }
-    t2 = NttTest::get_cpu_cycle();
-
     auto element_size = tensor_type2::size();
     std::cout << __FUNCTION__ << "_" << mode << " took " << std::setprecision(1)
               << std::fixed
@@ -81,19 +87,33 @@ void benchmark_ntt_transpose(const std::string &mode) {
               << " cycles" << std::endl;
 }
 
-template <size_t N, size_t C, size_t H, size_t W, size_t perm_n, size_t perm_c,
-          size_t perm_h, size_t perm_w>
+template <size_t perm_n, size_t perm_c, size_t perm_h, size_t perm_w>
 void benchmark_ntt_transpose_warmup([[maybe_unused]] const std::string &mode) {
-
+#if __riscv
+    constexpr size_t n = 4;
+    constexpr size_t c = 5;
+    constexpr size_t h = 6;
+    constexpr size_t w = 8;
+#elif __x86_64__
+    constexpr size_t n = 6;
+    constexpr size_t c = 6;
+    constexpr size_t h = 6;
+    constexpr size_t w = 6;
+#else
+    constexpr size_t n = 4;
+    constexpr size_t c = 5;
+    constexpr size_t h = 6;
+    constexpr size_t w = 8;
+#endif
     constexpr size_t run_size = 2000;
     constexpr size_t P = NTT_VLEN / (sizeof(float) * 8);
 
-    constexpr std::array<size_t, 4> org_dims = {N, C, H, W};
+    constexpr std::array<size_t, 4> org_dims = {n, c, h, w};
     constexpr std::array<size_t, 4> new_dims = {
         org_dims[perm_n], org_dims[perm_c], org_dims[perm_h], org_dims[perm_w]};
 
     using tensor_type1 =
-        ntt::tensor<ntt::vector<float, P>, ntt::fixed_shape<N, C, H, W>>;
+        ntt::tensor<ntt::vector<float, P>, ntt::fixed_shape<n, c, h, w>> ;
     using tensor_type2 = ntt::tensor<
         ntt::vector<float, P>,
         ntt::fixed_shape<new_dims[0], new_dims[1], new_dims[2], new_dims[3]>>;
@@ -107,6 +127,9 @@ void benchmark_ntt_transpose_warmup([[maybe_unused]] const std::string &mode) {
     for (size_t i = 0; i < warmup_size; i++) {
         ntt::transpose<ntt::fixed_shape<perm_n, perm_c, perm_h, perm_w>>(
             ntt_input, ntt_output);
+#if __x86_64__
+        asm volatile("" ::"g"(ntt_output));
+#endif
     }
 
     // run
@@ -125,46 +148,29 @@ int main(int argc, char *argv[]) {
     (void)argc;
     (void)argv;
 
-#if __riscv
-    constexpr size_t n = 4;
-    constexpr size_t c = 5;
-    constexpr size_t h = 6;
-    constexpr size_t w = 8;
-#elif __x86_64__
-    constexpr size_t n = 6;
-    constexpr size_t c = 6;
-    constexpr size_t h = 6;
-    constexpr size_t w = 6;
-#else
-    constexpr size_t n = 4;
-    constexpr size_t c = 5;
-    constexpr size_t h = 6;
-    constexpr size_t w = 8;
-#endif
-
-    benchmark_ntt_transpose_warmup<n, c, h, w, 0, 1, 2, 3>("NCHW");
-    benchmark_ntt_transpose<n, c, h, w, 0, 1, 2, 3>("NCHW");
-    benchmark_ntt_transpose<n, c, h, w, 0, 1, 3, 2>("NCWH");
-    benchmark_ntt_transpose<n, c, h, w, 0, 2, 1, 3>("NHCW");
-    benchmark_ntt_transpose<n, c, h, w, 0, 2, 3, 1>("NHWC");
-    benchmark_ntt_transpose<n, c, h, w, 0, 3, 1, 2>("NWCH");
-    benchmark_ntt_transpose<n, c, h, w, 0, 3, 2, 1>("NWHC");
-    benchmark_ntt_transpose<n, c, h, w, 1, 0, 2, 3>("CNHW");
-    benchmark_ntt_transpose<n, c, h, w, 1, 0, 3, 2>("CNWH");
-    benchmark_ntt_transpose<n, c, h, w, 1, 2, 0, 3>("CHNW");
-    benchmark_ntt_transpose<n, c, h, w, 1, 2, 3, 0>("CHWN");
-    benchmark_ntt_transpose<n, c, h, w, 1, 3, 0, 2>("CWNH");
-    benchmark_ntt_transpose<n, c, h, w, 1, 3, 2, 0>("CWHN");
-    benchmark_ntt_transpose<n, c, h, w, 2, 0, 1, 3>("HNCW");
-    benchmark_ntt_transpose<n, c, h, w, 2, 0, 3, 1>("HNWC");
-    benchmark_ntt_transpose<n, c, h, w, 2, 1, 0, 3>("HCNW");
-    benchmark_ntt_transpose<n, c, h, w, 2, 1, 3, 0>("HCWN");
-    benchmark_ntt_transpose<n, c, h, w, 2, 3, 0, 1>("HWNC");
-    benchmark_ntt_transpose<n, c, h, w, 2, 3, 1, 0>("HWCN");
-    benchmark_ntt_transpose<n, c, h, w, 3, 0, 1, 2>("WNCH");
-    benchmark_ntt_transpose<n, c, h, w, 3, 0, 2, 1>("WNHC");
-    benchmark_ntt_transpose<n, c, h, w, 3, 1, 0, 2>("WCNH");
-    benchmark_ntt_transpose<n, c, h, w, 3, 1, 2, 0>("WCHN");
-    benchmark_ntt_transpose<n, c, h, w, 3, 2, 0, 1>("WHNC");
-    benchmark_ntt_transpose<n, c, h, w, 3, 2, 1, 0>("WHCN");
+    benchmark_ntt_transpose_warmup<0, 1, 2, 3>("NCHW");
+    benchmark_ntt_transpose<0, 1, 2, 3>("NCHW");
+    benchmark_ntt_transpose<0, 1, 3, 2>("NCWH");
+    benchmark_ntt_transpose<0, 2, 1, 3>("NHCW");
+    benchmark_ntt_transpose<0, 2, 3, 1>("NHWC");
+    benchmark_ntt_transpose<0, 3, 1, 2>("NWCH");
+    benchmark_ntt_transpose<0, 3, 2, 1>("NWHC");
+    benchmark_ntt_transpose<1, 0, 2, 3>("CNHW");
+    benchmark_ntt_transpose<1, 0, 3, 2>("CNWH");
+    benchmark_ntt_transpose<1, 2, 0, 3>("CHNW");
+    benchmark_ntt_transpose<1, 2, 3, 0>("CHWN");
+    benchmark_ntt_transpose<1, 3, 0, 2>("CWNH");
+    benchmark_ntt_transpose<1, 3, 2, 0>("CWHN");
+    benchmark_ntt_transpose<2, 0, 1, 3>("HNCW");
+    benchmark_ntt_transpose<2, 0, 3, 1>("HNWC");
+    benchmark_ntt_transpose<2, 1, 0, 3>("HCNW");
+    benchmark_ntt_transpose<2, 1, 3, 0>("HCWN");
+    benchmark_ntt_transpose<2, 3, 0, 1>("HWNC");
+    benchmark_ntt_transpose<2, 3, 1, 0>("HWCN");
+    benchmark_ntt_transpose<3, 0, 1, 2>("WNCH");
+    benchmark_ntt_transpose<3, 0, 2, 1>("WNHC");
+    benchmark_ntt_transpose<3, 1, 0, 2>("WCNH");
+    benchmark_ntt_transpose<3, 1, 2, 0>("WCHN");
+    benchmark_ntt_transpose<3, 2, 0, 1>("WHNC");
+    benchmark_ntt_transpose<3, 2, 1, 0>("WHCN");
 }
