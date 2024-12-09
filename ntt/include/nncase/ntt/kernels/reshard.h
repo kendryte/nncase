dd5ea81d9e8d5bd18ca96e3ad78c432ce16691b9
huochenghai <huochenghai@canaan-creative.com>
2024-12-09 14:33:08 +0800

    fix newlib build

--- ntt/include/nncase/ntt/kernels/reshard.h
+++ ntt/include/nncase/ntt/kernels/reshard.h
@@ -208,7 +208,11 @@ struct reshard_impl<SrcTensor, DestTensor> {
             auto total_log_dim =
                 std::accumulate(log_dims.begin(), log_dims.end(), 0.f);
             for (size_t i = 0; i < non_split_tensor_axes.rank(); i++) {
+#if defined(__NEWLIB__)
+                float split_factor = total_log_dim == 0.f
+#else
                 auto split_factor = total_log_dim == 0.f
+#endif
                                         ? 0.f
                                         : (log_dims[i] / total_log_dim *
                                            std::log(expected_split_count));
