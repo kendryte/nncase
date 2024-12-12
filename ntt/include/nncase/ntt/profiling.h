cb2b5198e5005abdee8cf880ecd9fbd24de1cb5c
guodongliang <guodongliang@canaan-creative.com>
2024-12-12 16:54:43 +0800

    add topology for profiler

--- ntt/include/nncase/ntt/profiling.h
+++ ntt/include/nncase/ntt/profiling.h
@@ -15,6 +15,7 @@
 
 #pragma once
 
+#include "distributed.h"
 #include <chrono>
 #include <fstream>
 #include <iomanip>
@@ -23,9 +24,23 @@
 #include <unordered_map>
 #include <vector>
 
+using namespace nncase::ntt;
+using namespace nncase::ntt::distributed;
+
 namespace nncase::ntt {
+
+#define CHIP_COUNTER 2
+#define BLOCK_COUNTER 3
+#define THREAD_COUNTER 4
+
 class ntt_profiler {
   public:
+    struct instance_id {
+        int cid = -1;
+        int bid = -1;
+        int tid = -1;
+    };
+
     struct call_instance {
         uint64_t start_time;
         uint64_t end_time;
@@ -38,8 +53,21 @@ class ntt_profiler {
     };
 
     static ntt_profiler &get_instance() {
-        static ntt_profiler instance;
-        return instance;
+        static ntt_profiler instances[CHIP_COUNTER][BLOCK_COUNTER]
+                                     [THREAD_COUNTER];
+
+        auto cid = program_id<topology::chip>();
+        auto bid = program_id<topology::block>();
+        auto tid = program_id<topology::thread>();
+
+        instances[cid][bid][tid].instance_id_ = {(int)cid, (int)bid, (int)tid};
+
+        return instances[cid][bid][tid];
+    }
+
+    bool is_valid() const {
+        return instance_id_.cid != -1 && instance_id_.bid != -1 &&
+               instance_id_.tid != -1;
     }
 
     // 记录开始时间
@@ -56,165 +84,185 @@ class ntt_profiler {
 
     // print statistics
     void console_print() const {
-        uint64_t total_time = 0;
-        for (const auto &[name, stats] : function_stats_) {
-            total_time += stats.total_time;
-        }
 
-        std::cout << "\033[34m\nStatistics for NTT kernels. Total time: "
-                  << total_time << " microseconds. \033[0m\n";
-        for (const auto &[name, stats] : function_stats_) {
-            std::cout << "Function: " << name << "\n";
-            std::cout << "\tCalls: " << stats.call_count << "\n";
-            std::cout << "\tTotal time: " << stats.total_time
-                      << " microseconds\n";
-            std::cout << "\tTime Ratio: " << std::fixed << std::setprecision(2)
-                      << static_cast<double>(stats.total_time) /
-                             static_cast<double>(total_time)
-                      << std::endl;
-            uint64_t call_count = 0;
-            for (const auto &call : stats.calls) {
-                std::cout << "\t\t" << "Call " << call_count++ << ": \n";
-                std::cout << "\t\tStart time: " << call.start_time
+        if (is_valid()) {
+            uint64_t total_time = 0;
+            for (const auto &[name, stats] : function_stats_) {
+                total_time += stats.total_time;
+            }
+
+            std::cout << "\033[34m\n"
+                      << "Core Id:" << instance_id_.cid
+                      << ", Block Id:" << instance_id_.bid
+                      << ", Thread Id:" << instance_id_.tid << "\033[0m\n";
+            std::cout << "\033[34mStatistics for NTT kernels. Total time: "
+                      << total_time << " microseconds. \033[0m\n";
+            for (const auto &[name, stats] : function_stats_) {
+                std::cout << "Function: " << name << "\n";
+                std::cout << "\tCalls: " << stats.call_count << "\n";
+                std::cout << "\tTotal time: " << stats.total_time
                           << " microseconds\n";
-                std::cout << "\t\tEnd time: " << call.end_time
-                          << " microseconds\n";
-                std::cout << "\t\tDuration: " << call.end_time - call.start_time
-                          << " microseconds\n";
+                std::cout << "\tTime Ratio: " << std::fixed
+                          << std::setprecision(2)
+                          << static_cast<double>(stats.total_time) /
+                                 static_cast<double>(total_time)
+                          << std::endl;
+                uint64_t call_count = 0;
+                for (const auto &call : stats.calls) {
+                    std::cout << "\t\t" << "Call " << call_count++ << ": \n";
+                    std::cout << "\t\tStart time: " << call.start_time
+                              << " microseconds\n";
+                    std::cout << "\t\tEnd time: " << call.end_time
+                              << " microseconds\n";
+                    std::cout
+                        << "\t\tDuration: " << call.end_time - call.start_time
+                        << " microseconds\n";
+                }
             }
         }
     }
 
     void csv_print(const std::string &filename) const {
-        std::ofstream csv_file(filename);
-        if (!csv_file.is_open()) {
-            std::cerr << "Failed to open file: " << filename << std::endl;
-            return;
-        }
-
-        // Write CSV headers
-        csv_file << "Function,Calls,Total Time (microseconds),Time Ratio,Call "
-                    "Index,Start Time (microseconds),End Time "
-                    "(microseconds),Duration (microseconds)\n";
-
-        uint64_t total_time = 0;
-        for (const auto &[name, stats] : function_stats_) {
-            total_time += stats.total_time;
-        }
-
-        for (const auto &[name, stats] : function_stats_) {
-            double time_ratio = static_cast<double>(stats.total_time) /
-                                static_cast<double>(total_time);
-            for (size_t i = 0; i < stats.calls.size(); ++i) {
-                const auto &call = stats.calls[i];
-                uint64_t duration = call.end_time - call.start_time;
-
-                // Write each call's data to the CSV file
-                csv_file << name << ",";
-                csv_file << stats.call_count << ",";
-                csv_file << stats.total_time << ",";
-                csv_file << std::fixed << std::setprecision(2) << time_ratio
-                         << ",";
-                csv_file << i << ",";
-                csv_file << call.start_time << ",";
-                csv_file << call.end_time << ",";
-                csv_file << duration << "\n";
+        if (is_valid()) {
+            std::ofstream csv_file(filename);
+            if (!csv_file.is_open()) {
+                std::cerr << "Failed to open file: " << filename << std::endl;
+                return;
             }
-        }
 
-        csv_file.close();
+            // Write CSV headers
+            csv_file
+                << "Function,Calls,Total Time (microseconds),Time Ratio,Call "
+                   "Index,Start Time (microseconds),End Time "
+                   "(microseconds),Duration (microseconds)\n";
+
+            uint64_t total_time = 0;
+            for (const auto &[name, stats] : function_stats_) {
+                total_time += stats.total_time;
+            }
+
+            for (const auto &[name, stats] : function_stats_) {
+                double time_ratio = static_cast<double>(stats.total_time) /
+                                    static_cast<double>(total_time);
+                for (size_t i = 0; i < stats.calls.size(); ++i) {
+                    const auto &call = stats.calls[i];
+                    uint64_t duration = call.end_time - call.start_time;
+
+                    // Write each call's data to the CSV file
+                    csv_file << name << ",";
+                    csv_file << stats.call_count << ",";
+                    csv_file << stats.total_time << ",";
+                    csv_file << std::fixed << std::setprecision(2) << time_ratio
+                             << ",";
+                    csv_file << i << ",";
+                    csv_file << call.start_time << ",";
+                    csv_file << call.end_time << ",";
+                    csv_file << duration << "\n";
+                }
+            }
+
+            csv_file.close();
+        }
     }
 
     void markdown_print(const std::string &filename) const {
 
-        std::ofstream md_file(filename);
-        if (!md_file.is_open()) {
-            std::cerr << "Failed to open file: " << filename << std::endl;
-            return;
-        }
-
-        uint64_t total_time = 0;
-        for (const auto &[name, stats] : function_stats_) {
-            total_time += stats.total_time;
-        }
-
-        // Write the header of the Markdown file
-        md_file << "# NTT Kernel Statistics\n\n";
-        md_file << "Total time: **" << total_time << " microseconds**\n\n";
-
-        for (const auto &[name, stats] : function_stats_) {
-            md_file << "## Function: " << name << "\n";
-            md_file << "- Calls: **" << stats.call_count << "**\n";
-            md_file << "- Total Time: **" << stats.total_time
-                    << " microseconds**\n";
-            md_file << "- Time Ratio: **" << std::fixed << std::setprecision(2)
-                    << static_cast<double>(stats.total_time) /
-                           static_cast<double>(total_time)
-                    << "**\n\n";
-
-            // Write a table for the function calls
-            md_file << "| Call Index | Start Time (microseconds) | End Time "
-                       "(microseconds) | Duration (microseconds) |\n";
-            md_file << "|------------|----------------------------|------------"
-                       "--------------|-------------------------|\n";
-
-            for (size_t i = 0; i < stats.calls.size(); ++i) {
-                const auto &call = stats.calls[i];
-                uint64_t duration = call.end_time - call.start_time;
-
-                md_file << "| " << i << " | " << call.start_time << " | "
-                        << call.end_time << " | " << duration << " |\n";
+        if (is_valid()) {
+            std::ofstream md_file(filename);
+            if (!md_file.is_open()) {
+                std::cerr << "Failed to open file: " << filename << std::endl;
+                return;
             }
 
-            md_file << "\n";
+            uint64_t total_time = 0;
+            for (const auto &[name, stats] : function_stats_) {
+                total_time += stats.total_time;
+            }
+
+            // Write the header of the Markdown file
+            md_file << "# NTT Kernel Statistics\n\n";
+            md_file << "Total time: **" << total_time << " microseconds**\n\n";
+
+            for (const auto &[name, stats] : function_stats_) {
+                md_file << "## Function: " << name << "\n";
+                md_file << "- Calls: **" << stats.call_count << "**\n";
+                md_file << "- Total Time: **" << stats.total_time
+                        << " microseconds**\n";
+                md_file << "- Time Ratio: **" << std::fixed
+                        << std::setprecision(2)
+                        << static_cast<double>(stats.total_time) /
+                               static_cast<double>(total_time)
+                        << "**\n\n";
+
+                // Write a table for the function calls
+                md_file
+                    << "| Call Index | Start Time (microseconds) | End Time "
+                       "(microseconds) | Duration (microseconds) |\n";
+                md_file
+                    << "|------------|----------------------------|------------"
+                       "--------------|-------------------------|\n";
+
+                for (size_t i = 0; i < stats.calls.size(); ++i) {
+                    const auto &call = stats.calls[i];
+                    uint64_t duration = call.end_time - call.start_time;
+
+                    md_file << "| " << i << " | " << call.start_time << " | "
+                            << call.end_time << " | " << duration << " |\n";
+                }
+
+                md_file << "\n";
+            }
+
+            md_file << "\n*Note*: The `Time Ratio` is the fraction of the "
+                       "total time "
+                       "taken by each function.\n";
+
+            md_file.close();
         }
-
-        md_file
-            << "\n*Note*: The `Time Ratio` is the fraction of the total time "
-               "taken by each function.\n";
-
-        md_file.close();
     }
 
     void json_print(const std::string &filename) const {
 
-        std::ofstream json_file(filename);
-        if (!json_file.is_open()) {
-            std::cerr << "Failed to open file: " << filename << std::endl;
-            return;
-        }
-
-        // Write the JSON preamble
-        json_file << "[\n";
-
-        bool first_function = true;
-
-        for (const auto &[name, stats] : function_stats_) {
-            for (const auto &call : stats.calls) {
-                if (!first_function) {
-                    json_file << ",\n";
-                }
-                first_function = false;
-
-                json_file << "  {\n";
-                json_file << "    \"name\": \"" << name << "\",\n";
-                json_file
-                    << "    \"ph\": \"X\",\n"; // "X" indicates a complete event
-                json_file << "    \"ts\": " << call.start_time << ",\n";
-                json_file << "    \"dur\": "
-                          << (call.end_time - call.start_time) << ",\n";
-                json_file << "    \"pid\": 0,\n"; // Process ID (arbitrary, use
-                                                  // 0 for simplicity)
-                json_file << "    \"tid\": 0\n";  // Thread ID (arbitrary, use 0
-                                                  // for simplicity)
-                json_file << "  }";
+        if (is_valid()) {
+            std::ofstream json_file(filename);
+            if (!json_file.is_open()) {
+                std::cerr << "Failed to open file: " << filename << std::endl;
+                return;
             }
+
+            // Write the JSON preamble
+            json_file << "[\n";
+
+            bool first_function = true;
+
+            for (const auto &[name, stats] : function_stats_) {
+                for (const auto &call : stats.calls) {
+                    if (!first_function) {
+                        json_file << ",\n";
+                    }
+                    first_function = false;
+
+                    json_file << "  {\n";
+                    json_file << "    \"name\": \"" << name << "\",\n";
+                    json_file << "    \"ph\": \"X\",\n"; // "X" indicates a
+                                                         // complete event
+                    json_file << "    \"ts\": " << call.start_time << ",\n";
+                    json_file
+                        << "    \"dur\": " << (call.end_time - call.start_time)
+                        << ",\n";
+                    json_file << "    \"pid\": 0,\n"; // Process ID (arbitrary,
+                                                      // use 0 for simplicity)
+                    json_file << "    \"tid\": 0\n";  // Thread ID (arbitrary,
+                                                      // use 0 for simplicity)
+                    json_file << "  }";
+                }
+            }
+
+            // End the JSON array
+            json_file << "\n]\n";
+
+            json_file.close();
         }
-
-        // End the JSON array
-        json_file << "\n]\n";
-
-        json_file.close();
     }
 
   private:
@@ -234,6 +282,8 @@ class ntt_profiler {
     }
 
     std::unordered_map<std::string, function_stats> function_stats_;
+
+    instance_id instance_id_ = {-1, -1, -1};
 };
 
 // auto_profiler, start timing and end timing
