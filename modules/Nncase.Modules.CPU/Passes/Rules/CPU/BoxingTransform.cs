852ba16cdebe34db778faa1acc669eddd4db2a7a
huochenghai <huochenghai@canaan-creative.com>
2025-02-08 15:19:46 +0800

    Split PartialAndReshardBoxing

--- modules/Nncase.Modules.CPU/Passes/Rules/CPU/BoxingTransform.cs
+++ modules/Nncase.Modules.CPU/Passes/Rules/CPU/BoxingTransform.cs
@@ -50,3 +50,30 @@ public partial class UnfoldDistributedConst : RewriteRule<Pattern>
         return null;
     }
 }
+
+[RuleGenerator]
+public partial class SplitPartialAndReshardBoxing : RewriteRule<Pattern>
+{
+    /// <inheritdoc/>
+    public override Pattern Pattern { get; } = IsBoxing(
+        target_name: "boxing",
+        call_name: "call",
+        _ => true,
+        IsWildcard("input"));
+
+    private Expr? GetReplace(Call call, Expr input)
+    {
+        if (input.CheckedType is DistributedType it && it.NdSBP.Any(sbp => sbp is SBPPartial) && call.CheckedType is DistributedType ot)
+        {
+            var newSBPs = it.NdSBP.Select(sbp => sbp is SBPPartial ? SBP.B : sbp).ToArray();
+            if (newSBPs.Length != ot.NdSBP.Count || Enumerable.Range(0, newSBPs.Length).Any(i => newSBPs[i] != ot.NdSBP[i]))
+            {
+                return IR.F.CPU.Boxing(IR.F.CPU.Boxing(input, new DistributedType(it.TensorType, newSBPs, it.Placement)), ot);
+            }
+
+            return null;
+        }
+
+        return null;
+    }
+}
