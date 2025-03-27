c853e22028b06ce12ac5516036c79df2473764c3
CurioYang <curioyhq@gmail.com>
2025-03-27 06:04:33 +0000

    remove redundant code

--- src/Nncase.Importer/HuggingFace/Llama3.2.cs
+++ src/Nncase.Importer/HuggingFace/Llama3.2.cs
@@ -1,42 +1,9 @@
 ﻿// Copyright (c) Canaan Inc. All rights reserved.
 // Licensed under the Apache license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Xml;
-using CommunityToolkit.HighPerformance;
-using DryIoc;
-using LanguageExt;
-using NetFabric.Hyperlinq;
-using Nncase.Diagnostics;
-using Nncase.IR;
-using Nncase.IR.Math;
-using Nncase.IR.NN;
-using Nncase.IR.Tensors;
-using Nncase.TIR;
-using Nncase.Utilities;
-using static Nncase.IR.F.Math;
-using static Nncase.IR.F.NN;
-using static Nncase.IR.F.Tensors;
-using Buffer = System.Buffer;
-using F = Nncase.IR.F;
-using Tuple = System.Tuple;
-
 namespace Nncase.Importer
 {
-    // public partial class HuggingFaceImporter
-    // {
     public class Llama3_2 : HuggingFaceModel
     {
-        private ModelInitContext? _context;
-
-        public override void Initialize(ModelInitContext context, string dir)
-        {
-            base.Initialize(context, dir);
-            _context = context;
-        }
     }
 }
