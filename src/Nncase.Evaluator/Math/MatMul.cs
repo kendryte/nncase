08af32f263fe15506f822295bdec581b4caef705
huochenghai <huochenghai@canaan-creative.com>
2025-04-10 11:35:50 +0800

    fix matmul eval with float16

--- src/Nncase.Evaluator/Math/MatMul.cs
+++ src/Nncase.Evaluator/Math/MatMul.cs
@@ -263,7 +263,7 @@ public class MatMulEvaluator : IEvaluator<MatMul>, ITypeInferencer<MatMul>, ICos
             return new InvalidType("MatMul lhs and rhs have not compatiable shape");
         }
 
-        if (lhsDType == DataTypes.Float16 || lhsDType == DataTypes.Float8E4M3 || lhsDType == DataTypes.Float8E5M2 || lhsDType == DataTypes.Int8)
+        if (lhsDType == DataTypes.Float8E4M3 || lhsDType == DataTypes.Float8E5M2 || lhsDType == DataTypes.Int8)
         {
             dtype = DataTypes.Float32;
         }
