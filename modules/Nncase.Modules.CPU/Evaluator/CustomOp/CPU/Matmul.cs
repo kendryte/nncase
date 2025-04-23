1de1b849322761886c97ab36178cd0c496639e5d
huochenghai <huochenghai@canaan-creative.com>
2025-04-23 10:07:01 +0800

    update type infer of custom matmul

--- modules/Nncase.Modules.CPU/Evaluator/CustomOp/CPU/Matmul.cs
+++ modules/Nncase.Modules.CPU/Evaluator/CustomOp/CPU/Matmul.cs
@@ -8,6 +8,7 @@ using DryIoc.ImTools;
 using Nncase.CostModel;
 using Nncase.Diagnostics;
 using Nncase.IR;
+using Nncase.IR.F;
 using Nncase.IR.Math;
 using Nncase.TIR.CPU;
 using Nncase.Utilities;
@@ -25,6 +26,7 @@ public class MatMulEvaluator : IEvaluator<MatMul>, ITypeInferencer<MatMul>, ICos
     /// <inheritdoc/>
     public IValue Visit(IEvaluateContext context, MatMul matMul)
     {
+        // TODO: rewrite this to use OrtKISharp
         var dataType = context.CurrentCall.Arguments[Matmul.Lhs.Index].CheckedDataType;
         var lhs = context.GetArgumentValue(matMul, Matmul.Lhs).AsTensor();
         var rhs = context.GetArgumentValue(matMul, MatMul.Rhs).AsTensor();
@@ -48,8 +50,8 @@ public class MatMulEvaluator : IEvaluator<MatMul>, ITypeInferencer<MatMul>, ICos
         {
             return (lhs, rhs) switch
             {
-                (DistributedType a, DistributedType b) => new DistributedType((TensorType)Math.MatMulEvaluator.VisitTensorType(a.TensorType, b.TensorType, true, dimInfo), target.OutSBPs, a.Placement),
-                (TensorType a, TensorType b) => Math.MatMulEvaluator.VisitTensorType(a, b, true, dimInfo),
+                (DistributedType a, DistributedType b) => new DistributedType((TensorType)VisitTensorType(a.TensorType, b.TensorType, true, dimInfo), target.OutSBPs, a.Placement),
+                (TensorType a, TensorType b) => VisitTensorType(a, b, true, dimInfo),
                 _ => new InvalidType($"{lhs} {rhs} not support"),
             };
         }
@@ -81,5 +83,73 @@ public class MatMulEvaluator : IEvaluator<MatMul>, ITypeInferencer<MatMul>, ICos
         }
 
         return true;
+    }
+
+    private IRType VisitTensorType(TensorType lhs, TensorType rhs, bool packingK = false, Math.MatMulEvaluator.DimInfo? dimInfo = null)
+    {
+        if (lhs.Shape.IsUnranked || rhs.Shape.IsUnranked)
+        {
+            return new TensorType(lhs.DType, Shape.Unranked);
+        }
+
+        var (lm, lk, rk, rn) = dimInfo ?? new(lhs.Shape.Rank - 2, lhs.Shape.Rank - 1, rhs.Shape.Rank - 2, rhs.Shape.Rank - 1);
+        DataType dtype = lhs.DType;
+        DataType lhsDType = lhs.DType is VectorType l ? l.ElemType : lhs.DType;
+        DataType rhsDType = rhs.DType is VectorType r ? r.ElemType : rhs.DType;
+        if (lhsDType != rhsDType)
+        {
+            return new InvalidType("MatMul lhs and rhs have different DType");
+        }
+
+        if (lhs.Shape[lk] != rhs.Shape[rk] && lhs.Shape[lk].IsFixed && rhs.Shape[rk].IsFixed)
+        {
+            return new InvalidType("MatMul lhs and rhs have not compatiable shape");
+        }
+
+        if (lhsDType == DataTypes.Float8E4M3 || lhsDType == DataTypes.Float8E5M2 || lhsDType == DataTypes.Int8)
+        {
+            dtype = DataTypes.Float32;
+        }
+
+        if (lhs.DType is VectorType vl && rhs.DType is VectorType vr)
+        {
+            // pack k or m&n
+            var lhsElemType = vl.ElemType;
+            var outElementType = lhsElemType;
+            if (lhsElemType.IsFloat() && lhsElemType != DataTypes.Float32)
+            {
+                outElementType = DataTypes.Float32;
+            }
+
+            // TODO: support other custom packing
+            if (vl.Lanes.Count == 1 && vr.Lanes.Count == 1)
+            {
+                var scale = 1f * outElementType.SizeInBytes / lhsElemType.SizeInBytes;
+                dtype = packingK ? new VectorType(outElementType, (int)(vl.Lanes[0] / scale)) : new VectorType(outElementType, vl.Lanes[0], vr.Lanes[0]);
+            }
+            else
+            {
+                return new InvalidType("Not supported packing.");
+            }
+        }
+
+        var lhsShape = lhs.Shape.Rank >= rhs.Shape.Rank ? lhs.Shape.ToArray() : Enumerable.Repeat((Dimension)1, rhs.Shape.Rank - lhs.Shape.Rank).Concat(lhs.Shape).ToArray();
+        var rhsShape = lhs.Shape.Rank <= rhs.Shape.Rank ? rhs.Shape.ToArray() : Enumerable.Repeat((Dimension)1, lhs.Shape.Rank - rhs.Shape.Rank).Concat(rhs.Shape).ToArray();
+
+        var bigShape = Enumerable.Zip(lhsShape, rhsShape).SkipLast(2).Select(t =>
+            t.First.IsDynamic || t.Second.IsDynamic
+                ? (Dimension)Nncase.IR.F.Math.Max(t.First.Value, t.Second.Value)
+                : System.Math.Max(t.First.FixedValue, t.Second.FixedValue)).ToArray();
+
+        // batch and channel
+        var front = bigShape;
+
+        var end = new[] { lhs.Shape[lm], rhs.Shape[rn] };
+        if (lhs.DType is VectorType && rhs.DType is VectorType)
+        {
+            end = new[] { rhs.Shape[rn] / ((VectorType)dtype).Lanes[0], lhs.Shape[lm] };
+        }
+
+        return new TensorType(dtype, front.Concat(end).ToArray());
     }
 }
