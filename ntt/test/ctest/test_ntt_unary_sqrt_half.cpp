e820d8979f24ade67cb9f492e33aa626fa3faf6e
liuyubin0 <liuyubin@canaan-creative.com>
2025-04-24 19:17:13 +0800

    disable macOS sqrt half ulp

--- ntt/test/ctest/test_ntt_unary_sqrt_half.cpp
+++ ntt/test/ctest/test_ntt_unary_sqrt_half.cpp
@@ -176,7 +176,9 @@ template <typename T, size_t vl> void test_vector_ulp(double ulp_threshold) {
 #define TEST_VECTOR_ULP(T, ulp_threshold) _TEST_VECTOR_ULP(T, 1, ulp_threshold)
 #endif
 
+#if __riscv
 TEST(UnaryTestSqrtFloat, ulp_error) { TEST_VECTOR_ULP(_Float16, 0.) }
+#endif
 
 int main(int argc, char *argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
