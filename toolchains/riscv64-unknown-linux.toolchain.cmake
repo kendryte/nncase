db7c6d5182759320cae2a27a9b5302a5fb936ac9
CurioYang <curioyhq@gmail.com>
2025-05-16 17:15:30 +0800

    remove softmax fp16 kernel test

--- toolchains/riscv64-unknown-linux.toolchain.cmake
+++ toolchains/riscv64-unknown-linux.toolchain.cmake
@@ -25,5 +25,5 @@ set(ENABLE_VULKAN OFF)
 set(ENABLE_HALIDE OFF)
 set(BUILD_PYTHON_BINDING OFF)
 # -march=rv64gv_zvl128b_zvfh -mrvv-vector-bits=zvl
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv64gcv_zvfh -mabi=lp64d -mcmodel=medany -fstack-protector-strong -fPIE -pie -Wl,-z,now -Wl,-z,relro -mrvv-v0p10-compatible")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=rv64gcv_zvfh -mabi=lp64d -mcmodel=medany -fstack-protector-strong -fPIE -pie -Wl,-z,now -Wl,-z,relro  -mrvv-v0p10-compatible")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=rv64gcv_zfh_zvfh -mabi=lp64d -mcmodel=medany -fstack-protector-strong -fPIE -pie -Wl,-z,now -Wl,-z,relro -mrvv-v0p10-compatible")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=rv64gcv_zfh_zvfh -mabi=lp64d -mcmodel=medany -fstack-protector-strong -fPIE -pie -Wl,-z,now -Wl,-z,relro  -mrvv-v0p10-compatible")
