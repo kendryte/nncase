01bcd62de6de3b9e643f27c6e0b51af64102ba86
huochenghai <huochenghai@canaan-creative.com>
2025-05-21 10:33:11 +0800

    update Affine/TIR selection

--- modules/Nncase.Modules.NTT/Passes/AffineSelection/Reduce.cs
+++ modules/Nncase.Modules.NTT/Passes/AffineSelection/Reduce.cs
@@ -10,7 +10,7 @@ namespace Nncase.Passes;
 
 public partial class NTTAffineSelectionPass
 {
-    private Expr SelectReduce(IR.NTT.PackedReduce reduce, Call call, Expr output)
+    public Expr SelectReduce(IR.NTT.PackedReduce reduce, Call call, Expr output)
     {
         var input = (Expr)call[IR.NTT.PackedReduce.Input];
         if (output.CheckedShape is not { IsFixed: true, Rank: > 0 }
