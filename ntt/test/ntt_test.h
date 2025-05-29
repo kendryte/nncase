056190a18a33bfbf9e2ef5096845be286e2a697e
guodongliang <guodongliang@canaan-creative.com>
2025-05-29 11:54:39 +0800

    add scales for matmul

--- ntt/test/ntt_test.h
+++ ntt/test/ntt_test.h
@@ -227,7 +227,8 @@ bool compare_tensor(TTensor &lhs, TTensor &rhs, double threshold = 0.999f) {
         double norm2 = std::sqrt(
             std::inner_product(v2.begin(), v2.end(), v2.begin(), (double)0.0));
         double cosine_similarity = dotProduct / (norm1 * norm2);
-        pass = cosine_similarity > threshold;
+        pass = cosine_similarity > threshold &&
+               (norm1 > norm2 ? norm1 / norm2 < 1.01f : norm2 / norm1 < 1.01f);
         if (!pass)
             std::cerr << "cosine_similarity = " << cosine_similarity
                       << std::endl;
@@ -281,7 +282,8 @@ bool compare_tensor(ntt::tensor<ntt::vector<T, N>, Shape, Stride> &lhs,
         double norm2 = std::sqrt(
             std::inner_product(v2.begin(), v2.end(), v2.begin(), (double)0.0));
         double cosine_similarity = dotProduct / (norm1 * norm2);
-        pass = cosine_similarity > threshold;
+        pass = cosine_similarity > threshold &&
+               (norm1 > norm2 ? norm1 / norm2 < 1.01f : norm2 / norm1 < 1.01f);
         if (!pass)
             std::cerr << "cosine_similarity = " << cosine_similarity
                       << std::endl;
@@ -335,7 +337,8 @@ bool compare_tensor(ntt::tensor<ntt::vector<T, N, N>, Shape, Stride> &lhs,
         double norm2 = std::sqrt(
             std::inner_product(v2.begin(), v2.end(), v2.begin(), (double)0.0));
         double cosine_similarity = dotProduct / (norm1 * norm2);
-        pass = cosine_similarity > threshold;
+        pass = cosine_similarity > threshold &&
+               (norm1 > norm2 ? norm1 / norm2 < 1.01f : norm2 / norm1 < 1.01f);
         if (!pass)
             std::cerr << "cosine_similarity = " << cosine_similarity
                       << std::endl;
