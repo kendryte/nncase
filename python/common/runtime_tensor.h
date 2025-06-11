cfdba24b472095b4e6afb44ae787781e6edee017
curioyang <curioyang@users.noreply.github.com>
2025-06-11 10:40:45 +0000

    Apply code-format changes

--- python/common/runtime_tensor.h
+++ python/common/runtime_tensor.h
@@ -41,95 +41,103 @@ inline py::class_<runtime_tensor> register_runtime_tensor(py::module &m) {
         .def_readwrite("start", &tensor_desc::start)
         .def_readwrite("size", &tensor_desc::size);
 
-    auto rt_class = py::class_<runtime_tensor>(m, "RuntimeTensor")
-        .def_static(
-            "from_object",
-            [](const object &obj) {
-                if (!obj.is_a<llm::paged_attention_kv_cache>()) {
-                    throw std::invalid_argument(
-                        "currently only support objects type "
-                        "llm::paged_attention_kv_cache");
+    auto rt_class =
+        py::class_<runtime_tensor>(m, "RuntimeTensor")
+            .def_static("from_object",
+                        [](const object &obj) {
+                            if (!obj.is_a<llm::paged_attention_kv_cache>()) {
+                                throw std::invalid_argument(
+                                    "currently only support objects type "
+                                    "llm::paged_attention_kv_cache");
+                            }
+
+                            auto ref_type = nncase::reference_type_t(
+                                std::in_place,
+                                datatype_t::paged_attention_kv_cache);
+
+                            auto object_ptrs = new object_node *[1];
+                            auto bytes_span = as_span<std::byte>(
+                                std::span<object_node *>(object_ptrs, 1));
+                            hrt::data_deleter_t deleter = [](std::byte *ptr) {
+                                auto object_ptrs =
+                                    reinterpret_cast<object_node **>(ptr);
+                                nncase_object_release(object_ptrs[0]);
+                                delete[] object_ptrs;
+                            };
+
+                            auto runtime_tensor =
+                                hrt::create(ref_type, {}, bytes_span, deleter)
+                                    .unwrap_or_throw();
+
+                            auto host_buffer = runtime_tensor.impl()
+                                                   ->buffer()
+                                                   .as_host()
+                                                   .unwrap_or_throw();
+                            auto mapped_data =
+                                host_buffer.map(map_write).unwrap_or_throw();
+                            auto dest_buffer =
+                                as_span<object_node *>(mapped_data.buffer());
+                            dest_buffer[0] = object(obj).detach();
+                            return runtime_tensor;
+                        })
+            .def_static(
+                "from_numpy",
+                [](py::array arr) {
+                    arr = py::array::ensure(arr, py::array::c_style);
+                    auto src_buffer = arr.request();
+                    auto datatype = from_dtype(arr);
+                    auto tensor =
+                        host_runtime_tensor::create(
+                            datatype, to_rt_shape(src_buffer.shape),
+                            to_rt_strides(src_buffer.itemsize,
+                                          src_buffer.strides),
+                            std::span(
+                                reinterpret_cast<std::byte *>(src_buffer.ptr),
+                                src_buffer.size * src_buffer.itemsize),
+                            true, hrt::pool_shared)
+                            .unwrap_or_throw();
+                    // arr.inc_ref();
+                    return tensor;
+                })
+            .def("copy_to",
+                 [](runtime_tensor &from, runtime_tensor &to) {
+                     from.copy_to(to).unwrap_or_throw();
+                 })
+            .def("to_numpy",
+                 [](runtime_tensor &tensor) {
+                     auto host = tensor.to_host().unwrap_or_throw();
+                     auto src_map = std::move(
+                         hrt::map(host, runtime::map_read).unwrap_or_throw());
+                     auto src_buffer = src_map.buffer();
+                     return py::array(to_dtype(tensor.impl()->dtype()),
+                                      to_py_shape(tensor.impl()->dtype(),
+                                                  tensor.impl()->shape()),
+                                      to_py_strides(tensor.impl()->dtype(),
+                                                    tensor.impl()->strides()),
+                                      src_buffer.data());
+                 })
+            .def_property_readonly("dtype",
+                                   [](runtime_tensor &tensor) {
+                                       if (tensor.empty()) {
+                                           return py::dtype("empty");
+                                       }
+                                       return to_dtype(tensor.impl()->dtype());
+                                   })
+            .def_property_readonly("shape", [](runtime_tensor &tensor) {
+                if (tensor.empty()) {
+                    return std::vector<pybind11::ssize_t>();
                 }
-
-                auto ref_type = nncase::reference_type_t(
-                    std::in_place, datatype_t::paged_attention_kv_cache);
-
-                auto object_ptrs = new object_node *[1];
-                auto bytes_span = as_span<std::byte>(
-                    std::span<object_node *>(object_ptrs, 1));
-                hrt::data_deleter_t deleter = [](std::byte *ptr) {
-                    auto object_ptrs = reinterpret_cast<object_node **>(ptr);
-                    nncase_object_release(object_ptrs[0]);
-                    delete[] object_ptrs;
-                };
-
-                auto runtime_tensor =
-                    hrt::create(ref_type, {}, bytes_span, deleter)
-                        .unwrap_or_throw();
-
-                auto host_buffer =
-                    runtime_tensor.impl()->buffer().as_host().unwrap_or_throw();
-                auto mapped_data = host_buffer.map(map_write).unwrap_or_throw();
-                auto dest_buffer = as_span<object_node *>(mapped_data.buffer());
-                dest_buffer[0] = object(obj).detach();
-                return runtime_tensor;
-            })
-        .def_static(
-            "from_numpy",
-            [](py::array arr) {
-                arr = py::array::ensure(arr, py::array::c_style);
-                auto src_buffer = arr.request();
-                auto datatype = from_dtype(arr);
-                auto tensor =
-                    host_runtime_tensor::create(
-                        datatype, to_rt_shape(src_buffer.shape),
-                        to_rt_strides(src_buffer.itemsize, src_buffer.strides),
-                        std::span(reinterpret_cast<std::byte *>(src_buffer.ptr),
-                                  src_buffer.size * src_buffer.itemsize),
-                        true, hrt::pool_shared)
-                        .unwrap_or_throw();
-                // arr.inc_ref();
-                return tensor;
-            })
-        .def("copy_to",
-             [](runtime_tensor &from, runtime_tensor &to) {
-                 from.copy_to(to).unwrap_or_throw();
-             })
-        .def("to_numpy",
-             [](runtime_tensor &tensor) {
-                 auto host = tensor.to_host().unwrap_or_throw();
-                 auto src_map = std::move(
-                     hrt::map(host, runtime::map_read).unwrap_or_throw());
-                 auto src_buffer = src_map.buffer();
-                 return py::array(to_dtype(tensor.impl()->dtype()),
-                                  to_py_shape(tensor.impl()->dtype(),
-                                              tensor.impl()->shape()),
-                                  to_py_strides(tensor.impl()->dtype(),
-                                                tensor.impl()->strides()),
-                                  src_buffer.data());
-             })
-        .def_property_readonly("dtype",
-                               [](runtime_tensor &tensor) {
-                                   if (tensor.empty()) {
-                                       return py::dtype("empty");
-                                   }
-                                   return to_dtype(tensor.impl()->dtype());
-                               })
-        .def_property_readonly("shape", [](runtime_tensor &tensor) {
-            if (tensor.empty()) {
-                return std::vector<pybind11::ssize_t>();
-            }
-            auto py_shape =
-                to_py_shape(tensor.impl()->dtype(), tensor.impl()->shape());
-            if (tensor.impl()->dtype().is_a<vector_type_t>()) {
-                auto vtype =
-                    tensor.impl()->dtype().as<vector_type_t>().unwrap();
-                for (auto lane : vtype->lanes()) {
-                    py_shape.push_back(lane);
+                auto py_shape =
+                    to_py_shape(tensor.impl()->dtype(), tensor.impl()->shape());
+                if (tensor.impl()->dtype().is_a<vector_type_t>()) {
+                    auto vtype =
+                        tensor.impl()->dtype().as<vector_type_t>().unwrap();
+                    for (auto lane : vtype->lanes()) {
+                        py_shape.push_back(lane);
+                    }
                 }
-            }
-            return py_shape;
-        });
+                return py_shape;
+            });
 
     return rt_class;
 }
