041691237b79cf0989145ff05b4d96e43a4bf785
sunnycase <sunnycase@live.cn>
2025-06-25 06:52:22 +0000

    Refactor NTT for XPU arch

--- ntt/include/nncase/ntt/arch/xpu/remote_tensor.h
+++ ntt/include/nncase/ntt/arch/xpu/remote_tensor.h
@@ -14,10 +14,8 @@
  */
 #pragma once
 #include "../../distributed/remote_tensor.h"
-#include "../../distributed/topology.h"
 #include "../../tensor.h"
 #include "../../vector.h"
-#include <cstdint>
 
 #if defined(NNCASE_XPU_MODULE) && defined(SYS_MODE)
 #define PREFIX __device__
@@ -25,6 +23,8 @@
 #define PREFIX
 #endif
 
+namespace nncase::ntt::distributed {
+namespace detail {
 PREFIX extern decltype(nncase::ntt::make_tensor<
                        nncase::ntt::vector<uintptr_t, 2>>(
     nncase::ntt::distributed::topology_shape)) global_local_data_ptr;
@@ -32,4 +32,37 @@ PREFIX extern decltype(nncase::ntt::make_tensor<
 PREFIX extern decltype(nncase::ntt::make_tensor<uintptr_t>(
     nncase::ntt::distributed::topology_shape)) global_local_rdata_ptr;
 
-namespace nncase::ntt::distributed {} // namespace nncase::ntt::distributed
+template <class T, topology RemoteScope, topology TensorScope,
+          ScopedProgramIds<TensorScope> TLocalProgramIds,
+          ScopedProgramIds<TensorScope> TRemoteProgramIds>
+static auto get_remote_address(const TLocalProgramIds &local_program_ids,
+                               const TRemoteProgramIds &remote_program_ids,
+                               T *local_address) {
+    auto start = global_local_data_ptr(local_program_ids)(0_dim);
+    auto end = global_local_data_ptr(local_program_ids)(1_dim);
+    auto remote_address = global_local_data_ptr(remote_program_ids)(0_dim);
+    if ((uintptr_t)local_address < start || (uintptr_t)local_address >= end) {
+        start = global_local_rdata_ptr(local_program_ids)(0_dim);
+        remote_address = global_local_rdata_ptr(remote_program_ids)(0_dim);
+    }
+
+    return local_address - (T *)start + (T *)remote_address;
+}
+} // namespace detail
+
+template <topology RemoteScope, topology TensorScope>
+struct remote_tensor_constructor {
+    template <class T, Shape TShape, Strides TStrides,
+              ScopedProgramIds<TensorScope> TLocalProgramIds,
+              ScopedProgramIds<TensorScope> TRemoteProgramIds>
+    constexpr auto operator()(T *data, const TShape &shape,
+                              const TStrides &strides,
+                              const TLocalProgramIds &local_program_ids,
+                              const TRemoteProgramIds &remote_program_ids) {
+        auto remote_address =
+            detail::get_remote_address<T, RemoteScope, TensorScope>(
+                local_program_ids, remote_program_ids, data);
+        return make_tensor_view_from_address(remote_address, shape, strides);
+    }
+};
+} // namespace nncase::ntt::distributed
