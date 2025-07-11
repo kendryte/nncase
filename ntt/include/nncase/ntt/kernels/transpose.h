b7f0845204b6ed153d75b962c2b3b764a49de524
guodongliang <guodongliang@canaan-creative.com>
2025-07-11 15:59:43 +0800

    fallback

--- ntt/include/nncase/ntt/kernels/transpose.h
+++ ntt/include/nncase/ntt/kernels/transpose.h
@@ -21,8 +21,7 @@
 namespace nncase::ntt {
 namespace transpose_detail {
 
-template <Tensor TIn, class TOut, FixedDimensions TPerms, bool Arch>
-class transpose_impl {
+template <Tensor TIn, class TOut, FixedDimensions TPerms> class transpose_impl {
 
   public:
     constexpr void operator()(const TIn &input, TOut &output, const TPerms &) {
@@ -36,33 +35,6 @@ class transpose_impl {
         });
     }
 };
-
-template <FixedTensor TIn, FixedTensor TOut, FixedDimensions TPerms>
-class transpose_impl<TIn, TOut, TPerms, true> {
-
-  public:
-    constexpr void operator()(const TIn &input, TOut &output, const TPerms &) {
-        // constexpr auto rank = TIn::rank();
-        constexpr TPerms perm_const;
-        // constexpr auto pos_perms = positive_axes(perm_const, rank);
-        // ntt::apply(input.shape(), [&](auto index) {
-        //     auto out_index = generate_shape<rank>(
-        //         [&](auto i) { return index[pos_perms[i]]; });
-        //     output(out_index) = input(index);
-        // });
-
-        // auto domain = input.shape();
-        constexpr auto rank = TIn::rank();
-        // constexpr auto pos_perms = positive_axes(perm_const, rank);
-        constexpr std::array<long, 4> pos_perms = {2, 0, 3, 1};
-        dynamic_shape_t<rank> out_index;
-        ntt::apply(TIn::shape(), [&](auto index) {
-            loop<4>([&](auto i) { out_index[i] = index[pos_perms[i]]; });
-            output(out_index) = input(index);
-        });
-    }
-};
-
 } // namespace transpose_detail
 
 template <Tensor TIn, class TOut, FixedDimensions TPerms>
@@ -71,16 +43,7 @@ template <Tensor TIn, class TOut, FixedDimensions TPerms>
 void transpose(
     const TIn &input, TOut &&output,
     const TPerms &perms = make_index_shape<TIn::rank()>().reverse()) {
-
-    constexpr TPerms perm_const;
-    if constexpr (perm_const == fixed_shape_v<2, 0, 3, 1>) {
-        transpose_detail::transpose_impl<TIn, std::decay_t<TOut>, TPerms, true>
-            impl;
-        impl(input, output, perms);
-    } else {
-        transpose_detail::transpose_impl<TIn, std::decay_t<TOut>, TPerms, false>
-            impl;
-        impl(input, output, perms);
-    }
+    transpose_detail::transpose_impl<TIn, std::decay_t<TOut>, TPerms> impl;
+    impl(input, output, perms);
 }
 } // namespace nncase::ntt
