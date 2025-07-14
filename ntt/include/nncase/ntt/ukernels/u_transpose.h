c5b33a442bf34fd1a1c7b96d28c034ba6bdc756f
guodongliang <guodongliang@canaan-creative.com>
2025-07-14 09:39:46 +0800

    some transpose opt pattern

--- ntt/include/nncase/ntt/ukernels/u_transpose.h
+++ ntt/include/nncase/ntt/ukernels/u_transpose.h
@@ -36,41 +36,49 @@ class u_transpose_impl {
     }
 };
 
-template <Tensor TIn, class TOut>
-class u_transpose_impl<TIn, TOut, fixed_shape_t<2, 0, 3, 1>> {
-
-  public:
-    constexpr void operator()(const TIn &input, TOut &output,
-                              const fixed_shape_t<2, 0, 3, 1> &) {
-        for (auto i = 0; i < input.shape()[0]; i++) {
-            for (auto j = 0; j < input.shape()[1]; j++) {
-                for (auto k = 0; k < input.shape()[2]; k++) {
-                    for (auto l = 0; l < input.shape()[3]; l++) {
-                        output(k, i, l, j) = input(i, j, k, l);
-                    }
-                }
-            }
-        }
+#define DEFINE_U_TRANSPOSE_IMPL(PERM0, PERM1, PERM2, PERM3, ACCESS_EXPR)       \
+    template <Tensor TIn, class TOut>                                          \
+    class u_transpose_impl<TIn, TOut,                                          \
+                           fixed_shape_t<PERM0, PERM1, PERM2, PERM3>> {        \
+      public:                                                                  \
+        constexpr void                                                         \
+        operator()(const TIn &input, TOut &output,                             \
+                   const fixed_shape_t<PERM0, PERM1, PERM2, PERM3> &) {        \
+            for (auto i = 0; i < input.shape()[0]; ++i)                        \
+                for (auto j = 0; j < input.shape()[1]; ++j)                    \
+                    for (auto k = 0; k < input.shape()[2]; ++k)                \
+                        for (auto l = 0; l < input.shape()[3]; ++l)            \
+                            output ACCESS_EXPR = input(i, j, k, l);            \
+        }                                                                      \
     }
-};
 
-template <Tensor TIn, class TOut>
-class u_transpose_impl<TIn, TOut, fixed_shape_t<2, 1, 3, 0>> {
+DEFINE_U_TRANSPOSE_IMPL(0, 1, 2, 3, (i, j, k, l));
+DEFINE_U_TRANSPOSE_IMPL(0, 1, 3, 2, (i, j, l, k));
+DEFINE_U_TRANSPOSE_IMPL(0, 2, 1, 3, (i, k, j, l));
+DEFINE_U_TRANSPOSE_IMPL(0, 2, 3, 1, (i, k, l, j));
+DEFINE_U_TRANSPOSE_IMPL(0, 3, 1, 2, (i, l, j, k));
+DEFINE_U_TRANSPOSE_IMPL(0, 3, 2, 1, (i, l, k, j));
 
-  public:
-    constexpr void operator()(const TIn &input, TOut &output,
-                              const fixed_shape_t<2, 1, 3, 0> &) {
-        for (auto i = 0; i < input.shape()[0]; i++) {
-            for (auto j = 0; j < input.shape()[1]; j++) {
-                for (auto k = 0; k < input.shape()[2]; k++) {
-                    for (auto l = 0; l < input.shape()[3]; l++) {
-                        output(k, j, l, i) = input(i, j, k, l);
-                    }
-                }
-            }
-        }
-    }
-};
+DEFINE_U_TRANSPOSE_IMPL(1, 0, 2, 3, (j, i, k, l));
+DEFINE_U_TRANSPOSE_IMPL(1, 0, 3, 2, (j, i, l, k));
+DEFINE_U_TRANSPOSE_IMPL(1, 2, 0, 3, (j, k, i, l));
+DEFINE_U_TRANSPOSE_IMPL(1, 2, 3, 0, (j, k, l, i));
+DEFINE_U_TRANSPOSE_IMPL(1, 3, 0, 2, (j, l, i, k));
+DEFINE_U_TRANSPOSE_IMPL(1, 3, 2, 0, (j, l, k, i));
+
+DEFINE_U_TRANSPOSE_IMPL(2, 0, 1, 3, (k, i, j, l));
+DEFINE_U_TRANSPOSE_IMPL(2, 0, 3, 1, (k, i, l, j));
+DEFINE_U_TRANSPOSE_IMPL(2, 1, 0, 3, (k, j, i, l));
+DEFINE_U_TRANSPOSE_IMPL(2, 1, 3, 0, (k, j, l, i));
+DEFINE_U_TRANSPOSE_IMPL(2, 3, 0, 1, (k, l, i, j));
+DEFINE_U_TRANSPOSE_IMPL(2, 3, 1, 0, (k, l, j, i));
+
+DEFINE_U_TRANSPOSE_IMPL(3, 0, 1, 2, (l, i, j, k));
+DEFINE_U_TRANSPOSE_IMPL(3, 0, 2, 1, (l, i, k, j));
+DEFINE_U_TRANSPOSE_IMPL(3, 1, 0, 2, (l, j, i, k));
+DEFINE_U_TRANSPOSE_IMPL(3, 1, 2, 0, (l, j, k, i));
+DEFINE_U_TRANSPOSE_IMPL(3, 2, 0, 1, (l, k, i, j));
+DEFINE_U_TRANSPOSE_IMPL(3, 2, 1, 0, (l, k, j, i));
 
 } // namespace u_transpose_detail
 
