567b10d68c63cd4807f49cde5ac235b9376fff97
huochenghai <huochenghai@canaan-creative.com>
2025-07-19 14:30:38 +0800

    fix hidden size of PA

--- src/Nncase.Importer/HuggingFace/HuggingFaceBaseModel.cs
+++ src/Nncase.Importer/HuggingFace/HuggingFaceBaseModel.cs
@@ -883,7 +883,7 @@ public abstract class HuggingFaceModel
             scaling.CastTo(pagedAttentionConfig.KVPrimType, CastMode.KDefault),
             count,
             qDestLayout,
-            (int)(long)Context!.Config!["hidden_size"]);
+            (int)((long)Context!.Config!["num_attention_heads"] * (long)Context!.Config!["head_dim"]));
 
         output = qLanes.Length > 0 ? IR.F.Tensors.Unpack(output, qLanes, qPackedAxis) : output;
         output = pagedAttentionConfig.KVPrimType != DataTypes.Float32 ? IR.F.Tensors.Cast(output, DataTypes.Float32) : output;
