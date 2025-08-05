fd43f497997462cf69d533ce59eb34475467ee52
zhengqihang <597323109@qq.com>
2025-08-05 09:36:14 +0000

    support domain less than 1

--- src/Nncase.Evaluator/TypeInferenceVisitor.cs
+++ src/Nncase.Evaluator/TypeInferenceVisitor.cs
@@ -376,7 +376,7 @@ internal sealed partial class TypeInferenceVisitor : ExprVisitor<IRType, Unit>
 
     protected override IRType VisitLeafPhysicalBuffer(PhysicalBuffer expr)
     {
-        VerifySubField(expr, expr.Start, TypePatternUtility.IsNoneType() | TypePatternUtility.IsIntegralScalar());
+        VerifySubField(expr, expr.Start, TypePatternUtility.IsNoneType() | TypePatternUtility.IsIntegralScalar() | TypePatternUtility.IsPointer());
         VerifySubField(expr, expr.Size, TypePatternUtility.IsDimensionType());
         return new PointerType(DataTypes.UInt8);
     }
