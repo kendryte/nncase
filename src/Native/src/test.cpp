a1d9ba842dfbf724f216fe830ebef7582dcaa310
QiHang Zheng <597323109@qq.com>
2025-08-06 19:44:45 +0800

    fix build

--- src/Native/src/test.cpp
+++ src/Native/src/test.cpp
@@ -119,12 +119,12 @@ void test_shape() {
 void test_strides() {
     // dynamic shape strides
     {
-        auto shape = ntt::make_shape(2_dim, 1, 3_dim); // dim1 is dynamic
-        auto strides = ntt::make_strides(3_dim, 3_dim, 1_dim);
+        NNCASE_UNUSED auto shape = ntt::make_shape(2_dim, 1, 3_dim); // dim1 is dynamic
+        NNCASE_UNUSED auto strides = ntt::make_strides(3_dim, 3_dim, 1_dim);
         assert(ntt::contiguous_dims(shape, strides) == 3);
 
-        auto shape1 = ntt::make_shape(8_dim, 1); // dim1 is dynamic
-        auto strides1 = ntt::make_strides(2_dim, 1_dim);
+        NNCASE_UNUSED auto shape1 = ntt::make_shape(8_dim, 1); // dim1 is dynamic
+        NNCASE_UNUSED auto strides1 = ntt::make_strides(2_dim, 1_dim);
         assert(ntt::contiguous_dims(shape1, strides1) == 1);
     }
 }
