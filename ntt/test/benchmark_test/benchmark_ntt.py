24887797eb089c9acb31016b8eecc5dd3fc29923
guodongliang <guodongliang@canaan-creative.com>
2025-08-11 16:19:47 +0800

    fallback

--- ntt/test/benchmark_test/benchmark_ntt.py
+++ ntt/test/benchmark_test/benchmark_ntt.py
@@ -1026,23 +1026,23 @@ if __name__ == '__main__':
     
     # 2. benchmark ntt f16
     # 2.1 x86_64
-    # ntt_x86_64 = BenchmarkNTT_x86_64(args.x86_64_target, args.x86_64_path, 'fp16')
-    # ntt_x86_64.run()
+    ntt_x86_64 = BenchmarkNTT_x86_64(args.x86_64_target, args.x86_64_path, 'fp16')
+    ntt_x86_64.run()
 
-    # # 2.2 riscv64
-    # ntt_riscv64 = BenchmarkNTT_riscv64(args.riscv64_target, args.riscv64_path, 'fp16')
-    # ntt_riscv64.run()
+    # 2.2 riscv64
+    ntt_riscv64 = BenchmarkNTT_riscv64(args.riscv64_target, args.riscv64_path, 'fp16')
+    ntt_riscv64.run()
 
-    # # 2.3 merge benchmark list
-    # benchmark_list = []
-    # for i in range(len(ntt_x86_64.benchmark_list)):
-    #     item = {**ntt_x86_64.benchmark_list[i], **ntt_riscv64.benchmark_list[i]}
-    #     benchmark_list.append(item)
+    # 2.3 merge benchmark list
+    benchmark_list = []
+    for i in range(len(ntt_x86_64.benchmark_list)):
+        item = {**ntt_x86_64.benchmark_list[i], **ntt_riscv64.benchmark_list[i]}
+        benchmark_list.append(item)
 
-    # # 2.4 generate md
-    # md_file = ntt_f16_report_file('benchmark_ntt_f16.md')
-    # benchmark_list = sorted(benchmark_list, key=lambda d: (d['kind'], d['op']))
-    # generate_benchmark_ntt_md(benchmark_list, 'kind', md_file)
+    # 2.4 generate md
+    md_file = ntt_f16_report_file('benchmark_ntt_f16.md')
+    benchmark_list = sorted(benchmark_list, key=lambda d: (d['kind'], d['op']))
+    generate_benchmark_ntt_md(benchmark_list, 'kind', md_file)
 
     # 3. benchmark ntt matmul
     # 3.1 x86_64
