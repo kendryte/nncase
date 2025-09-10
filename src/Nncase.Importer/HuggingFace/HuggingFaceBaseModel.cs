2503a0555a9de2a1e71568a26dc837453d3f06ea
Curio Yang <curioyhq@gmail.com>
2025-09-10 06:42:52 +0000

    fix importer

--- src/Nncase.Importer/HuggingFace/HuggingFaceBaseModel.cs
+++ src/Nncase.Importer/HuggingFace/HuggingFaceBaseModel.cs
@@ -339,10 +339,10 @@ public abstract class HuggingFaceModel
             qInput = Nncase.IR.F.Tensors.Cast(qInput, DataTypes.Float8E4M3);
             var transposed_weight = IR.F.Tensors.Transpose(weight, new long[] { 1, 0 }).Evaluate().AsTensor();
             var qWeights = IR.F.Tensors.Cast(transposed_weight, DataTypes.Float8E4M3);
-            var qMatmul = Nncase.IR.F.Math.MatMul(qInput, qWeights, DataTypes.Float32).With(metadata: new IRMetadata() { OutputNames = new[] { layerName } });
+            var result = Nncase.IR.F.Math.MatMul(qInput, qWeights, expr.CheckedDataType, deqScaleA * deqScaleB).With(metadata: new IRMetadata() { OutputNames = new[] { layerName } });
 
-            var result = Nncase.IR.F.Math.Binary(Nncase.BinaryOp.Mul, qMatmul, deqScaleA * deqScaleB);
-            result = Nncase.IR.F.Tensors.Cast(result, expr.CheckedDataType);
+            // var result = Nncase.IR.F.Math.Binary(Nncase.BinaryOp.Mul, qMatmul, deqScaleA * deqScaleB);
+            // result = Nncase.IR.F.Tensors.Cast(result, expr.CheckedDataType);
             if (bias != null)
             {
                 bias = bias.CastTo(expr.CheckedDataType);
