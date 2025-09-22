5bd55113aeec2595d348d0984325ec21295960c6
QiHang Zheng <597323109@qq.com>
2025-09-22 14:27:03 +0800

    fix build

--- src/Nncase.Core/IR/Tensors/Functional.cs
+++ src/Nncase.Core/IR/Tensors/Functional.cs
@@ -217,5 +217,5 @@ public static class Tensors
         return new Call(new Unpack(lanes, axes), input);
     }
 
-    public static Call LocalShardDim(Dimension dim, SBP axisPolicy, Placement placement) => new Call(new LocalShardDim(axisPolicy, placement), dim);
+    public static Call LocalShardDim(Dimension dim, SBP axisPolicy, Placement placement) => new Call(new LocalShardDim(axisPolicy, placement), (BaseExpr)dim);
 }
