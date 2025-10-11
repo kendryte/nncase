02d5d85d098663d5ec257fd457b7dfe99624b141
guodongliang <guodongliang@canaan-creative.com>
2025-10-11 15:09:38 +0800

    better performance

--- ntt/include/nncase/ntt/ukernels/u_top_k.h
+++ ntt/include/nncase/ntt/ukernels/u_top_k.h
@@ -12,18 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma once
-#include "../post_ops.h"
-#include "../primitive_ops.h"
-#include "../vector.h"
-#include "nncase/ntt/tensor_traits.h"
-
 namespace nncase::ntt {
 namespace ukernels {
-
-template <bool Arch> struct u_top_k_policy {
-    static constexpr size_t unroll = 1;
-};
 
 template <bool Arch, Scalar TProbs, Scalar TIndices, size_t Rank, size_t Axis>
 struct u_top_k {
@@ -33,150 +23,149 @@ struct u_top_k {
                               TIndices *slice_indices_ptr, int64_t input_stride,
                               int64_t out_probs_stride,
                               int64_t out_indices_stride, int K,
-                              int64_t largest, int64_t sorted) {
-        for (int i = 0; i < K; i++) {
-            slice_indices_ptr[i * out_indices_stride] = i;
-            slice_probs_ptr[i * out_probs_stride] =
-                slice_input_ptr[i * input_stride];
-        }
+                              int64_t largest, int64_t sorted) const {
+        if (K <= 0 || inner_size <= 0)
+            return;
+        if (K > inner_size)
+            K = static_cast<int>(inner_size);
 
-        for (int i = K; i < inner_size; i++) {
-            auto candidate_value = slice_input_ptr[i * input_stride];
-            for (int k = 0; k < K; k++) {
-                auto top_value = slice_probs_ptr[k * out_probs_stride];
-                if (largest) {
-                    if (candidate_value > top_value) {
-                        requeue_descending(slice_probs_ptr, slice_indices_ptr,
-                                           out_probs_stride, out_indices_stride,
-                                           candidate_value, i, k, K);
+        constexpr int MAX_K = 128;
+
+        TProbs heap_vals[MAX_K];
+        int64_t heap_ids[MAX_K];
+        int heap_size = 0;
+
+        auto heap_swap = [&](int a, int b) {
+            TProbs tv = heap_vals[a];
+            heap_vals[a] = heap_vals[b];
+            heap_vals[b] = tv;
+            int64_t ti = heap_ids[a];
+            heap_ids[a] = heap_ids[b];
+            heap_ids[b] = ti;
+        };
+
+        if (largest) {
+            auto heapify_up = [&](int idx) {
+                while (idx > 0) {
+                    int parent = (idx - 1) / 2;
+                    if (!(heap_vals[idx] < heap_vals[parent]))
                         break;
-                    }
-                } else {
-                    if (candidate_value < top_value) {
-                        requeue_ascending(slice_probs_ptr, slice_indices_ptr,
-                                          out_probs_stride, out_indices_stride,
-                                          candidate_value, i, k, K);
+                    heap_swap(idx, parent);
+                    idx = parent;
+                }
+            };
+            auto heapify_down = [&](int idx) {
+                while (true) {
+                    int left = idx * 2 + 1;
+                    int right = left + 1;
+                    int smallest = idx;
+                    if (left < heap_size &&
+                        heap_vals[left] < heap_vals[smallest])
+                        smallest = left;
+                    if (right < heap_size &&
+                        heap_vals[right] < heap_vals[smallest])
+                        smallest = right;
+                    if (smallest == idx)
                         break;
+                    heap_swap(idx, smallest);
+                    idx = smallest;
+                }
+            };
+
+            for (int64_t i = 0; i < inner_size; ++i) {
+                TProbs v = slice_input_ptr[i * input_stride];
+                if (heap_size < K) {
+                    heap_vals[heap_size] = v;
+                    heap_ids[heap_size] = i;
+                    heapify_up(heap_size);
+                    ++heap_size;
+                } else if (v > heap_vals[0]) {
+                    heap_vals[0] = v;
+                    heap_ids[0] = i;
+                    heapify_down(0);
+                }
+            }
+
+            if (sorted) {
+                for (int i = 0; i < heap_size - 1; ++i) {
+                    int max_i = i;
+                    for (int j = i + 1; j < heap_size; ++j) {
+                        if (heap_vals[j] > heap_vals[max_i])
+                            max_i = j;
                     }
-                }
-            }
-        }
-
-        if (sorted) {
-            if (largest) {
-                sort_descending(slice_probs_ptr, slice_indices_ptr,
-                                out_probs_stride, out_indices_stride, K);
-            } else {
-                sort_ascending(slice_probs_ptr, slice_indices_ptr,
-                               out_probs_stride, out_indices_stride, K);
-            }
-        }
-    }
-
-  private:
-    void sort_descending(TProbs *out_probs, TIndices *out_indices,
-                         int64_t out_probs_stride, int64_t out_indices_stride,
-                         int K) {
-        for (int cur_idx = 0; cur_idx < K - 1; ++cur_idx) {
-            auto max_idx = cur_idx;
-
-            for (int next_idx = cur_idx + 1; next_idx < K; ++next_idx) {
-                if (out_probs[next_idx * out_probs_stride] >
-                    out_probs[max_idx * out_probs_stride]) {
-                    max_idx = next_idx;
+                    if (max_i != i)
+                        heap_swap(i, max_i);
                 }
             }
 
-            if (max_idx != cur_idx) {
-                auto tempValue = out_probs[cur_idx * out_probs_stride];
-                out_probs[cur_idx * out_probs_stride] =
-                    out_probs[max_idx * out_probs_stride];
-                out_probs[max_idx * out_probs_stride] = tempValue;
-
-                auto tempIndex = out_indices[cur_idx * out_indices_stride];
-                out_indices[cur_idx * out_indices_stride] =
-                    out_indices[max_idx * out_indices_stride];
-                out_indices[max_idx * out_indices_stride] = tempIndex;
+            for (int i = 0; i < heap_size; ++i) {
+                slice_probs_ptr[i * out_probs_stride] = heap_vals[i];
+                slice_indices_ptr[i * out_indices_stride] =
+                    static_cast<TIndices>(heap_ids[i]);
             }
-        }
-    }
+        } else {
+            auto heapify_up = [&](int idx) {
+                while (idx > 0) {
+                    int parent = (idx - 1) / 2;
+                    if (!(heap_vals[idx] > heap_vals[parent]))
+                        break;
+                    heap_swap(idx, parent);
+                    idx = parent;
+                }
+            };
+            auto heapify_down = [&](int idx) {
+                while (true) {
+                    int left = idx * 2 + 1;
+                    int right = left + 1;
+                    int largest_i = idx;
+                    if (left < heap_size &&
+                        heap_vals[left] > heap_vals[largest_i])
+                        largest_i = left;
+                    if (right < heap_size &&
+                        heap_vals[right] > heap_vals[largest_i])
+                        largest_i = right;
+                    if (largest_i == idx)
+                        break;
+                    heap_swap(idx, largest_i);
+                    idx = largest_i;
+                }
+            };
 
-    void sort_ascending(TProbs *out_probs, TIndices *out_indices,
-                        int64_t out_probs_stride, int64_t out_indices_stride,
-                        int K) {
-        for (int cur_idx = 0; cur_idx < K - 1; ++cur_idx) {
-            auto min_idx = cur_idx;
-
-            for (int next_idx = cur_idx + 1; next_idx < K; ++next_idx) {
-                if (out_probs[next_idx * out_probs_stride] <
-                    out_probs[min_idx * out_probs_stride]) {
-                    min_idx = next_idx;
+            for (int64_t i = 0; i < inner_size; ++i) {
+                TProbs v = slice_input_ptr[i * input_stride];
+                if (heap_size < K) {
+                    heap_vals[heap_size] = v;
+                    heap_ids[heap_size] = i;
+                    heapify_up(heap_size);
+                    ++heap_size;
+                } else if (v < heap_vals[0]) {
+                    heap_vals[0] = v;
+                    heap_ids[0] = i;
+                    heapify_down(0);
                 }
             }
 
-            if (min_idx != cur_idx) {
-                auto tempValue = out_probs[cur_idx * out_probs_stride];
-                out_probs[cur_idx * out_probs_stride] =
-                    out_probs[min_idx * out_probs_stride];
-                out_probs[min_idx * out_probs_stride] = tempValue;
-
-                auto tempIndex = out_indices[cur_idx * out_indices_stride];
-                out_indices[cur_idx * out_indices_stride] =
-                    out_indices[min_idx * out_indices_stride];
-                out_indices[min_idx * out_indices_stride] = tempIndex;
+            if (sorted) {
+                for (int i = 0; i < heap_size - 1; ++i) {
+                    int min_i = i;
+                    for (int j = i + 1; j < heap_size; ++j) {
+                        if (heap_vals[j] < heap_vals[min_i])
+                            min_i = j;
+                    }
+                    if (min_i != i)
+                        heap_swap(i, min_i);
+                }
             }
-        }
-    }
 
-    void requeue_descending(TProbs *out_probs, TIndices *out_indices,
-                            int64_t out_probs_stride,
-                            int64_t out_indices_stride, TProbs candidate_value,
-                            int64_t candidate_index, int64_t current_top_index,
-                            int64_t K) {
-        auto next_candidate_value =
-            out_probs[current_top_index * out_probs_stride];
-        auto next_candidata_index =
-            out_indices[current_top_index * out_indices_stride];
-
-        out_probs[current_top_index * out_probs_stride] = candidate_value;
-        out_indices[current_top_index * out_indices_stride] = candidate_index;
-
-        bool replaced = false;
-        for (int k = 0; k < K; k++) {
-            auto top_value = out_probs[k * out_probs_stride];
-            if (next_candidate_value > top_value && !replaced) {
-                requeue_descending(out_probs, out_indices, out_probs_stride,
-                                   out_indices_stride, next_candidate_value,
-                                   next_candidata_index, k, K);
-                replaced = true;
-            }
-        }
-    }
-
-    void requeue_ascending(TProbs *out_probs, TIndices *out_indices,
-                           int64_t out_probs_stride, int64_t out_indices_stride,
-                           TProbs candidate_value, TIndices candidate_index,
-                           TIndices current_down_index, int64_t K) {
-        auto next_candidate_value =
-            out_probs[current_down_index * out_probs_stride];
-        auto next_candidata_index =
-            out_indices[current_down_index * out_indices_stride];
-
-        out_probs[current_down_index * out_probs_stride] = candidate_value;
-        out_indices[current_down_index * out_indices_stride] = candidate_index;
-
-        bool replaced = false;
-        for (int k = 0; k < K; k++) {
-            auto down_value = out_probs[k * out_probs_stride];
-            if (next_candidate_value < down_value && !replaced) {
-                requeue_ascending(out_probs, out_indices, out_probs_stride,
-                                  out_indices_stride, next_candidate_value,
-                                  next_candidata_index, k, K);
-                replaced = true;
+            for (int i = 0; i < heap_size; ++i) {
+                slice_probs_ptr[i * out_probs_stride] = heap_vals[i];
+                slice_indices_ptr[i * out_indices_stride] =
+                    static_cast<TIndices>(heap_ids[i]);
             }
         }
     }
 };
+
 } // namespace ukernels
 
 template <Scalar TProbs, Scalar TIndices, size_t Rank, size_t Axis>
@@ -191,4 +180,5 @@ void u_top_k(int64_t inner_size, const TProbs *slice_input_ptr,
          input_stride, out_probs_stride, out_indices_stride, K, largest,
          sorted);
 }
+
 } // namespace nncase::ntt
