a0e85bfc732b78c783aa9903b5dffa8a8a20ee30
guodongliang <guodongliang@canaan-creative.com>
2025-10-11 16:07:50 +0800

    more cases for topk

--- ntt/test/ctest/test_ntt_top_k.cpp
+++ ntt/test/ctest/test_ntt_top_k.cpp
@@ -22,7 +22,247 @@
 using namespace nncase;
 using namespace ortki;
 
-TEST(top_k_2d, typical_largest_sorted_axis_1) {
+TEST(top_k_2d, typical1_largest_sorted_axis_0) {
+
+    constexpr int64_t axis = 0;
+    const int64_t largest = 1;
+    const int64_t sorted = 1;
+    constexpr int64_t M = 64;
+    constexpr int64_t N = 128;
+    constexpr int64_t k = 16;
+
+    auto ntt_probs = ntt::make_tensor<float>(ntt::fixed_shape_v<M, N>);
+    auto ntt_k = ntt::make_tensor<int64_t>(ntt::fixed_shape_v<1>);
+    NttTest::init_tensor(ntt_probs, -10.f, 10.f);
+    ntt_k(0) = k;
+
+    auto ntt_top_k_probs_expect =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_expect =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    auto ntt_top_k_probs_actual =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_actual =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    ntt::top_k(ntt_probs, ntt_k, ntt_top_k_probs_actual,
+               ntt_top_k_indices_actual, fixed_dim_v<axis>, largest, sorted);
+
+    auto ort_probs = NttTest::ntt2ort(ntt_probs);
+    auto ort_k = NttTest::ntt2ort(ntt_k);
+    auto ort_output = ortki_TopK(ort_probs, ort_k, axis, largest, sorted);
+    auto ort_top_k_probs = tensor_seq_get_value(ort_output, 0);
+    auto ort_top_k_idices = tensor_seq_get_value(ort_output, 1);
+    NttTest::ort2ntt(ort_top_k_probs, ntt_top_k_probs_expect);
+    NttTest::ort2ntt(ort_top_k_idices, ntt_top_k_indices_expect);
+    EXPECT_TRUE(NttTest::compare_tensor(ntt_top_k_probs_expect,
+                                        ntt_top_k_probs_actual) &&
+                NttTest::compare_tensor(ntt_top_k_indices_expect,
+                                        ntt_top_k_indices_actual));
+}
+
+TEST(top_k_2d, typical1_smallest_sorted_axis_0) {
+
+    constexpr int64_t axis = 0;
+    const int64_t largest = 0;
+    const int64_t sorted = 1;
+    constexpr int64_t M = 64;
+    constexpr int64_t N = 128;
+    constexpr int64_t k = 16;
+
+    auto ntt_probs = ntt::make_tensor<float>(ntt::fixed_shape_v<M, N>);
+    auto ntt_k = ntt::make_tensor<int64_t>(ntt::fixed_shape_v<1>);
+    NttTest::init_tensor(ntt_probs, -10.f, 10.f);
+    ntt_k(0) = k;
+
+    auto ntt_top_k_probs_expect =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_expect =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    auto ntt_top_k_probs_actual =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_actual =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    ntt::top_k(ntt_probs, ntt_k, ntt_top_k_probs_actual,
+               ntt_top_k_indices_actual, fixed_dim_v<axis>, largest, sorted);
+
+    auto ort_probs = NttTest::ntt2ort(ntt_probs);
+    auto ort_k = NttTest::ntt2ort(ntt_k);
+    auto ort_output = ortki_TopK(ort_probs, ort_k, axis, largest, sorted);
+    auto ort_top_k_probs = tensor_seq_get_value(ort_output, 0);
+    auto ort_top_k_idices = tensor_seq_get_value(ort_output, 1);
+    NttTest::ort2ntt(ort_top_k_probs, ntt_top_k_probs_expect);
+    NttTest::ort2ntt(ort_top_k_idices, ntt_top_k_indices_expect);
+    EXPECT_TRUE(NttTest::compare_tensor(ntt_top_k_probs_expect,
+                                        ntt_top_k_probs_actual) &&
+                NttTest::compare_tensor(ntt_top_k_indices_expect,
+                                        ntt_top_k_indices_actual));
+}
+
+TEST(top_k_2d, typical1_largest_sorted_axis_1) {
+
+    constexpr int64_t axis = -1;
+    const int64_t largest = 1;
+    const int64_t sorted = 1;
+    constexpr int64_t M = 64;
+    constexpr int64_t N = 128;
+    constexpr int64_t k = 16;
+
+    auto ntt_probs = ntt::make_tensor<float>(ntt::fixed_shape_v<M, N>);
+    auto ntt_k = ntt::make_tensor<int64_t>(ntt::fixed_shape_v<1>);
+    NttTest::init_tensor(ntt_probs, -10.f, 10.f);
+    ntt_k(0) = k;
+
+    auto ntt_top_k_probs_expect =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<M, k>);
+    auto ntt_top_k_indices_expect =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<M, k>);
+
+    auto ntt_top_k_probs_actual =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<M, k>);
+    auto ntt_top_k_indices_actual =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<M, k>);
+
+    ntt::top_k(ntt_probs, ntt_k, ntt_top_k_probs_actual,
+               ntt_top_k_indices_actual, fixed_dim_v<axis>, largest, sorted);
+
+    auto ort_probs = NttTest::ntt2ort(ntt_probs);
+    auto ort_k = NttTest::ntt2ort(ntt_k);
+    auto ort_output = ortki_TopK(ort_probs, ort_k, axis, largest, sorted);
+    auto ort_top_k_probs = tensor_seq_get_value(ort_output, 0);
+    auto ort_top_k_idices = tensor_seq_get_value(ort_output, 1);
+    NttTest::ort2ntt(ort_top_k_probs, ntt_top_k_probs_expect);
+    NttTest::ort2ntt(ort_top_k_idices, ntt_top_k_indices_expect);
+    EXPECT_TRUE(NttTest::compare_tensor(ntt_top_k_probs_expect,
+                                        ntt_top_k_probs_actual) &&
+                NttTest::compare_tensor(ntt_top_k_indices_expect,
+                                        ntt_top_k_indices_actual));
+}
+
+TEST(top_k_2d, typical1_smallest_sorted_axis_1) {
+
+    constexpr int64_t axis = -1;
+    const int64_t largest = 0;
+    const int64_t sorted = 1;
+    constexpr int64_t M = 64;
+    constexpr int64_t N = 128;
+    constexpr int64_t k = 16;
+
+    auto ntt_probs = ntt::make_tensor<float>(ntt::fixed_shape_v<M, N>);
+    auto ntt_k = ntt::make_tensor<int64_t>(ntt::fixed_shape_v<1>);
+    NttTest::init_tensor(ntt_probs, -10.f, 10.f);
+    ntt_k(0) = k;
+
+    auto ntt_top_k_probs_expect =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<M, k>);
+    auto ntt_top_k_indices_expect =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<M, k>);
+
+    auto ntt_top_k_probs_actual =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<M, k>);
+    auto ntt_top_k_indices_actual =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<M, k>);
+
+    ntt::top_k(ntt_probs, ntt_k, ntt_top_k_probs_actual,
+               ntt_top_k_indices_actual, fixed_dim_v<axis>, largest, sorted);
+
+    auto ort_probs = NttTest::ntt2ort(ntt_probs);
+    auto ort_k = NttTest::ntt2ort(ntt_k);
+    auto ort_output = ortki_TopK(ort_probs, ort_k, axis, largest, sorted);
+    auto ort_top_k_probs = tensor_seq_get_value(ort_output, 0);
+    auto ort_top_k_idices = tensor_seq_get_value(ort_output, 1);
+    NttTest::ort2ntt(ort_top_k_probs, ntt_top_k_probs_expect);
+    NttTest::ort2ntt(ort_top_k_idices, ntt_top_k_indices_expect);
+    EXPECT_TRUE(NttTest::compare_tensor(ntt_top_k_probs_expect,
+                                        ntt_top_k_probs_actual) &&
+                NttTest::compare_tensor(ntt_top_k_indices_expect,
+                                        ntt_top_k_indices_actual));
+}
+
+TEST(top_k_2d, typical2_largest_sorted_axis_0) {
+
+    constexpr int64_t axis = 0;
+    const int64_t largest = 1;
+    const int64_t sorted = 1;
+    constexpr int64_t M = 64;
+    constexpr int64_t N = 128;
+    constexpr int64_t k = 8;
+
+    auto ntt_probs = ntt::make_tensor<float>(ntt::fixed_shape_v<M, N>);
+    auto ntt_k = ntt::make_tensor<int64_t>(ntt::fixed_shape_v<1>);
+    NttTest::init_tensor(ntt_probs, -10.f, 10.f);
+    ntt_k(0) = k;
+
+    auto ntt_top_k_probs_expect =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_expect =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    auto ntt_top_k_probs_actual =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_actual =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    ntt::top_k(ntt_probs, ntt_k, ntt_top_k_probs_actual,
+               ntt_top_k_indices_actual, fixed_dim_v<axis>, largest, sorted);
+
+    auto ort_probs = NttTest::ntt2ort(ntt_probs);
+    auto ort_k = NttTest::ntt2ort(ntt_k);
+    auto ort_output = ortki_TopK(ort_probs, ort_k, axis, largest, sorted);
+    auto ort_top_k_probs = tensor_seq_get_value(ort_output, 0);
+    auto ort_top_k_idices = tensor_seq_get_value(ort_output, 1);
+    NttTest::ort2ntt(ort_top_k_probs, ntt_top_k_probs_expect);
+    NttTest::ort2ntt(ort_top_k_idices, ntt_top_k_indices_expect);
+    EXPECT_TRUE(NttTest::compare_tensor(ntt_top_k_probs_expect,
+                                        ntt_top_k_probs_actual) &&
+                NttTest::compare_tensor(ntt_top_k_indices_expect,
+                                        ntt_top_k_indices_actual));
+}
+
+TEST(top_k_2d, typical2_smallest_sorted_axis_0) {
+
+    constexpr int64_t axis = 0;
+    const int64_t largest = 0;
+    const int64_t sorted = 1;
+    constexpr int64_t M = 64;
+    constexpr int64_t N = 128;
+    constexpr int64_t k = 8;
+
+    auto ntt_probs = ntt::make_tensor<float>(ntt::fixed_shape_v<M, N>);
+    auto ntt_k = ntt::make_tensor<int64_t>(ntt::fixed_shape_v<1>);
+    NttTest::init_tensor(ntt_probs, -10.f, 10.f);
+    ntt_k(0) = k;
+
+    auto ntt_top_k_probs_expect =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_expect =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    auto ntt_top_k_probs_actual =
+        ntt::make_tensor<float>(ntt::fixed_shape_v<k, N>);
+    auto ntt_top_k_indices_actual =
+        ntt::make_tensor<int64_t>(ntt::fixed_shape_v<k, N>);
+
+    ntt::top_k(ntt_probs, ntt_k, ntt_top_k_probs_actual,
+               ntt_top_k_indices_actual, fixed_dim_v<axis>, largest, sorted);
+
+    auto ort_probs = NttTest::ntt2ort(ntt_probs);
+    auto ort_k = NttTest::ntt2ort(ntt_k);
+    auto ort_output = ortki_TopK(ort_probs, ort_k, axis, largest, sorted);
+    auto ort_top_k_probs = tensor_seq_get_value(ort_output, 0);
+    auto ort_top_k_idices = tensor_seq_get_value(ort_output, 1);
+    NttTest::ort2ntt(ort_top_k_probs, ntt_top_k_probs_expect);
+    NttTest::ort2ntt(ort_top_k_idices, ntt_top_k_indices_expect);
+    EXPECT_TRUE(NttTest::compare_tensor(ntt_top_k_probs_expect,
+                                        ntt_top_k_probs_actual) &&
+                NttTest::compare_tensor(ntt_top_k_indices_expect,
+                                        ntt_top_k_indices_actual));
+}
+
+TEST(top_k_2d, typical2_largest_sorted_axis_1) {
 
     constexpr int64_t axis = -1;
     const int64_t largest = 1;
@@ -62,7 +302,7 @@ TEST(top_k_2d, typical_largest_sorted_axis_1) {
                                         ntt_top_k_indices_actual));
 }
 
-TEST(top_k_2d, typical_smallest_sorted_axis_1) {
+TEST(top_k_2d, typical2_smallest_sorted_axis_1) {
 
     constexpr int64_t axis = -1;
     const int64_t largest = 0;
