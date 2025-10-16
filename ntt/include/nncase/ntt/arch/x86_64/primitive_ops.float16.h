c24ccadb68ac5636ce0991fbdb29a6af820be39a
sunnycase <sunnycase@live.cn>
2025-10-16 09:30:17 +0000

    Optimize fp16 inner_product

--- ntt/include/nncase/ntt/arch/x86_64/primitive_ops.float16.h
+++ ntt/include/nncase/ntt/arch/x86_64/primitive_ops.float16.h
@@ -950,24 +950,28 @@ struct mul_add<half, ntt::vector<half, 16>, ntt::vector<float, 2, 8>> {
 //     }
 // };
 
-// // inner product
-// template <> struct inner_product<ntt::vector<float, 8>, ntt::vector<float,
-// 8>> {
-//     float operator()(const ntt::vector<float, 8> &v1,
-//                      const ntt::vector<float, 8> &v2) const noexcept {
-//         // Multiply the elements
-//         __m256 mul = _mm256_mul_ps(v1, v2);
+// inner product
+template <> struct inner_product<ntt::vector<half, 16>, ntt::vector<half, 16>> {
+    half operator()(const ntt::vector<half, 16> &v1,
+                    const ntt::vector<half, 16> &v2) const noexcept {
+        const auto v1_f32 = ntt::cast_elem<float>(v1);
+        const auto v2_f32 = ntt::cast_elem<float>(v2);
 
-//         // Sum the elements in the 256-bit vector directly
-//         __m128 sum1 = _mm_add_ps(_mm256_castps256_ps128(mul),
-//                                  _mm256_extractf128_ps(mul, 1));
-//         sum1 = _mm_add_ps(sum1, _mm_movehl_ps(sum1, sum1));
-//         sum1 = _mm_add_ss(sum1, _mm_shuffle_ps(sum1, sum1, 1));
+        // Multiply the elements
+        __m256 mul_0 = _mm256_mul_ps(v1_f32(0_dim), v2_f32(0_dim));
+        __m256 mul_1 = _mm256_mul_ps(v1_f32(1_dim), v2_f32(1_dim));
+        __m256 mul = _mm256_add_ps(mul_0, mul_1);
 
-//         // Extract and return the final sum
-//         return _mm_cvtss_f32(sum1);
-//     }
-// };
+        // Sum the elements in the 256-bit vector directly
+        __m128 sum1 = _mm_add_ps(_mm256_castps256_ps128(mul),
+                                 _mm256_extractf128_ps(mul, 1));
+        sum1 = _mm_add_ps(sum1, _mm_movehl_ps(sum1, sum1));
+        sum1 = _mm_add_ss(sum1, _mm_shuffle_ps(sum1, sum1, 1));
+
+        // Extract and return the final sum
+        return ntt::cast_elem<half>(_mm_cvtss_f32(sum1));
+    }
+};
 
 // // outer product
 // template <> struct outer_product<ntt::vector<float, 8>, ntt::vector<float,
