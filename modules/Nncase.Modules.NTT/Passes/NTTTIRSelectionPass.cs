5b2047df89d601ce107118c3f42e957818e4a0fb
huochenghai <huochenghai@canaan-creative.com>
2025-10-30 13:52:17 +0800

    skip block local data opt of tiling ops

--- modules/Nncase.Modules.NTT/Passes/NTTTIRSelectionPass.cs
+++ modules/Nncase.Modules.NTT/Passes/NTTTIRSelectionPass.cs
@@ -324,7 +324,7 @@ public sealed class NTTTIRSelectionPass : TIRSelectionPass
 
     private bool TryGenerateGatherThreadsReshard(TIR.Buffer inBuffer, ref Expr output, DistributedType inType, DistributedType outType, [MaybeNullWhen(false)] out Expr newCall)
     {
-        if (inType.Partial is not null)
+        if (inType.Partial is not null || inBuffer.Users.Any(u => u is Call { Target: TIR.PrimFunction }))
         {
             newCall = null;
             return false;
@@ -407,7 +407,7 @@ public sealed class NTTTIRSelectionPass : TIRSelectionPass
     private bool TryGenerateSplitThreadsReshard(TIR.Buffer inBuffer, ref Expr output, DistributedType inType, DistributedType outType, [MaybeNullWhen(false)] out Expr newCall)
     {
         // Avoid P -> B -> S
-        if (inType.Partial is not null)
+        if (inType.Partial is not null || inBuffer.Users.Any(u => u is Call { Target: TIR.PrimFunction }))
         {
             newCall = null;
             return false;
